42cb87013acd795c7394eb0192fa8bc581d9406b
Julie Schramm <Julie.Schramm@noaa.gov>
2018-07-23 21:42:59 +0000

    - Moved help comments from plot_fields.ncl to README file - Added user comments to top of make_FV3_RAP_domain_plots.sh - Added input_file, fields, nlev and fcst_index to allow   user to set these values from the make_FV3_RAP_domain_plots.sh   script.  Currently, only one value can be set at a time. - Cleaned up the make*.sh script.  No need to move files   containing plots, since they are named in the ncl script.

--- ush/NCL/make_FV3_RAP_domain_plots.sh
+++ ush/NCL/make_FV3_RAP_domain_plots.sh
@@ -1,4 +1,22 @@
 #!/bin/sh -l
+#==========================================================================
+# Description: This script runs plot_fields.ncl and sets all of the command-
+#              line arguments.  There are 6 examples.  The default plot is at
+#              the bottom on this file and plots FV3 output on a subregion over
+#              CONUS with an outline of the RAP boundary.  Parameters that should
+#              be modified by the user at the top of this script are:
+#
+#                 input_file        # An FV3 netcdf output file
+#                 grid_dir          # The location of your input file
+#                 fields            # A field to plot from input_file
+#                 nlev              # A vertical level for (time,x,y,x) fields
+#                 fcst_index        # A time index to plot
+#
+#              There is more documentation at the top of plot_fields.ncl and
+#              in the README file.
+#
+# Usage: ./make_FV3_RAP_domain_plots.sh
+#==========================================================================
 
 set -eux
 
@@ -10,20 +28,29 @@ RES="96"
 
 CRES="C${RES}"
 
+input_file="atmos_4xdaily.nc"     # FV3 output file
+fields="u1000"                    # Fields to plot from input_file
+nlev="50"                         # Vertical index to plot for 3D fields
+fcst_index="2"                    # Time index of forecast to plot
 #grid_dir="/scratch3/BMC/det/beck/FV3-CAM/work.C384r0p7n3_regional_RAP/INPUT"
 #grid_dir="/scratch3/BMC/fim/Gerard.Ketefian/regional_FV3_EMC_visit_20180509/work_FV3_regional_C96_2018032900/INPUT"
 grid_dir="/scratch3/BMC/fim/Julie.Schramm/regional_FV3_EMC_visit_20180509/work_FV3_regional_C96_2018032900/INPUT"
 #grid_dir="/scratch3/BMC/fim/Gerard.Ketefian/regional_FV3_EMC_visit_20180509/work_FV3_regional_C96_2018032900/INPUT"
 
-RAP_grid_fn="/scratch3/BMC/fim/Gerard.Ketefian/regional_FV3_EMC_visit_20180509/geo_em.d01.nc"
+#RAP_grid_fn="/scratch3/BMC/fim/Gerard.Ketefian/regional_FV3_EMC_visit_20180509/geo_em.d01.nc"
+RAP_grid_fn="/scratch3/BMC/fim/Gerard.Ketefian/regional_FV3_EMC_visit_20180509/geo_em.d01.RAP.nc"
 
 if [ 0 = 1 ]; then
 #
 # Show FV3 regional domain (tile 7) and the original RAP domain outline
-# on a cylindrical projection.
+# on a global cylindrical projection.
 #
 ncl -n plot_fields.ncl \
   grid_dir=\"$grid_dir\" \
+  input_file=\"$input_file\" \
+  fields=\"${fields}\" \
+  nlev=${nlev} \
+  fcst_index=${fcst_index} \
   res=${RES} \
   tile_inds=\(/7/\) \
   draw_tile_bdy=True \
@@ -35,7 +62,6 @@ ncl -n plot_fields.ncl \
   map_proj=\"cyln\" \
   graphics_type=\"png\"
 #
-mv ${CRES}_grid.png ${CRES}rgnl_grid_size_and_RAP_domain_cyln.png
 fi
 
 if [ 0 = 1 ]; then
@@ -45,6 +71,10 @@ if [ 0 = 1 ]; then
 #
 ncl -n plot_fields.ncl \
   grid_dir=\"$grid_dir\" \
+  input_file=\"$input_file\" \
+  fields=\"${fields}\" \
+  nlev=${nlev} \
+  fcst_index=${fcst_index} \
   res=${RES} \
   tile_inds=\(/7/\) \
   draw_tile_bdy=True \
@@ -57,17 +87,19 @@ ncl -n plot_fields.ncl \
   map_proj_ctr=\(/-105,50/\) \
   graphics_type=\"png\"
 #
-mv ${CRES}_grid.png ${CRES}rgnl_grid_size_and_RAP_domain_sphr.png
 fi
-
 
 if [ 0 = 1 ]; then
 #
-# Show FV3 regional domain (tile 7) and the original RAP domain outline
-# on a sphere (orthogonal spherical projection).
+# Show FV3 regional domain (tiles 5,6,7) and the original RAP domain outline
+# on a cylindrical projection.
 #
 ncl -n plot_fields.ncl \
   grid_dir=\"$grid_dir\" \
+  input_file=\"$input_file\" \
+  fields=\"${fields}\" \
+  nlev=${nlev} \
+  fcst_index=${fcst_index} \
   res=${RES} \
   tile_inds=\(/5,6,7/\) \
   draw_tile_bdy=True \
@@ -80,16 +112,19 @@ ncl -n plot_fields.ncl \
   subreg=\(/-47,-40,15,22/\) \
   graphics_type=\"png\"
 #
-mv ${CRES}_grid.png ${CRES}rgnl_and_RAP_gridlines_cyln_closeup.png
 fi
 
 if [ 0 = 1 ]; then
 #
 # Show FV3 regional domain (tile 7) and the original RAP domain outline
-# on a cylindrical projection.
+# on a global cylindrical projection (no subregion).
 #
 ncl -n plot_fields.ncl \
   grid_dir=\"$grid_dir\" \
+  input_file=\"$input_file\" \
+  fields=\"${fields}\" \
+  nlev=${nlev} \
+  fcst_index=${fcst_index} \
   res=${RES} \
   tile_inds=\(/7/\) \
   draw_tile_bdy=False \
@@ -101,36 +136,21 @@ ncl -n plot_fields.ncl \
   map_proj=\"cyln\" \
   graphics_type=\"png\"
 #
-mv ${CRES}_grid.png ${CRES}rgnl_RAP_grids_cyln.png
-
-fi
-
-
-if [ 0 = 1 ]; then
-#
-#
-ncl -n plot_grid.ncl \
-  grid_dir=\"$grid_dir\" \
-  res=${RES} \
-  tile_inds=\(/7/\) \
-  draw_tile_bdy=False \
-  draw_tile_grid=False \
-  draw_RAP_domain=True \
-  RAP_grid_fn=\"$RAP_grid_fn\" \
-  draw_RAP_bdy=True \
-  draw_RAP_grid=True \
-  map_proj=\"cyln\" \
-  subreg=\(/-130,-60,25,55/\) \
-  graphics_type=\"png\"
-#
-mv ${CRES}_grid.png ${CRES}rgnl_RAP_grids_cyln_subreg_CONUS.png
 
 fi
 
 if [ 0 = 1 ]; then
+#
+# Show FV3 regional domain (tile 7) and the original RAP domain outline
+# on a global cylindrical projection (no subregion). Drawing tile boundary
+# and grid makes a black tile 7 over CONUS.
 #
 ncl -n plot_fields.ncl \
   grid_dir=\"$grid_dir\" \
+  input_file=\"$input_file\" \
+  fields=\"${fields}\" \
+  nlev=${nlev} \
+  fcst_index=${fcst_index} \
   res=${RES} \
   tile_inds=\(/7/\) \
   draw_tile_bdy=True \
@@ -147,17 +167,20 @@ ncl -n plot_fields.ncl \
 #  subreg=\(/-150,-40,0,70/\) \
 #  subreg=\(/-120,-70,20,70/\) \
 #
-#mv ${CRES}_grid.png ${CRES}rgnl_RAP_grids_cyln_subreg.png
-mv ${CRES}_grid.png ${CRES}rgnl_RAP_grids_cyln.png
 
 fi
 
-
-
 if [ 1 = 1 ]; then
+#
+# Show FV3 regional domain (tile 7) and the original RAP domain outline
+# on a subregional cylindrical projection centered over CONUS.
 #
 ncl -n plot_fields.ncl \
   grid_dir=\"$grid_dir\" \
+  input_file=\"$input_file\" \
+  fields=\"${fields}\" \
+  nlev=${nlev} \
+  fcst_index=${fcst_index} \
   res=${RES} \
   tile_inds=\(/7/\) \
   draw_tile_bdy=True \
@@ -175,28 +198,5 @@ ncl -n plot_fields.ncl \
 #  subreg=\(/-150,-40,0,70/\) \
 #  subreg=\(/-120,-70,20,70/\) \
 #
-#mv ${CRES}_grid.png ${CRES}rgnl_RAP_fields_cyln_subreg.png
-mv ${CRES}_grid.png ${CRES}rgnl_RAP_fields_cyln.png
 
 fi
-
-if [ 0 = 1 ]; then
-ncl -n plot_grid.ncl \
-    'grid_dir="./some_dir/grid"' \
-    'res=96' \
-    'tile_inds=(/1,2,3/)' \
-    'draw_tile_bdy=True' \
-    'draw_tile_grid=True' \
-    'draw_RAP_domain=True' \
-    'RAP_grid_fn="./some_dir/RAP_grid.nc"' \
-    'draw_RAP_bdy=True' \
-    'draw_RAP_grid=True' \
-    'map_proj="cyln"' \
-    'map_proj_ctr=(/0,90/)' \
-    'subreg=(/-30,30,-25,25/)' \
-    'graphics_type="ncgm"'
-fi
-
-
-
-
