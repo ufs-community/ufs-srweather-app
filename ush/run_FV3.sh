c07183d1272729a7e1c1303d6b039ac4500fe223
Jeff Beck <Jeff.Beck@noaa.gov>
2018-08-17 21:51:00 +0000

    Changes to run_FV3.sh

--- ush/run_FV3.sh
+++ ush/run_FV3.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+module purge
+module load intel/16.1.150 impi/5.1.1.109 netcdf/4.3.0
+module use /scratch4/NCEPDEV/nems/noscrub/emc.nemspara/soft/modulefiles
+
+module list
+
+ulimit -s unlimited
+ulimit -a
+
 #Source variables from user-defined file
 . ${BASEDIR}/fv3gfs/ush/setup_grid_orog_ICs_BCs.sh
 
