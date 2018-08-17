d5032bf2b02893ea352472b1f383d9aa2904f595
Jeff Beck <Jeff.Beck@noaa.gov>
2018-08-17 20:26:49 +0000

    Changes to run_FV3.sh

--- ush/run_FV3.sh
+++ ush/run_FV3.sh
@@ -4,4 +4,4 @@
 . ${BASEDIR}/fv3gfs/ush/setup_grid_orog_ICs_BCs.sh
 
 #Run the run.regional script to execute FV3
-${BASEDIR}/fv3gfs/run_dirs/${subdir_name}/run.regional
+${BASEDIR}/run_dirs/${subdir_name}/run.regional
