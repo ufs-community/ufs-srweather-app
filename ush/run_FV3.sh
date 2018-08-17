dd2577e147ea42791839b3e0fd18e08bafa91cd8
Jeff Beck <Jeff.Beck@noaa.gov>
2018-08-17 18:39:19 +0000

    Added run_FV3.sh

---
+++ ush/run_FV3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Source variables from user-defined file
+. ${BASEDIR}/fv3gfs/ush/setup_grid_orog_ICs_BCs.sh
+
+#Run the run.regional script to execute FV3
+${BASEDIR}/fv3gfs/run_dirs/${subdir_name}/run.regional
