e5f8c5ed1fd4f6ff25e27ef49c2bea924b7eee5a
gerard ketefian <gerard.ketefian@noaa.gov>
2019-05-15 15:19:00 -0600

    New scripts for running chgres_cube to generate ICs and LBCs from HRRRX and RAPX.  Not yet finalized.
    Former-commit-id: 508df416d4647d88bce73e7aef2c80bcdd1fcb16

---
+++ ush/make_ICS_surf_LBC0.sh
@@ -0,0 +1,334 @@
+#!/bin/bash -l
+#
+#-----------------------------------------------------------------------
+#
+# This script generates:
+#
+# 1) A NetCDF initial condition (IC) file on a regional grid for the
+#    date/time on which the analysis files in the directory specified by
+#    INIDIR are valid.  Note that this file does not include data in the
+#    halo of this regional grid (that data is found in the boundary con-
+#    dition (BC) files).
+#
+# 2) A NetCDF surface file on the regional grid.  As with the IC file,
+#    this file does not include data in the halo.
+#
+# 3) A NetCDF boundary condition (BC) file containing data on the halo
+#    of the regional grid at the initial time (i.e. at the same time as
+#    the one at which the IC file is valid).
+#
+# 4) A NetCDF "control" file named gfs_ctrl.nc that contains infor-
+#    mation on the vertical coordinate and the number of tracers for
+#    which initial and boundary conditions are provided.
+#
+# All four of these NetCDF files are placed in the directory specified
+# by WORKDIR_ICSLBCS_CDATE, defined as
+#
+#   WORKDIR_ICSLBCS_CDATE="$WORKDIR_ICSLBCS/$CDATE"
+#
+# where CDATE is the externally specified starting date and cycle hour
+# of the current forecast.
+#
+#-----------------------------------------------------------------------
+
+#
+#-----------------------------------------------------------------------
+#
+# Source the variable definitions script.
+#
+#-----------------------------------------------------------------------
+#
+. $SCRIPT_VAR_DEFNS_FP
+#
+#-----------------------------------------------------------------------
+#
+# Source function definition files.
+#
+#-----------------------------------------------------------------------
+#
+. $USHDIR/source_funcs.sh
+#
+#-----------------------------------------------------------------------
+#
+# Save current shell options (in a global array).  Then set new options
+# for this script/function.
+#
+#-----------------------------------------------------------------------
+#
+{ save_shell_opts; set -u -x; } > /dev/null 2>&1
+#
+#-----------------------------------------------------------------------
+#
+# Set the name of and create the directory in which the output from this
+# script will be placed (if it doesn't already exist).
+#
+#-----------------------------------------------------------------------
+#
+WORKDIR_ICSLBCS_CDATE="$WORKDIR_ICSLBCS/$CDATE"
+WORKDIR_ICSLBCS_CDATE_ICSSURF_WORK="$WORKDIR_ICSLBCS_CDATE/ICSSURF_work"
+mkdir_vrfy -p "$WORKDIR_ICSLBCS_CDATE_ICSSURF_WORK"
+cd ${WORKDIR_ICSLBCS_CDATE_ICSSURF_WORK}
+#
+#-----------------------------------------------------------------------
+#
+# Load modules and set machine-dependent parameters.
+#
+#-----------------------------------------------------------------------
+#
+case $MACHINE in
+#
+"WCOSS_C")
+#
+  { save_shell_opts; set +x; } > /dev/null 2>&1
+
+  { restore_shell_opts; } > /dev/null 2>&1
+  ;;
+#
+"WCOSS")
+#
+  { save_shell_opts; set +x; } > /dev/null 2>&1
+
+  { restore_shell_opts; } > /dev/null 2>&1
+  ;;
+#
+"DELL")
+#
+  { save_shell_opts; set +x; } > /dev/null 2>&1
+
+  { restore_shell_opts; } > /dev/null 2>&1
+  ;;
+#
+"THEIA")
+#
+  { save_shell_opts; set +x; } > /dev/null 2>&1
+
+   ulimit -s unlimited
+   ulimit -a
+
+   module purge
+   module load intel/18.1.163
+   module load impi/5.1.1.109
+   module load netcdf/4.3.0
+   module load hdf5/1.8.14
+   module load wgrib2/2.0.8
+
+   APRUN="srun"
+
+  { restore_shell_opts; } > /dev/null 2>&1
+  ;;
+#
+"JET")
+#
+  { save_shell_opts; set +x; } > /dev/null 2>&1
+
+  { restore_shell_opts; } > /dev/null 2>&1
+  ;;
+#
+"ODIN")
+#
+  ;;
+#
+"CHEYENNE")
+#
+  ;;
+#
+esac
+#
+#-----------------------------------------------------------------------
+#
+# Create links to the grid and orography files with 4 halo cells.  These
+# are needed by chgres_cube to create the boundary data.
+#
+#-----------------------------------------------------------------------
+#
+# Are these still needed for chgres_cube?
+#export HALO=${nh4_T7}
+#
+#ln_vrfy -sf $WORKDIR_SHVE/${CRES}_grid.tile7.halo${HALO}.nc \
+#            $WORKDIR_SHVE/${CRES}_grid.tile7.nc
+#
+#ln_vrfy -sf $WORKDIR_SHVE/${CRES}_oro_data.tile7.halo${HALO}.nc \
+#            $WORKDIR_SHVE/${CRES}_oro_data.tile7.nc
+#
+#-----------------------------------------------------------------------
+#
+# Find the directory in which the wgrib2 executable is located.
+#
+#-----------------------------------------------------------------------
+#
+WGRIB2_DIR=$( which wgrib2 ) || print_err_msg_exit "\
+Directory in which the wgrib2 executable is located not found:
+  WGRIB2_DIR = \"${WGRIB2_DIR}\"
+"
+#
+#-----------------------------------------------------------------------
+#
+# Set the directory containing the external model output files.
+#
+#-----------------------------------------------------------------------
+#
+EXTRN_MDL_FILES_DIR="${EXTRN_MDL_FILES_BASEDIR_ICSSURF}/${CDATE}"
+#
+#-----------------------------------------------------------------------
+#
+# Source the file (generated by a previous task) that contains variable
+# definitions (e.g. forecast hours, file and directory names, etc) re-
+# lated to the exteranl model run that is providing fields from which
+# we will generate LBC files for the FV3SAR.
+#
+#-----------------------------------------------------------------------
+#
+. ${EXTRN_MDL_FILES_DIR}/${EXTRN_MDL_INFO_FN}
+#
+#-----------------------------------------------------------------------
+#
+# Get the name of the external model to use in the chgres FORTRAN name-
+# list file.
+#
+#-----------------------------------------------------------------------
+#
+case "$EXTRN_MDL_NAME_ICSSURF" in
+#
+"GFS")
+  external_model="GFS"
+  ;;
+"RAPX")
+  external_model="RAP"
+  ;;
+"HRRRX")
+  external_model="HRRR"
+  ;;
+*)
+  print_err_msg_exit "\
+The external model name to use in the chgres FORTRAN namelist file is 
+not specified for this external model:
+  EXTRN_MDL_NAME_ICSSURF = \"${EXTRN_MDL_NAME_ICSSURF}\"
+"
+  ;;
+#
+esac
+#
+#-----------------------------------------------------------------------
+#
+# Get the name of the physics suite to use in the chgres FORTRAN name-
+# list file.
+#
+#-----------------------------------------------------------------------
+#
+case "$CCPP_phys_suite" in
+#
+"GFS")
+  phys_suite="GFS"
+  ;;
+"GSD")
+  phys_suite="GSD"
+  ;;
+*)
+  print_err_msg_exit "\
+The physics suite name to use in the chgres FORTRAN namelist file is not
+specified for this physics suite:
+  CCPP_phys_suite = \"${CCPP_phys_suite}\"
+"
+  ;;
+#
+esac
+#
+#-----------------------------------------------------------------------
+#
+# Get the starting year, month, day, and hour of the the external model
+# run.
+#
+#-----------------------------------------------------------------------
+#
+#yyyy="${EXTRN_MDL_CDATE:0:4}"
+mm="${EXTRN_MDL_CDATE:4:2}"
+dd="${EXTRN_MDL_CDATE:6:2}"
+hh="${EXTRN_MDL_CDATE:8:2}"
+#yyyymmdd="${EXTRN_MDL_CDATE:0:8}"
+#
+#-----------------------------------------------------------------------
+#
+# Set the name of the file ???
+# What to do about GFS?  Which file (atmanl, sfcanl, nstanl) contains the input grid definition?
+#
+#-----------------------------------------------------------------------
+#
+fn="${EXTRN_MDL_FNS[0]}"
+#
+#-----------------------------------------------------------------------
+#
+# Build the FORTRAN namelist file that chgres_cube will read in.
+#
+#-----------------------------------------------------------------------
+#
+cat > fort.41 <<EOF
+&config
+ fix_dir_target_grid="${BASEDIR}/JP_grid_HRRR_like_fix_files_chgres_cube"
+ mosaic_file_target_grid="${EXPTDIR}/INPUT/${CRES}_mosaic.nc"
+ orog_dir_target_grid="${EXPTDIR}/INPUT"
+ orog_files_target_grid="${CRES}_oro_data.tile7.halo${nh4_T7}.nc"
+ vcoord_file_target_grid="${FV3SAR_DIR}/fix/fix_am/global_hyblev.l64.txt"
+ mosaic_file_input_grid=""
+ orog_dir_input_grid=""
+ base_install_dir="${SORCDIR}/chgres_cube.fd"
+ wgrib2_path="${WGRIB2_DIR}"
+ data_dir_input_grid="${EXTRN_MDL_FILES_DIR}"
+ grib2_file_input_grid="${fn}"
+ cycle_mon=${mm}
+ cycle_day=${dd}
+ cycle_hour=${hh}
+ convert_atm=.true.
+ convert_sfc=.true.
+ convert_nst=.false.
+ regional=1
+ input_type="grib2"
+ external_model="${external_model}"
+ phys_suite="${phys_suite}"
+/
+EOF
+#
+#-----------------------------------------------------------------------
+#
+# Run chgres_cube.
+#
+#-----------------------------------------------------------------------
+#
+${APRUN} ${exec_dir}/global_chgres.exe || print_err_msg_exit "\
+Call to executable to generate surface and initial conditions files for
+the FV3SAR failed:
+  EXTRN_MDL_FILES_DIR = \"${EXTRN_MDL_FILES_DIR}\"
+  fn = \"$fn\"
+"
+#
+#-----------------------------------------------------------------------
+#--------------------------------------------------------------
+#
+# Move surface, control, and boundary file to ICs_BCs directory 
+#
+#-----------------------------------------------------------------------
+#
+mv_vrfy gfs_bndy.nc ${WORKDIR_ICSLBCS_CDATE}/gfs_bndy.tile7.000.nc
+mv_vrfy gfs_ctrl.nc ${WORKDIR_ICSLBCS_CDATE}/
+# The surface file name will be changed to say "tile7" at some point.
+mv_vrfy out.sfc.tile1.nc ${WORKDIR_ICSLBCS_CDATE}/sfc_data.tile7.nc
+#
+#-----------------------------------------------------------------------
+#
+# Print message indicating successful completion of script.
+#
+#-----------------------------------------------------------------------
+#
+print_info_msg "\
+
+========================================================================
+Surface and initial condition (IC) files generated successfully!!!
+========================================================================"
+#
+#-----------------------------------------------------------------------
+#
+# Restore the shell options saved at the beginning of this script/func-
+# tion.
+#
+#-----------------------------------------------------------------------
+#
+{ restore_shell_opts; } > /dev/null 2>&1
