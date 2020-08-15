37140a24056bd8ee49996618ce41548e0ac483f1
jeff beck <jeff.beck@noaa.gov>
2020-08-15 00:05:36 +0000

    Remove builds for utilities previously in regional_workflow

--- src/build_UFS_UTILS.sh
+++ src/build_UFS_UTILS.sh
@@ -4,10 +4,6 @@
 # Description: Builds chgres, chgres_cube, orog, fre-nctools, sfc_climo_gen,
 #              regional_grid, global_equiv_resol, and mosaic_file.
 #
-#              Note:  The global_equiv_resol, mosaic_file and regional_grid 
-#              reside in the ../regional_workflow/sorc directory and are
-#              cloned with that repository.
-#
 # Usage: ./build_UFS_UTILS.sh
 #
 #==========================================================================
@@ -44,52 +40,7 @@ cd $UFS_UTILS
 if [ $? -eq 0 ] ; then
   echo "UFS_UTILS build SUCCEEDED"
 else
-  echo "UFS_UTILS build FAILED see $logs_dir/build_regional_grid.log"
-  exit 1
-fi
-}
-
-#------------------------------------
-# build regional_grid
-#------------------------------------
-$Build_regional_grid && {
-echo " .... Building regional_grid .... "
-cd $build_dir
-./build_regional_grid.sh > $logs_dir/build_regional_grid.log 2>&1
-if [ $? -eq 0 ] ; then
-  echo "regional_grid build SUCCEEDED"
-else
-  echo "regional_grid build FAILED see $logs_dir/build_regional_grid.log"
-  exit 1
-fi
-}
-
-#------------------------------------
-# build global_equiv_resol
-#------------------------------------
-$Build_global_equiv_resol && {
-echo " .... Building global_equiv_resol .... "
-cd $build_dir
-./build_global_equiv_resol.sh > $logs_dir/build_global_equiv_resol.log 2>&1
-if [ $? -eq 0 ] ; then
-  echo "global_equiv_resol build SUCCEEDED"
-else
-  echo "global_equiv_resol build FAILED see $logs_dir/build_global_equiv_resol.log"
-  exit 1
-fi
-}
-
-#------------------------------------
-# build mosaic file
-#------------------------------------
-$Build_mosaic_file && {
-echo " .... Building mosaic_file .... "
-cd $build_dir
-./build_mosaic_file.sh > $logs_dir/build_mosaic_file.log 2>&1
-if [ $? -eq 0 ] ; then
-  echo "mosaic_file build SUCCEEDED"
-else
-  echo "mosaic_file build FAILED see $logs_dir/build_mosaic.log"
+  echo "UFS_UTILS build FAILED see $logs_dir/build_UFS_UTILS.log"
   exit 1
 fi
 }
