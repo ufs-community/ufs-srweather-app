da87a570802737c7855bedc46f98e849243ce925
JulieSchramm <schramm@ucar.edu>
2020-10-29 14:59:19 -0600

    Release/public v1 Add Introduction to User's Guide (#46)
    * Change branch of regional_workflow from develop to release/public-v1.
    
    * Add "-j 4" to make for a considerable speed-up in the build.
    
    * src/machine-setup.sh: don't purge on gaea
    
    * - Add RST version of Introduction to UFS SRW App User's Guide
    - Add sphinx_rtd_theme to requirements.txt and conf.py
    
    Both html and pdf builds succeed.

--- docs/UsersGuide/source/conf.py
+++ docs/UsersGuide/source/conf.py
@@ -78,7 +78,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -86,7 +86,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -122,6 +123,7 @@ htmlhelp_basename = 'UFS-SR-Weather-App'
 
 # -- Options for LaTeX output ------------------------------------------------
 
+latex_engine = 'pdflatex'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
       'papersize': 'letterpaper',
