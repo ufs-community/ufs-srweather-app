4b2b639e677a134e7979236baf654dc9d385c696
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2021-05-28 13:52:36 +0000

    Change dir structure

--- conf_cmaq/update_conf.sh
+++ conf_cmaq/update_conf.sh
@@ -14,14 +14,16 @@ COMPILER="intel"
 
 #cd to location of script
 MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+SCRIPT_DIR="${MYDIR}/${FCST_MODEL}"
+ORG_DIR="${MYDIR}/.."
 
 echo "... update config and env ..."
 # External components
-cp ${MYDIR}/${FCST_MODEL}_Externals.cfg ${MYDIR}/../Externals.cfg
+cp ${SCRIPT_DIR}/${FCST_MODEL}_Externals.cfg ${ORG_DIR}/Externals.cfg
 # CMakeLists in src
-cp ${MYDIR}/${FCST_MODEL}_src_CMakeLists.txt ${MYDIR}/../src/CMakeLists.txt
+cp ${SCRIPT_DIR}/${FCST_MODEL}_src_CMakeLists.txt ${ORG_DIR}/src/CMakeLists.txt
 # Build environment file for UFS_UTILS, arl_nexus, emc_post
-cp ${MYDIR}/${FCST_MODEL}_build_${MACHINE}_${COMPILER}.env ${MYDIR}/../env/build_${MACHINE}_${COMPILER}.env
+cp ${SCRIPT_DIR}/${FCST_MODEL}_build_${MACHINE}_${COMPILER}.env ${ORG_DIR}/env/build_${MACHINE}_${COMPILER}.env
 
 
 exit 0
