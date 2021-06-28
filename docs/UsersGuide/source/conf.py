c37069a5b3563814ae8ef6aa5be56afb228e569d
Julie Schramm <schramm@ucar.edu>
2021-06-28 11:08:35 -0600

    Replace deprecated add_stylesheet with add_css_file to fix error: AttributeError: 'Sphinx' object has no attribute 'add_stylesheet'

--- docs/UsersGuide/source/conf.py
+++ docs/UsersGuide/source/conf.py
@@ -110,7 +110,7 @@ html_context = {
      }
 
 def setup(app):
-    app.add_stylesheet('custom.css')  # may also be an URL
+    app.add_css_file('custom.css')  # may also be an URL
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
