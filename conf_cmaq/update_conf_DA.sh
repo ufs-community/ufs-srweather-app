e3cf2476900c8c4b05d139c487a8eb2144ae389d
chan-hoo <chan-hoo.jeon@noaa.gov>
2021-09-13 14:37:21 +0000

    Add DA file

---
+++ conf_cmaq/update_conf_DA.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eu
+
+##### User's choice ##########
+
+FCST_MODEL="fv3gfs_aqm"
+MACHINE="wcoss_dell_p3"
+COMPILER="intel"
+
+##############################
+
+### ========================================================================
+
+#cd to location of script
+MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+SCRIPT_DIR="${MYDIR}/${FCST_MODEL}"
+ORG_DIR="${MYDIR}/.."
+
+echo "... update config and env ..."
+# External components
+cp ${SCRIPT_DIR}/${FCST_MODEL}_Externals_DA.cfg ${ORG_DIR}/Externals.cfg
+# CMakeLists in src
+cp ${SCRIPT_DIR}/${FCST_MODEL}_src_CMakeLists_DA.txt ${ORG_DIR}/src/CMakeLists.txt
+# Build environment file for components
+cp ${SCRIPT_DIR}/${FCST_MODEL}_build_${MACHINE}_${COMPILER}_DA.env ${ORG_DIR}/env/build_${MACHINE}_${COMPILER}.env
+
+
+exit 0
