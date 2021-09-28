7818213e3f2be9a97843f0daeaa545d2c144d795
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2021-09-28 10:48:20 +0000

    make DA selectable

--- conf_cmaq/update_conf_DA.sh
+++ conf_cmaq/update_conf_DA.sh
@@ -8,21 +8,30 @@ FCST_MODEL="fv3gfs_aqm"
 MACHINE="hera"
 COMPILER="intel"
 
+DA_opt="YES"
 ##############################
 
 ### ========================================================================
 
-#cd to location of script
+# cd to location of script
 MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 SCRIPT_DIR="${MYDIR}/${FCST_MODEL}"
 ORG_DIR="${MYDIR}/.."
 
+# Suffix for DA 
+if ["${DA_opt}"="YES"] || ["${DA_opt}"="yes"]; then
+  DA_add="_DA"
+  echo "... ## !!! for DA !!! ## ..."
+else
+  DA_add=""
+  echo "... ## !!! for free-forecast (non-DA) !!! ## ..."
+fi 
+
 echo "... update config and env ..."
-echo "... ## !!! for DA !!! ## ..."
 # External components
-cp ${SCRIPT_DIR}/${FCST_MODEL}_Externals_DA.cfg ${ORG_DIR}/Externals.cfg
+cp ${SCRIPT_DIR}/${FCST_MODEL}_Externals${DA_add}.cfg ${ORG_DIR}/Externals.cfg
 # CMakeLists in src
-cp ${SCRIPT_DIR}/${FCST_MODEL}_src_CMakeLists_DA.txt ${ORG_DIR}/src/CMakeLists.txt
+cp ${SCRIPT_DIR}/${FCST_MODEL}_src_CMakeLists${DA_add}.txt ${ORG_DIR}/src/CMakeLists.txt
 # Build environment file for components
 cp ${SCRIPT_DIR}/${FCST_MODEL}_build_${MACHINE}_${COMPILER}.env ${ORG_DIR}/env/build_${MACHINE}_${COMPILER}.env
 
