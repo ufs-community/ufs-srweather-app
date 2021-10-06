792bb401766467670337c40e48d01990b0cc4ae6
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2021-10-06 15:34:15 +0000

    Remove unnecessary GSI executables

--- conf_cmaq/fv3gfs_aqm/build_GSI_hera.sh
+++ conf_cmaq/fv3gfs_aqm/build_GSI_hera.sh
@@ -6,7 +6,7 @@ echo ${CWD}
 cd ${CWD}/../src/gsi
 ./ush/build.comgsi
 cp build/bin/gsi.x ../../bin/
-cp build/bin/enkf_wrf.x ../../bin/ 
-cp build/bin/enspreproc.x ../../bin/
+#cp build/bin/enkf_wrf.x ../../bin/ 
+#cp build/bin/enspreproc.x ../../bin/
 
 cd ../../bin
