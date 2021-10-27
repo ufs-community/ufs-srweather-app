ca778a9bc27b3e93b91918fdbb38723e026bfd51
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2021-10-27 06:34:20 -0400

    increase wtime_make_orog for RRFS NA 3km on WCOSS (#622)
    * increase wtime_make_orog for RRFS_NA_3km on wcoss
    
    * remove ppn_run_fcst from WE2E script for RRFS_NA_3km
    
    * Add EMC configuration in comment

--- ush/set_predef_grid_params.sh
+++ ush/set_predef_grid_params.sh
@@ -1227,8 +1227,8 @@ case ${PREDEF_GRID_NAME} in
 
   DT_ATMOS="${DT_ATMOS:-36}"
 
-  LAYOUT_X="${LAYOUT_X:-18}"
-  LAYOUT_Y="${LAYOUT_Y:-36}"
+  LAYOUT_X="${LAYOUT_X:-18}"   # 40 - EMC operational configuration
+  LAYOUT_Y="${LAYOUT_Y:-36}"   # 45 - EMC operational configuration
   BLOCKSIZE="${BLOCKSIZE:-28}"
 
   if [ "$QUILTING" = "TRUE" ]; then
