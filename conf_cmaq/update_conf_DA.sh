6b2382a3fd886b330605358d4ba5cad3bb8a1409
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2021-11-30 15:08:37 +0000

    Make MACHINE auto-detected

--- conf_cmaq/update_conf_DA.sh
+++ conf_cmaq/update_conf_DA.sh
@@ -2,21 +2,27 @@
 
 set -eu
 
+MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+cd ${MYDIR}
+
 ##### User's choice ##########
 
 FCST_MODEL="fv3gfs_aqm"
-MACHINE="hera"
+#MACHINE="hera"     # If set, the auto-detected MACHINE will be replaced with this value.
 COMPILER="intel"
 
 DA_opt="YES"
 ##############################
 
-### ========================================================================
+# Detect MACHINE
+source detect_machine.sh
+echo "MACHINE:" $MACHINE
 
-# cd to location of script
-MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 SCRIPT_DIR="${MYDIR}/${FCST_MODEL}"
 ORG_DIR="${MYDIR}/.."
+
+echo "FCST_MODEL:" ${FCST_MODEL}
+echo "COMPILER  :" ${COMPILER}
 
 # Suffix for DA 
 if [ "${DA_opt}" = "YES" ] || [ "${DA_opt}" = "yes" ]; then
@@ -34,6 +40,6 @@ cp ${SCRIPT_DIR}/${FCST_MODEL}_Externals${DA_add}.cfg ${ORG_DIR}/Externals.cfg
 cp ${SCRIPT_DIR}/${FCST_MODEL}_src_CMakeLists${DA_add}.txt ${ORG_DIR}/src/CMakeLists.txt
 # Build environment file for components
 cp ${SCRIPT_DIR}/${FCST_MODEL}_build_${MACHINE}_${COMPILER}.env ${ORG_DIR}/env/build_${MACHINE}_${COMPILER}.env
-
+echo "... updated ..."
 
 exit 0
