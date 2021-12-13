374963bd3083a112a456e3676cd1a768320919de
chan-hoo <chan-hoo.jeon@noaa.gov>
2021-12-13 11:55:48 +0000

    add detect_machine

--- conf_cmaq/update_conf.sh
+++ conf_cmaq/update_conf.sh
@@ -2,21 +2,26 @@
 
 set -eu
 
+MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+cd ${MYDIR}
+
 ##### User's choice ##########
 
 FCST_MODEL="fv3gfs_aqm"
-MACHINE="wcoss_dell_p3"
+#MACHINE="hera"     # If set, the auto-detected MACHINE will be replaced with this value.
 COMPILER="intel"
 
 ##############################
 
-### ========================================================================
+# Detect MACHINE
+source detect_machine.sh
+echo "MACHINE:" $MACHINE
 
-#cd to location of script
-MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 SCRIPT_DIR="${MYDIR}/${FCST_MODEL}"
 ORG_DIR="${MYDIR}/.."
 
+echo "FCST_MODEL:" ${FCST_MODEL}
+echo "COMPILER  :" ${COMPILER}
 echo "... update config and env ..."
 # External components
 cp ${SCRIPT_DIR}/${FCST_MODEL}_Externals.cfg ${ORG_DIR}/Externals.cfg
@@ -24,6 +29,6 @@ cp ${SCRIPT_DIR}/${FCST_MODEL}_Externals.cfg ${ORG_DIR}/Externals.cfg
 cp ${SCRIPT_DIR}/${FCST_MODEL}_src_CMakeLists.txt ${ORG_DIR}/src/CMakeLists.txt
 # Build environment file for UFS_UTILS, arl_nexus, emc_post
 cp ${SCRIPT_DIR}/${FCST_MODEL}_build_${MACHINE}_${COMPILER}.env ${ORG_DIR}/env/build_${MACHINE}_${COMPILER}.env
-
+echo "... updated ..."
 
 exit 0
