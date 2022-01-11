02d4fc29f1e35761f315c2ec6257d0f45732d3e4
chan-hoo <chan-hoo.jeon@noaa.gov>
2022-01-11 14:36:03 +0000

    Remove unnecessary arguments

--- devbuild.sh
+++ devbuild.sh
@@ -230,15 +230,15 @@ if [ "${VERBOSE}" = true ]; then
 fi
 
 # source the environment file for this platform/compiler combination, then build the code
-printf "... Source ENV_FILE and create BUILD directory ...\n" >&2
+printf "... Source ENV_FILE and create BUILD directory ...\n"
 module use ${SRC_DIR}/env
 . ${ENV_FILE}
 module list
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-printf "... Generate CMAKE configuration ...\n" >&2
+printf "... Generate CMAKE configuration ...\n"
 cmake ${SRC_DIR} ${CMAKE_SETTINGS} 2>&1 | tee log.cmake
-printf "... Compile executables ...\n" >&2
+printf "... Compile executables ...\n"
 make ${MAKE_SETTINGS} 2>&1 | tee log.make
 
 exit 0
