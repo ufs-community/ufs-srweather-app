a542cee424ccd8ed67fee08938566c5324007aa1
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2022-02-14 19:33:41 +0000

    Combine RRFS-CMAQ non-DA and DA scripts

--- build_RRFS.sh
+++ build_RRFS.sh
@@ -15,21 +15,25 @@ else
   readonly MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 fi
 
+###########################################################
+## User specific parameters                              ##
+###########################################################
+##
+## Compiler:
+##
+export COMPILER="intel"
+##
+## CCPP_SUITES:
+##
+export CCPP_SUITES="FV3_CPT_v0,FV3_GFS_2017_gfdlmp,FV3_GFS_2017_gfdlmp_regional,FV3_GSD_SAR,FV3_GSD_v0,FV3_GFS_v15p2,FV3_GFS_v16,FV3_RRFS_v1beta,FV3_HRRR,FV3_RRFS_v1alpha,FV3_GFS_v15_thompson_mynn_lam3km"
+##
+###########################################################
+
 # Check out the external components for RRFS
 echo "... Checking out the common external components: regional workflow, UFS_UTILS, UPP ..."
 ./manage_externals/checkout_externals -e comp_conf/COMMON/Externals.cfg
 echo "... Checking out the RRFS forecast model: ufs-weather-model ..."
 ./manage_externals/checkout_externals -e comp_conf/RRFS/Externals.cfg
-
-###########################################################
-## User specific parameters                              ##
-###########################################################
-##
-export COMPILER="intel"
-##
-export CCPP_SUITES="FV3_CPT_v0,FV3_GFS_2017_gfdlmp,FV3_GFS_2017_gfdlmp_regional,FV3_GSD_SAR,FV3_GSD_v0,FV3_GFS_v15p2,FV3_GFS_v16,FV3_RRFS_v1beta,FV3_HRRR,FV3_RRFS_v1alpha,FV3_GFS_v15_thompson_mynn_lam3km"
-##
-###########################################################
 
 # Build the external components for RRFS
 echo "... Building UFS_UTILS ..."
