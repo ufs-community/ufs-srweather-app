376b47aee0510965199274feb50c17552ac03b46
Daniel Abdi <daniel.abdi@noaa.gov>
2022-04-30 13:17:49 +0000

    Move cmake init out of lmod-setup.sh on odin

--- lmod-setup.sh
+++ lmod-setup.sh
@@ -19,10 +19,6 @@ elif [ "$L_MACHINE" = odin ]; then
    module --initial_load --no_redirect restore
    #module use <$HOME>/<your-modulefiles-dir>
    export MODULEPATH=/oldscratch/ywang/external/hpc-stack/modulefiles/mpi/intel/2020/cray-mpich/7.7.16:/oldscratch/ywang/external/hpc-stack/modulefiles/compiler/intel/2020:/oldscratch/ywang/external/hpc-stack/modulefiles/core:/oldscratch/ywang/external/hpc-stack/modulefiles/stack:/opt/cray/pe/perftools/21.02.0/modulefiles:/opt/cray/ari/modulefiles:/opt/cray/pe/craype-targets/default/modulefiles:/opt/cray/pe/modulefiles:/opt/cray/modulefiles:/opt/modulefiles
-   
-   #module purge
-   export CMAKE=/home/yunheng.wang/tools/cmake-3.23.0-rc2/bin/cmake
-   export PATH=/home/yunheng.wang/tools/cmake-3.23.0-rc2/bin:${PATH}
 else
    module purge
 fi
