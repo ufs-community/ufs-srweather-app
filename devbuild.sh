106168c376911949505355d56ea3da17e3910e53
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2022-05-22 02:58:09 +0000

    port to wcoss2

--- devbuild.sh
+++ devbuild.sh
@@ -10,7 +10,7 @@ OPTIONS
       show this help guide
   -p, --platform=PLATFORM
       name of machine you are building on
-      (e.g. cheyenne | hera | jet | orion | wcoss_dell_p3)
+      (e.g. cheyenne | hera | jet | orion | wcoss_dell_p3 | wcoss2)
   -c, --compiler=COMPILER
       compiler to use; default depends on platform
       (e.g. intel | gnu | cray | gccgfortran)
@@ -155,6 +155,7 @@ if [ -z "${COMPILER}" ] ; then
     jet|hera|gaea) COMPILER=intel ;;
     orion) COMPILER=intel ;;
     wcoss_dell_p3) COMPILER=intel ;;
+    wcoss2) COMPILER=intel ;;
     cheyenne) COMPILER=intel ;;
     macos,singularity) COMPILER=gnu ;;
     odin) COMPILER=intel ;;
