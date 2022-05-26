35d6902dcb601aebc560e129b58c7332ba984e10
EdwardSnyder-NOAA <96196752+EdwardSnyder-NOAA@users.noreply.github.com>
2022-05-26 12:45:38 -0500

    update IC/LBC file paths to match new IC/LBC file structure (#766)
    * update IC/LBC file paths to match new IC/LBC file structure
    
    * change forecast length
    
    * Update config.community.sh
    
    These changes reflects the SRW team's preference for a 12-hours forecast. The gfs.t18z.pgrb2.0p25.f012 file will need to be added to all Level 1 systems.
    
    Co-authored-by: Gillian Petro <96886803+gspetro-NOAA@users.noreply.github.com>

--- ush/config.community.sh
+++ ush/config.community.sh
@@ -15,12 +15,12 @@ DO_ENSEMBLE="FALSE"
 NUM_ENS_MEMBERS="2"
 
 CCPP_PHYS_SUITE="FV3_GFS_v16"
-FCST_LEN_HRS="48"
+FCST_LEN_HRS="12"
 LBC_SPEC_INTVL_HRS="6"
 
 DATE_FIRST_CYCL="20190615"
 DATE_LAST_CYCL="20190615"
-CYCL_HRS=( "00" )
+CYCL_HRS=( "18" )
 
 EXTRN_MDL_NAME_ICS="FV3GFS"
 EXTRN_MDL_NAME_LBCS="FV3GFS"
@@ -28,7 +28,7 @@ EXTRN_MDL_NAME_LBCS="FV3GFS"
 FV3GFS_FILE_FMT_ICS="grib2"
 FV3GFS_FILE_FMT_LBCS="grib2"
 
-WTIME_RUN_FCST="01:00:00"
+WTIME_RUN_FCST="02:00:00"
 
 MODEL="FV3_GFS_v16_CONUS_25km"
 METPLUS_PATH="path/to/METPlus"
@@ -59,8 +59,7 @@ RUN_TASK_VX_ENSPOINT="FALSE"
 # if on another platform, using other dates, other external models, etc.
 # Uncomment the following EXTRN_MDL_*_ICS/LBCS only when USE_USER_STAGED_EXTRN_FILES=TRUE
 #
-#EXTRN_MDL_SOURCE_BASEDIR_ICS="/scratch2/BMC/det/UFS_SRW_app/v1p0/model_data/FV3GFS"
-#EXTRN_MDL_FILES_ICS=( "gfs.pgrb2.0p25.f000" )
-#EXTRN_MDL_SOURCE_BASEDIR_LBCS="/path/to/model_data/FV3GFS"
-#EXTRN_MDL_FILES_LBCS=( "gfs.pgrb2.0p25.f006" "gfs.pgrb2.0p25.f012" "gfs.pgrb2.0p25.f018" "gfs.pgrb2.0p25.f024" \
-#                       "gfs.pgrb2.0p25.f030" "gfs.pgrb2.0p25.f036" "gfs.pgrb2.0p25.f042" "gfs.pgrb2.0p25.f048" )
+#EXTRN_MDL_SOURCE_BASEDIR_ICS="/scratch2/BMC/det/UFS_SRW_App/develop/input_model_data/FV3GFS/grib2/2019061518"
+#EXTRN_MDL_FILES_ICS=( "gfs.t18z.pgrb2.0p25.f000" )
+#EXTRN_MDL_SOURCE_BASEDIR_LBCS="/scratch2/BMC/det/UFS_SRW_App/develop/input_model_data/FV3GFS/grib2/2019061518"
+#EXTRN_MDL_FILES_LBCS=( "gfs.t18z.pgrb2.0p25.f006" "gfs.t18z.pgrb2.0p25.f012")
