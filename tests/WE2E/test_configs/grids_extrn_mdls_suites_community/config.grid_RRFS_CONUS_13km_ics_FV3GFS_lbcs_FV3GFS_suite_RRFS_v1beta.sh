dacf7c6ceb2ec917341514161e5e63126371edd8
EdwardSnyder-NOAA <96196752+EdwardSnyder-NOAA@users.noreply.github.com>
2022-06-10 16:48:16 -0500

    adding new e2e tests (#792)

---
+++ tests/WE2E/test_configs/grids_extrn_mdls_suites_community/config.grid_RRFS_CONUS_13km_ics_FV3GFS_lbcs_FV3GFS_suite_RRFS_v1beta.sh
@@ -0,0 +1,25 @@
+#
+# TEST PURPOSE/DESCRIPTION:
+# ------------------------
+#
+# This test is to ensure that the workflow running in community mode 
+# completes successfully on the RRFS_CONUS_13km grid using the RRFS_v1beta
+# physics suite with ICs and LBCs derived from the FV3GFS.
+#
+
+RUN_ENVIR="community"
+PREEXISTING_DIR_METHOD="rename"
+
+PREDEF_GRID_NAME="RRFS_CONUS_13km"
+CCPP_PHYS_SUITE="FV3_RRFS_v1beta"
+
+EXTRN_MDL_NAME_ICS="FV3GFS"
+EXTRN_MDL_NAME_LBCS="FV3GFS"
+USE_USER_STAGED_EXTRN_FILES="TRUE"
+
+DATE_FIRST_CYCL="20190701"
+DATE_LAST_CYCL="20190701"
+CYCL_HRS=( "00" )
+
+FCST_LEN_HRS="6"
+LBC_SPEC_INTVL_HRS="3"
