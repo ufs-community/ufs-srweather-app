d2a2e47c96a69ae774c816899dcd4ca97f7ff25d
Christopher Harrop <Christopher.W.Harrop@noaa.gov>
2022-06-16 11:50:14 -0600

    Update ncio version to get bug fixes and update build test script

--- test/build.sh
+++ test/build.sh
@@ -75,7 +75,7 @@ cd ${TOP_DIR}
 
 ENV_DIR=${TOP_DIR}/env
 #-----------------------------------------------------------------------
-# Array of all executables built
+# Array of all required executables built
 #-----------------------------------------------------------------------
 declare -a executables_created=( chgres_cube \
                                  emcsfc_ice_blend \
@@ -85,6 +85,8 @@ declare -a executables_created=( chgres_cube \
                                  fvcom_to_FV3 \
                                  global_cycle \
                                  global_equiv_resol \
+                                 inland \
+                                 lakefrac \
                                  make_hgrid \
                                  make_solo_mosaic \
                                  upp.x \
@@ -96,6 +98,38 @@ declare -a executables_created=( chgres_cube \
                                  ufs_model \
                                  vcoord_gen )
 
+
+#-----------------------------------------------------------------------
+# Array of all optional GSI executables built
+#-----------------------------------------------------------------------
+declare -a executables_gsi_created=( enkf.x \
+                                     gsi.x \
+                                     nc_diag_cat.x \
+                                     ncdiag_cat_serial.x \
+                                     test_nc_unlimdims.x )
+
+#-----------------------------------------------------------------------
+# Array of all optional rrfs_utl executables built
+#-----------------------------------------------------------------------
+declare -a executables_rrfs_utl_created=( adjust_soiltq.exe \
+                                          check_imssnow_fv3lam.exe \
+                                          fv3lam_nonvarcldana.exe \
+                                          gen_annual_maxmin_GVF.exe \
+                                          gen_cs.exe \
+                                          gen_ensmean_recenter.exe \
+                                          lakesurgery.exe \
+                                          process_imssnow_fv3lam.exe \
+                                          process_larccld.exe \
+                                          process_Lightning.exe \
+                                          process_metarcld.exe \
+                                          process_NSSL_mosaic.exe \
+                                          process_updatesst.exe \
+                                          ref2tten.exe \
+                                          update_bc.exe \
+                                          update_GVF.exe \
+                                          update_ice.exe \
+                                          use_raphrrr_sfc.exe )
+
 #-----------------------------------------------------------------------
 # Set up the build environment and run the build script.
 #-----------------------------------------------------------------------
@@ -105,14 +139,14 @@ declare -a executables_created=( chgres_cube \
     EXEC_DIR=${BIN_DIR}/bin
     if [ $build_it -eq 0 ] ; then
       ./devbuild.sh --platform=${machine} --compiler=${compiler} --build-dir=${BUILD_DIR} --install-dir=${BIN_DIR} \
-        --clean || fail "Build ${machine} ${compiler} FAILED"
+        --clean --rrfs || fail "Build ${machine} ${compiler} FAILED"
     fi    # End of skip build for testing
 
   #-----------------------------------------------------------------------
   # check for existence of executables.
   #-----------------------------------------------------------------------
   n_fail=0
-  for file in "${executables_created[@]}" ; do
+  for file in "${executables_created[@]} ${executables_gsi_created[@]} ${executables_rrfs_utl_created[@]}" ; do
     exec_file=${EXEC_DIR}/${file}
     if [ -f ${exec_file} ]; then
       echo "SUCCEED: ${compiler} executable file ${exec_file} exists" >> ${TEST_OUTPUT}
