e32859517985bb6a6b3bde59e36edb24ec37a3a9
EdwardSnyder-NOAA <96196752+EdwardSnyder-NOAA@users.noreply.github.com>
2022-07-01 12:04:56 -0500

    [develop] Add MET/METplus to Jet (#799)
    * add metplus paths
    
    * add run_vx.local for jet
    
    Co-authored-by: Edward Snyder <Edward.Snyder@noaa.com>

--- ush/machine/jet.sh
+++ ush/machine/jet.sh
@@ -81,6 +81,14 @@ RUN_CMD_UTILS="srun"
 RUN_CMD_FCST="srun"
 RUN_CMD_POST="srun"
 
+# MET/METplus-Related Paths
+MET_INSTALL_DIR=${MET_INSTALL_DIR:-"/contrib/met/10.1.1"}
+METPLUS_PATH=${METPLUS_PATH:-"/contrib/met/METplus/METplus-4.1.1"}
+CCPA_OBS_DIR=${CCPA_OBS_DIR:-"${staged_data_dir}/obs_data/ccpa/proc"}
+MRMS_OBS_DIR=${MRMS_OBS_DIR:-"${staged_data_dir}/obs_data/mrms/proc"}
+NDAS_OBS_DIR=${NDAS_OBS_DIR:-"${staged_data_dir}/obs_data/ndas/proc"}
+MET_BIN_EXEC=${MET_BIN_EXEC:-"bin"}
+
 # Test Data Locations
 TEST_COMIN="${staged_data_dir}/COMGFS"
 TEST_PREGEN_BASEDIR="${staged_data_dir}/FV3LAM_pregen"
