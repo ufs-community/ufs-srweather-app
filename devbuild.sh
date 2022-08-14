643437fb30cf87ce9249caeef776b27d506c95b3
Daniel Abdi <daniel.abdi@noaa.gov>
2022-08-14 02:03:38 +0000

    Add separate building of rrfs components.

--- devbuild.sh
+++ devbuild.sh
@@ -3,7 +3,7 @@
 # usage instructions
 usage () {
 cat << EOF_USAGE
-Usage: $0 --platform=PLATFORM [OPTIONS]...
+Usage: $0 --platform=PLATFORM [OPTIONS]... [TARGET]
 
 OPTIONS
   -h, --help
@@ -19,8 +19,6 @@ OPTIONS
       (e.g. ATM | ATMW | S2S | S2SW)
   --ccpp="CCPP_SUITE1,CCPP_SUITE2..."
       CCPP suites (CCPP_SUITES) to include in build; delimited with ','
-  --rrfs
-      build rrfs system components: GSI, rrfs_utl
   --enable-options="OPTION1,OPTION2,..."
       enable ufs-weather-model options; delimited with ','
       (e.g. 32BIT | INLINE_POST | UFS_GOCART | MOM6 | CICE6 | WW3 | CMEPS)
@@ -35,6 +33,8 @@ OPTIONS
       build directory
   --install-dir=INSTALL_DIR
       installation prefix
+  --bin-dir=BIN_DIR
+      installation binary directory
   --build-type=BUILD_TYPE
       build type; defaults to RELEASE
       (e.g. DEBUG | RELEASE | RELWITHDEBINFO)
@@ -42,6 +42,10 @@ OPTIONS
       number of build jobs; defaults to 4
   -v, --verbose
       build with verbose output
+
+TARGETS
+   all = builds the default list of apps (also not passing any target does the same)
+   Or any combinations of (ufs, ufs_utils, upp, gsi, rrfs_utils)
 
 NOTE: This script is for internal developer use only;
 See User's Guide for detailed build instructions
@@ -57,6 +61,7 @@ Settings:
   SRW_DIR=${SRW_DIR}
   BUILD_DIR=${BUILD_DIR}
   INSTALL_DIR=${INSTALL_DIR}
+  BIN_DIR=${BIN_DIR}
   PLATFORM=${PLATFORM}
   COMPILER=${COMPILER}
   APP=${APPLICATION}
@@ -85,10 +90,10 @@ SRW_DIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 MACHINE_SETUP=${SRW_DIR}/src/UFS_UTILS/sorc/machine-setup.sh
 BUILD_DIR="${SRW_DIR}/build"
 INSTALL_DIR=${SRW_DIR}
+BIN_DIR="exec"
 COMPILER=""
 APPLICATION=""
 CCPP_SUITES=""
-RRFS="off"
 ENABLE_OPTIONS=""
 DISABLE_OPTIONS=""
 BUILD_TYPE="RELEASE"
@@ -96,6 +101,14 @@ BUILD_JOBS=4
 CLEAN=false
 CONTINUE=false
 VERBOSE=false
+
+#Turn off all apps to build and choose default later
+DEFAULT_BUILD=true 
+BUILD_UFS="off"
+BUILD_UFS_UTILS="off"
+BUILD_UPP="off"
+BUILD_GSI="off"
+BUILD_RRFS_UTILS="off"
 
 # process required arguments
 if [[ ("$1" == "--help") || ("$1" == "-h") ]]; then
@@ -115,8 +128,6 @@ while :; do
     --app|--app=|-a|-a=) usage_error "$1 requires argument." ;;
     --ccpp=?*) CCPP_SUITES=${1#*=} ;;
     --ccpp|--ccpp=) usage_error "$1 requires argument." ;;
-    --rrfs) RRFS="on" ;;
-    --rrfs=*) usage_error "$1 argument ignored." ;;
     --enable-options=?*) ENABLE_OPTIONS=${1#*=} ;;
     --enable-options|--enable-options=) usage_error "$1 requires argument." ;;
     --disable-options=?*) DISABLE_OPTIONS=${1#*=} ;;
@@ -129,17 +140,36 @@ while :; do
     --build-dir|--build-dir=) usage_error "$1 requires argument." ;;
     --install-dir=?*) INSTALL_DIR=${1#*=} ;;
     --install-dir|--install-dir=) usage_error "$1 requires argument." ;;
+    --bin-dir=?*) BIN_DIR=${1#*=} ;;
+    --bin-dir|--bin-dir=) usage_error "$1 requires argument." ;;
     --build-type=?*) BUILD_TYPE=${1#*=} ;;
     --build-type|--build-type=) usage_error "$1 requires argument." ;;
     --build-jobs=?*) BUILD_JOBS=$((${1#*=})) ;;
     --build-jobs|--build-jobs=) usage_error "$1 requires argument." ;;
     --verbose|-v) VERBOSE=true ;;
     --verbose=?*|--verbose=) usage_error "$1 argument ignored." ;;
+    # targets
+    all) ;;
+    ufs) DEFAULT_BUILD=false; BUILD_UFS="on" ;;
+    ufs_utils) DEFAULT_BUILD=false; BUILD_UFS_UTILS="on" ;;
+    upp) DEFAULT_BUILD=false; BUILD_UPP="on" ;;
+    gsi) DEFAULT_BUILD=false; BUILD_GSI="on" ;;
+    rrfs_utils) DEFAULT_BUILD=false; BUILD_GSI="on" ;;
+    # unknown
     -?*|?*) usage_error "Unknown option $1" ;;
     *) break
   esac
   shift
 done
+
+# choose default apps to build
+if [ "${DEFAULT_BUILD}" = true ]; then
+  BUILD_UFS="on"
+  BUILD_UFS_UTILS="on"
+  BUILD_UPP="on"
+  BUILD_GSI="off"
+  BUILD_RRFS_UTILS="off"
+fi
 
 # Ensure uppercase / lowercase ============================================
 APPLICATION="${APPLICATION^^}"
@@ -231,17 +261,21 @@ else
 fi
 
 # cmake settings
-CMAKE_SETTINGS="-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
-CMAKE_SETTINGS="${CMAKE_SETTINGS} -DCMAKE_INSTALL_BINDIR=exec"
-CMAKE_SETTINGS="${CMAKE_SETTINGS} -DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
+CMAKE_SETTINGS="\
+ -DCMAKE_BUILD_TYPE=${BUILD_TYPE}\
+ -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}\
+ -DCMAKE_INSTALL_BINDIR=${BIN_DIR}\
+ -DBUILD_UFS=${BUILD_UFS}\
+ -DBUILD_UFS_UTILS=${BUILD_UFS_UTILS}\
+ -DBUILD_UPP=${BUILD_UPP}\
+ -DBUILD_GSI=${BUILD_GSI}\
+ -DBUILD_RRFS_UTILS=${BUILD_RRFS_UTILS}"
+
 if [ ! -z "${APPLICATION}" ]; then
   CMAKE_SETTINGS="${CMAKE_SETTINGS} -DAPP=${APPLICATION}"
 fi
 if [ ! -z "${CCPP_SUITES}" ]; then
   CMAKE_SETTINGS="${CMAKE_SETTINGS} -DCCPP_SUITES=${CCPP_SUITES}"
-fi
-if [ ! -z "${RRFS}" ]; then
-  CMAKE_SETTINGS="${CMAKE_SETTINGS} -DENABLE_RRFS=${RRFS}"
 fi
 if [ ! -z "${ENABLE_OPTIONS}" ]; then
   CMAKE_SETTINGS="${CMAKE_SETTINGS} -DENABLE_OPTIONS=${ENABLE_OPTIONS}"
