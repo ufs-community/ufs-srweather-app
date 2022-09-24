a268b9f9954a413bf8c873c1002223f5771c2197
Daniel Abdi <daniel.abdi@noaa.gov>
2022-09-24 18:29:11 +0000

    Use preamble in function too.

--- ush/preamble.sh
+++ ush/preamble.sh
@@ -23,20 +23,13 @@
 #-----------------------------------------------------------------------
 #
 set +x
+
+# Id for MPMD jobs
 if [[ -v '1' ]]; then
-    id="(${1})"
+    local id="(${1})"
 else
-    id=""
+    local id=""
 fi
-
-# Record the start time so we can calculate the elapsed time later
-start_time=$(date +%s)
-
-# Get the base name of the calling script
-_calling_script=$(basename ${BASH_SOURCE[1]})
-
-# Announce the script has begun
-echo "Begin ${_calling_script} at $(date -u)"
 
 # Stage our variables
 export STRICT=${STRICT:-"TRUE"}
@@ -54,40 +47,53 @@ if [[ $TRACE == "TRUE" ]]; then
     export PS4='+ $(basename $BASH_SOURCE)[$LINENO]'"$id: "
 fi
 
-function postamble() {
-    #
-    #-----------------------------------------------------------------------
-    #
-    # Commands to execute when a script ends. 
-    #
-    # Syntax:
-    #   postamble script start_time rc
-    #
-    #   Arguments:
-    #     script: name of the script ending
-    #     start_time: start time of script (in seconds)
-    #     rc: the exit code of the script
-    #
-    #-----------------------------------------------------------------------
-    #
+# Only when not called from a function
+if [[ -z ${FUNCNAME[0]} ]]; then
 
-    set +x
-    script=${1}
-    start_time=${2}
-    rc=${3}
+    function postamble() {
+        #
+        #-----------------------------------------------------------------------
+        #
+        # Commands to execute when a script ends. 
+        #
+        # Syntax:
+        #   postamble script start_time rc
+        #
+        #   Arguments:
+        #     script: name of the script ending
+        #     start_time: start time of script (in seconds)
+        #     rc: the exit code of the script
+        #
+        #-----------------------------------------------------------------------
+        #
+    
+        set +x
+        script=${1}
+        start_time=${2}
+        rc=${3}
+    
+        # Calculate the elapsed time
+        end_time=$(date +%s)
+        elapsed_sec=$((end_time - start_time))
+        elapsed=$(date -d@${elapsed_sec} -u +%H:%M:%S)
+    
+        # Announce the script has ended, then pass the error code up
+        echo "End ${script} at $(date -u) with error code ${rc:-0} (time elapsed: ${elapsed})"
+        exit ${rc}
+    }
+    
+    # Record the start time so we can calculate the elapsed time later
+    local start_time=$(date +%s)
+    
+    # Get the base name of the calling script
+    local _calling_script=$(basename ${BASH_SOURCE[1]})
+    
+    # Announce the script has begun
+    echo "Begin ${_calling_script} at $(date -u)"
 
-    # Calculate the elapsed time
-    end_time=$(date +%s)
-    elapsed_sec=$((end_time - start_time))
-    elapsed=$(date -d@${elapsed_sec} -u +%H:%M:%S)
-
-    # Announce the script has ended, then pass the error code up
-    echo "End ${script} at $(date -u) with error code ${rc:-0} (time elapsed: ${elapsed})"
-    exit ${rc}
-}
-
-# Place the postamble in a trap so it is always called no matter how the script exits
-trap "postamble ${_calling_script} ${start_time} \$?" EXIT
+    # Place the postamble in a trap so it is always called no matter how the script exits
+    trap "postamble ${_calling_script} ${start_time} \$?" EXIT
+fi
 
 # Turn on our settings
 $ERR_EXIT_CMD
