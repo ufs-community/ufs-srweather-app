e7d25620b7b52f4a740eab7c942f304e9e1d5c52
Christina.Holt <Christina.Holt@noaa.gov>
2022-10-11 21:59:50 +0000

    Deprecating CYCL_HRS
    Changes were made to all config files and scripts to use FIRST and LAST
    cycle definitions to accept the cycle HH, and frequency will start from
    those for all relevant computation.

--- ush/wrappers/run_make_lbcs.sh
+++ ush/wrappers/run_make_lbcs.sh
@@ -2,7 +2,7 @@
 export GLOBAL_VAR_DEFNS_FP="${EXPTDIR}/var_defns.sh"
 set -x
 source ${GLOBAL_VAR_DEFNS_FP}
-export CDATE=${DATE_FIRST_CYCL}${CYCL_HRS}
+export CDATE=${DATE_FIRST_CYCL}
 export CYCLE_DIR=${EXPTDIR}/${CDATE}
 export SLASH_ENSMEM_SUBDIR=""
 
