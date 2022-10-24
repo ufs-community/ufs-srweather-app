7e348e10861442bfb25cea69ee6ec5e4e2de2af1
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2022-10-24 09:03:35 -0700

    Update miniconda_regional_workflow.lua
    update miniconda path

--- modulefiles/tasks/noaacloud/miniconda_regional_workflow.lua
+++ modulefiles/tasks/noaacloud/miniconda_regional_workflow.lua
@@ -1,4 +1,4 @@
-prepend_path("MODULEPATH", "/contrib/GST/miniconda3/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.10.3"))
+prepend_path("MODULEPATH", "/contrib/EPIC/miniconda3/modulefiles")
+load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 setenv("SRW_ENV", "regional_workflow")
