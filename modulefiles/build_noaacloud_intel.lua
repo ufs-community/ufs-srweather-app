da395dba22a3fdeed6a9261ed9a815eca893a0d9
Parallel Works app-run user <Cameron.Book@mgmt-cameronbook-pclusternoaav2use1-00027.pw-noaa-us-east-1.pw.local>
2022-10-24 22:47:25 +0000

    Add srw_common_spack lua file and update build_noaacloud_intel.

--- modulefiles/build_noaacloud_intel.lua
+++ modulefiles/build_noaacloud_intel.lua
@@ -6,13 +6,9 @@ the NOAA cloud using Intel-oneapi
 whatis([===[Loads libraries needed for building the UFS SRW App on NOAA cloud ]===])
 
 prepend_path("MODULEPATH", "/contrib/EPIC/spack-stack/envs/srw-develop-intel/install/modulefiles/Core")
+load("intel/2021.3.0")
 load("stack-intel")
 load("stack-intel-oneapi-mpi")
 load("cmake/3.22.1") 
 
-load("srw_common")
-load("esmf/8.3.0b09")
-load("netcdf-c")
-load("netcdf-fortran")
-load("libpng")
-load("jasper")
+load("srw_common_spack")
