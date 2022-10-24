d51a5e3fd7bb73358b2b4e14fb02ccca333e93db
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2022-10-24 08:58:55 -0700

    Update build_noaacloud_intel.lua
    update spack-stack location & esmf version

--- modulefiles/build_noaacloud_intel.lua
+++ modulefiles/build_noaacloud_intel.lua
@@ -5,13 +5,13 @@ the NOAA cloud using Intel-oneapi
 
 whatis([===[Loads libraries needed for building the UFS SRW App on NOAA cloud ]===])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/apps/srw-app-test/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/EPIC/spack-stack/envs/srw-develop-intel/install/modulefiles/Core")
 load("stack-intel")
 load("stack-intel-oneapi-mpi")
 load("cmake/3.22.1") 
 
 load("srw_common")
-load("esmf/8.2.0")
+load("esmf/8.3.0b09")
 load("netcdf-c")
 load("netcdf-fortran")
 load("libpng")
