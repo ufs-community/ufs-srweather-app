01ed3ba9c17708b8d2a3853c907d85a951cbe88a
Natalie Perlin <68030316+natalie-perlin@users.noreply.github.com>
2022-10-25 16:18:17 -0400

    Update wflow_jet.lua
    Update miniconda3 module location and ask to activate regional_workflow

--- modulefiles/wflow_jet.lua
+++ modulefiles/wflow_jet.lua
@@ -7,8 +7,8 @@ whatis([===[Loads libraries needed for running the UFS SRW App on Jet ]===])
 
 load("rocoto")
 
-prepend_path("MODULEPATH","/contrib/miniconda3/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.5.12"))
+prepend_path("MODULEPATH","/mnt/lfs4/HFIP/hfv3gfs/role.epic/miniconda3/modulefiles")
+load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
