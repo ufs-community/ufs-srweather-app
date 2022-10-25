dbad22d29f057ec59b791a0a5cf04d2b04e88520
michael.lueken <Michael.Lueken@noaa.gov>
2022-10-25 17:54:15 +0000

    Issue #429 - Removed my_load_any function from modulefiles/srw_common.lua, replace my_load_any with load_any in modulefiles/srw_common.lua, and removed load(pathJoin(mapl, os.getenv(mapl_ver) or 2.11.0-esmf-8.3.0b09)) from modulefiles/build_cheyenne_intel.lua so that the SRW would build with Intel compilers.

--- modulefiles/srw_common.lua
+++ modulefiles/srw_common.lua
@@ -1,21 +1,11 @@
-
--- Until Cheyenne updates Lmod version to >=8.3.7
--- emulate load_any with try_load + isloaded() combo
-function my_load_any(pkg1, pkg2)
-   try_load(pkg1)
-   if not isloaded(pkg1) then
-      load(pkg2)
-   end
-end
-
 load("jasper/2.0.25")
 load("zlib/1.2.11")
-my_load_any("png/1.6.35", "libpng/1.6.37")
+load_any("png/1.6.35", "libpng/1.6.37")
 
-my_load_any("netcdf/4.7.4", "netcdf-c/4.7.4")
-my_load_any("netcdf/4.7.4", "netcdf-fortran/4.5.4")
-my_load_any("pio/2.5.3", "parallelio/2.5.2")
-my_load_any("esmf/8.3.0b09", "esmf/8.2.0")
+load_any("netcdf/4.7.4", "netcdf-c/4.7.4")
+load_any("netcdf/4.7.4", "netcdf-fortran/4.5.4")
+load_any("pio/2.5.3", "parallelio/2.5.2")
+load_any("esmf/8.3.0b09", "esmf/8.2.0")
 load("fms/2022.01")
 
 load("bufr/11.7.0")
@@ -27,9 +17,9 @@ load("ip/3.3.3")
 load("sp/2.3.3")
 load("w3emc/2.9.2")
 
-my_load_any("gftl-shared/v1.5.0", "gftl-shared/1.5.0")
-my_load_any("yafyaml/v0.5.1", "yafyaml/0.5.1")
-my_load_any("mapl/2.22.0-esmf-8.3.0b09", "mapl/2.11.0-esmf-8.2.0")
+load_any("gftl-shared/v1.5.0", "gftl-shared/1.5.0")
+load_any("yafyaml/v0.5.1", "yafyaml/0.5.1")
+load_any("mapl/2.22.0-esmf-8.3.0b09", "mapl/2.11.0-esmf-8.2.0")
 
 load("nemsio/2.5.4")
 load("sfcio/1.4.1")
