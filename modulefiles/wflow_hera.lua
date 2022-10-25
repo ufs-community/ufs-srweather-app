5c7282ce45742341d258203c541e0cc2776aa87d
Natalie Perlin <68030316+natalie-perlin@users.noreply.github.com>
2022-10-25 16:17:06 -0400

    Update wflow_hera.lua
    Load an updated miniconda3 and ask to activate regional_workflow environment

--- modulefiles/wflow_hera.lua
+++ modulefiles/wflow_hera.lua
@@ -7,8 +7,8 @@ whatis([===[Loads libraries needed for running the UFS SRW App on Hera ]===])
 
 load("rocoto")
 
-prepend_path("MODULEPATH","/contrib/miniconda3/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.5.12"))
+prepend_path("MODULEPATH","/scratch1/NCEPDEV/nems/role.epic/miniconda3/modulefiles")
+load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
