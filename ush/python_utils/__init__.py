12c41c61d10df042edae5988f464f02a0147c16a
Daniel Abdi <daniel.abdi@noaa.gov>
2022-10-25 20:12:28 +0000

    Removed unused python utilities.

--- ush/python_utils/__init__.py
+++ ush/python_utils/__init__.py
@@ -1,7 +1,6 @@
 from .misc import uppercase, lowercase, find_pattern_in_str, find_pattern_in_file
 from .check_for_preexist_dir_file import check_for_preexist_dir_file
 from .check_var_valid_value import check_var_valid_value
-from .count_files import count_files
 from .create_symlink_to_file import create_symlink_to_file
 from .define_macos_utilities import define_macos_utilities
 from .environment import (
@@ -25,12 +24,9 @@ from .filesys_cmds_vrfy import (
     mkdir_vrfy,
     cd_vrfy,
 )
-from .get_elem_inds import get_elem_inds
-from .interpol_to_arbit_CRES import interpol_to_arbit_CRES
 from .print_input_args import print_input_args
 from .print_msg import print_info_msg, print_err_msg_exit, log_info
 from .run_command import run_command
-from .get_charvar_from_netcdf import get_charvar_from_netcdf
 from .xml_parser import load_xml_file, has_tag_with_value
 from .config_parser import (
     load_json_config,
