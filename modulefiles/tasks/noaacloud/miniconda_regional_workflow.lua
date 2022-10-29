439adfbbd71ad8b861a7b767bd0931f0e52d32f8
Mark Potts <mark.potts@noaa.gov>
2022-10-29 14:56:50 +0000

    removed extra lines

--- modulefiles/tasks/noaacloud/miniconda_regional_workflow.lua
+++ modulefiles/tasks/noaacloud/miniconda_regional_workflow.lua
@@ -1,7 +1,1 @@
---prepend_path("MODULEPATH", "/contrib/GST/miniconda3/modulefiles")
---load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.10.3"))
-
-prepend_path("PATH", "/apps/oneapi/mpi/2021.3.0/bin")
 prepend_path("PATH", "/contrib/EPIC/miniconda3/4.12.0/envs/regional_workflow/bin")
---prepend_path("LD_LIBRARY_PATH", "/lib64")
---setenv("SRW_ENV", "regional_workflow")
