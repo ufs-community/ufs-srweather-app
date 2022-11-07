e86269c65b3e12a5338a81053eb8b5c646ef06df
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2022-11-07 14:51:29 +0000

    update module files for aqm on wcoss2

--- modulefiles/tasks/wcoss2/aqm_lbcs.local.lua
+++ modulefiles/tasks/wcoss2/aqm_lbcs.local.lua
@@ -1,5 +1,5 @@
-load(pathJoin("intel", os.getenv("intel_ver")))
-load(pathJoin("python", os.getenv("python_ver")))
+load("python_regional_workflow")
+
 load(pathJoin("udunits", os.getenv("udunits_ver")))
 load(pathJoin("gsl", os.getenv("gsl_ver")))
 load(pathJoin("nco", os.getenv("nco_ver")))
