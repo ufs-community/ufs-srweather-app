17de2774b674c2c98fd8772797c8f2b2c39cb4b5
Michael Kavulich, Jr <kavulich@ucar.edu>
2022-11-22 19:47:38 +0000

    Add more command line arguments from original script, update config.yaml accordingly

--- tests/WE2E/run_WE2E_tests.py
+++ tests/WE2E/run_WE2E_tests.py
@@ -83,15 +83,36 @@ def run_we2e_tests(HOMEdir, args) -> None:
     for test in tests_to_run:
         #Starting with test yaml template, fill in user-specified and machine- and 
         # test-specific options, then write resulting complete config.yaml
-        logging.debug(f"For test {os.path.basename(test)}, constructing config.yaml")
+        test_name = os.path.basename(test)
+        logging.debug(f"For test {test_name}, constructing config.yaml")
         test_cfg = load_config_file(test)
 
         test_cfg['user'].update({"machine": args.machine})
         test_cfg['user'].update({"account": args.account})
-        test_cfg['user'].update({"compiler": args.compiler})
-        test_cfg['user'].update({"BUILD_MOD_FN": args.modulefile})
+        if args.run_envir is not None:
+            test_cfg['user'].update({"RUN_ENVIR": args.run_envir})
+        # if platform section was not in input config, initialize as empty dict
+        if 'platform' not in test_cfg:
+            test_cfg['platform'] = dict()
+        test_cfg['platform'].update({"BUILD_MOD_FN": args.modulefile})
+        test_cfg['workflow'].update({"compiler": args.compiler})
+        if args.expt_basedir:
+            test_cfg['workflow'].update({"EXPT_BASEDIR": args.expt_basedir})
+        test_cfg['workflow'].update({"EXPT_SUBDIR": test_name})
+        if args.exec_subdir:
+            test_cfg['workflow'].update({"EXEC_SUBDIR": args.exec_subdir})
+        if args.use_cron_to_relaunch:
+            test_cfg['workflow'].update({"USE_CRON_TO_RELAUNCH": args.use_cron_to_relaunch})
+        if args.cron_relaunch_intvl_mnts:
+            test_cfg['workflow'].update({"CRON_RELAUNCH_INTVL_MNTS": args.cron_relaunch_intvl_mnts})
+        if args.debug_tests:
+            test_cfg['workflow'].update({"DEBUG": args.debug_tests})
+        if args.verbose_tests:
+            test_cfg['workflow'].update({"VERBOSE": args.verbose_tests})
 
+        
 
+        logging.debug(f"Writing updated config.yaml for test {test_name}\nbased on specified command-line arguments:\n")
         logging.debug(cfg_to_yaml_str(test_cfg))
         with open(USHdir + "/config.yaml","w+") as f:
             f.writelines(cfg_to_yaml_str(test_cfg))
@@ -127,7 +148,7 @@ if __name__ == "__main__":
     logfile='log.run_WE2E_tests'
 
     #Parse arguments
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(epilog="For more information about config arguments (denoted in CAPS), see ush/config_defaults.yaml\n")
     optional = parser._action_groups.pop() # Edited this line
     required = parser.add_argument_group('required arguments')
 
@@ -135,8 +156,14 @@ if __name__ == "__main__":
     required.add_argument('-m', '--machine', type=str, help='Machine name; see ush/machine/ for valid values', required=True)
     required.add_argument('-a', '--account', type=str, help='Account name for running submitted jobs', required=True)
     parser.add_argument('-c', '--compiler', type=str, help='Compiler used for building the app', default='intel')
-    parser.add_argument('-mf', '--modulefile', type=str, help='Modulefile used for building the app')
-
+    parser.add_argument('--modulefile', type=str, help='Modulefile used for building the app')
+    parser.add_argument('--run_envir', type=str, help='Overrides RUN_ENVIR variable to a new value ( "nco" or "community" ) for all experiments')
+    parser.add_argument('--expt_basedir', type=str, help='Explicitly set EXPT_BASEDIR for all experiments')
+    parser.add_argument('--exec_subdir', type=str, help='Explicitly set EXEC_SUBDIR for all experiments')
+    parser.add_argument('use_cron_to_relaunch', action='store_true', help='Explicitly set USE_CRON_TO_RELAUNCH for all experiments')
+    parser.add_argument('--cron_relaunch_intvl_mnts', type=str, help='Overrides CRON_RELAUNCH_INTVL_MNTS for all experiments')
+    parser.add_argument('--debug_tests', action='store_true', help='Explicitly set DEBUG=TRUE for all experiments')
+    parser.add_argument('--verbose_tests', action='store_true', help='Explicitly set VERBOSE=TRUE for all experiments')
     parser._action_groups.append(optional)
 
     args = parser.parse_args()
