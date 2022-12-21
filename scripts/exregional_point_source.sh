8dcced97503cf36e231a01c7cd95f2f0bcccd469
bbakernoaa <barry.baker@noaa.gov>
2022-12-21 18:13:00 +0000

    add pt sources to aqm.rc

--- scripts/exregional_point_source.sh
+++ scripts/exregional_point_source.sh
@@ -110,6 +110,8 @@ if [ ! -s "${DATA}/pt-${yyyymmddhh}.nc" ]; then
   cp_vrfy ${HOMEdir}/sorc/AQM-utils/python_utils/stack-pt-merge.py stack-pt-merge.py
   python3 stack-pt-merge.py -s ${yyyymmddhh} -n ${nstep} -conus ${PT_SRC_CONUS} -hi ${PT_SRC_HI} -ak ${PT_SRC_AK}
 
+  # Link the file to INPUT
+  ln_vrfy ${DATA}/pt-${yyyymmddhh}.nc ${INPUT_DATA}/INPUT/PT.nc
   if [ ! -s "${DATA}/pt-${yyyymmddhh}.nc" ]; then
     print_err_msg_exit "\
 The point source file \"pt-${yyyymmddhh}.nc\" was not generated."
@@ -125,12 +127,12 @@ fi
 #
 #-----------------------------------------------------------------------
 #
-export NX=${ESGgrid_NX}
-export NY=${ESGgrid_NY}
-export LAYOUT_X
-export LAYOUT_Y
-export TOPO="${NEXUS_FIX_DIR}/${NEXUS_GRID_FN}"
-export PT_IN="${DATA}/pt-${yyyymmddhh}.nc"
+# export NX=${ESGgrid_NX}
+# export NY=${ESGgrid_NY}
+# export LAYOUT_X
+# export LAYOUT_Y
+# export TOPO="${NEXUS_FIX_DIR}/${NEXUS_GRID_FN}"
+# export PT_IN="${DATA}/pt-${yyyymmddhh}.nc"
 
 #
 #----------------------------------------------------------------------
@@ -139,7 +141,7 @@ export PT_IN="${DATA}/pt-${yyyymmddhh}.nc"
 #
 #-----------------------------------------------------------------------
 #
-mkdir_vrfy -p "${DATA}/PT"
+#mkdir_vrfy -p "${DATA}/PT"
 
 #
 #----------------------------------------------------------------------
@@ -148,11 +150,11 @@ mkdir_vrfy -p "${DATA}/PT"
 #
 #-----------------------------------------------------------------------
 #
-PREP_STEP
-eval ${RUN_CMD_AQM} ${EXECdir}/decomp-ptemis-mpi ${REDIRECT_OUT_ERR} || \
-print_err_msg_exit "\
-Call to execute PT_SOURCE for Online-CMAQ failed."
-POST_STEP
+# PREP_STEP
+# eval ${RUN_CMD_AQM} ${EXECdir}/decomp-ptemis-mpi ${REDIRECT_OUT_ERR} || \
+# print_err_msg_exit "\
+# Call to execute PT_SOURCE for Online-CMAQ failed."
+# POST_STEP
 
 #
 #-----------------------------------------------------------------------
@@ -161,7 +163,7 @@ POST_STEP
 #
 #-----------------------------------------------------------------------
 #
-mv_vrfy "${DATA}/PT" ${INPUT_DATA}
+#mv_vrfy "${DATA}/PT" ${INPUT_DATA}
 
 #
 #-----------------------------------------------------------------------
