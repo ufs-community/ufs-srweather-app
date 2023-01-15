eafca271beec159fc44ee6a0c8e7479454840481
Daniel Abdi <daniel.abdi@noaa.gov>
2023-01-15 00:23:44 +0000

    Workflow generation starting to work.

--- ush/create_model_configure_file.py
+++ ush/create_model_configure_file.py
@@ -336,7 +336,7 @@ class Testing(unittest.TestCase):
         set_env_var("FCST_LEN_HRS", 72)
         set_env_var("DT_ATMOS", 1)
         set_env_var("OMP_NUM_THREADS_RUN_FCST", 1)
-        set_env_var("RESTART_INTERVAL", 4)
+        set_env_var("RESTART_INTERVAL", [4] )
 
         set_env_var("WRTCMP_write_groups", 1)
         set_env_var("WRTCMP_write_tasks_per_group", 2)
