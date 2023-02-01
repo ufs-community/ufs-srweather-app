5fd6f0cb6e6cf1fc923f775f4206d8356ac759b9
gerard ketefian <gerard.ketefian@noaa.gov>
2023-02-01 13:14:40 -0700

    Modify module file for vx tasks to include intel module (because when running with GNU compiler, the intel libraries are still needed because MET/METplus is built with intel only).

--- modulefiles/tasks/hera/run_vx.local.lua
+++ modulefiles/tasks/hera/run_vx.local.lua
@@ -1,1 +1,9 @@
+--[[
+Loading intel is really only necessary when running verification tasks
+with the COMPILER experiment parameter set to "gnu" because in that case,
+the intel libraries aren't loaded, but the MET/METplus vx software still
+needs them because it's built using the intel compiler.  This line can
+be removed if/when there is a version of MET/METplus built using GNU.
+--]]
+load(pathJoin("intel", os.getenv("intel_ver") or "18.0.5.274"))
 load("miniconda_regional_workflow")
