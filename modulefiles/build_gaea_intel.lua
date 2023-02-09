3bb744f761a3cbe4a49b823a102fb1de59d19185
Michael Lueken <michael.lueken@noaa.gov>
2023-02-09 19:53:38 +0000

    Update modulefiles to use EPIC maintained locations and address Hera still submitting in Jenkins testing.

--- modulefiles/build_gaea_intel.lua
+++ modulefiles/build_gaea_intel.lua
@@ -5,18 +5,9 @@ the NOAA RDHPC machine Gaea using Intel-2022.1.2
 
 whatis([===[Loads libraries needed for building the UFS SRW App on Gaea ]===])
 
-load("PrgEnv-intel/6.0.5")
-unload("intel")
-unload("cray-mpich")
-unload("netcdf")
-load("intel/18.0.6.288")
-load("cray-mpich/7.7.11")
-load("cray-python/3.7.3.2")
-load("alps")
-
 load(pathJoin("cmake", os.getenv("cmake_ver") or "3.20.1"))
 
-prepend_path("MODULEPATH","/lustre/f2/pdata/ncep_shared/hpc-stack.epic/libs/intel/2021.3.0/modulefiles/stack")
+prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-2021.3.0/modulefiles/stack")
 load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
 load(pathJoin("intel", os.getenv("intel_ver") or "2021.3.0"))
 load(pathJoin("hpc-intel", os.getenv("hpc_intel_ver") or "2021.3.0"))
--- modulefiles/build_orion_intel.lua
+++ modulefiles/build_orion_intel.lua
@@ -11,7 +11,7 @@ load("noaatools")
 load(pathJoin("cmake", os.getenv("cmake_ver") or "3.22.1"))
 load(pathJoin("python", os.getenv("python_ver") or "3.9.2"))
 
-prepend_path("MODULEPATH","/work/noaa/epic-ps/hpc-stack/lib/intel/2022.1.2/modulefiles/stack")
+prepend_path("MODULEPATH","/work/noaa/epic-ps/role-epic-ps/hpc-stack/libs/intel-2022.1.2/modulefiles/stack")
 load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
 load(pathJoin("hpc-intel", os.getenv("hpc_intel_ver") or "2022.1.2"))
 load(pathJoin("hpc-impi", os.getenv("hpc_impi_ver") or "2022.1.2"))
@@ -20,8 +20,6 @@ load("srw_common")
 
 load(pathJoin("nccmp", os.getenv("nccmp_ver") or "1.8.9.0"))
 load(pathJoin("nco", os.getenv("nco_ver") or "4.9.3"))
-
-load(pathJoin("wgrib2", os.getenv("wgrib2_ver") or "2.0.8"))
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER","mpiicpc")
