dbaeb45a08cb176090595b6203ee5a511a47321d
chan-hoo <chan-hoo.jeon@noaa.gov>
2023-02-21 13:30:21 +0000

    remove typo

--- modulefiles/tasks/wcoss2/fire_emission.local.lua
+++ modulefiles/tasks/wcoss2/fire_emission.local.lua
@@ -4,4 +4,3 @@ load(pathJoin("udunits", os.getenv("udunits_ver")))
 load(pathJoin("gsl", os.getenv("gsl_ver")))
 load(pathJoin("netcdf", os.getenv("netcdf_ver")))
 load(pathJoin("nco", os.getenv("nco_ver")))
-~
