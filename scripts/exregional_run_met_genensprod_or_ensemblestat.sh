0627d6bbb39ecd4245de7d5697f0aa86dabe730d
gerard ketefian <gerard.ketefian@noaa.gov>
2023-03-19 13:27:39 -0600

    Add j-job and ex-script for running EnsembleStat for APCP, all accumulations (01, 03, 06, and 24); modify METplus conf file for accumulation of 01 hour so it's a template; add template Metplus conf file for accumulatons > 01hour; modify ROCOTO xml to run with new j-job.

---
+++ scripts/exregional_run_met_genensprod_or_ensemblestat.sh
@@ -0,0 +1,429 @@
+#!/bin/bash
+
+#
+#-----------------------------------------------------------------------
+#
+# Source the variable definitions file and the bash utility functions.
+#
+#-----------------------------------------------------------------------
+#
+. $USHdir/source_util_funcs.sh
+source_config_for_task "task_run_vx_ensgrid|task_run_post" ${GLOBAL_VAR_DEFNS_FP}
+#
+#-----------------------------------------------------------------------
+#
+# Source files defining auxiliary functions for verification.
+#
+#-----------------------------------------------------------------------
+#
+. $USHdir/set_vx_params.sh
+. $USHdir/set_vx_fhr_list.sh
+#
+#-----------------------------------------------------------------------
+#
+# Save current shell options (in a global array).  Then set new options
+# for this script/function.
+#
+#-----------------------------------------------------------------------
+#
+{ save_shell_opts; . $USHdir/preamble.sh; } > /dev/null 2>&1
+#
+#-----------------------------------------------------------------------
+#
+# Get the full path to the file in which this script/function is located
+# (scrfunc_fp), the name of that file (scrfunc_fn), and the directory in
+# which the file is located (scrfunc_dir).
+#
+#-----------------------------------------------------------------------
+#
+scrfunc_fp=$( $READLINK -f "${BASH_SOURCE[0]}" )
+scrfunc_fn=$( basename "${scrfunc_fp}" )
+scrfunc_dir=$( dirname "${scrfunc_fp}" )
+#
+#-----------------------------------------------------------------------
+#
+# Specify the set of valid argument names for this script/function.
+# Then process the arguments provided to this script/function (which
+# should consist of a set of name-value pairs of the form arg1="value1",
+# etc).
+#
+#-----------------------------------------------------------------------
+#
+valid_args=( \
+  "met_tool_sc" \
+  "met_tool_pc" \
+  )
+process_args valid_args "$@"
+#
+#-----------------------------------------------------------------------
+#
+# For debugging purposes, print out values of arguments passed to this
+# script.  Note that these will be printed out only if VERBOSE is set to
+# TRUE.
+#
+#-----------------------------------------------------------------------
+#
+print_input_args "valid_args"
+#
+#-----------------------------------------------------------------------
+#
+# Print message indicating entry into script.
+#
+#-----------------------------------------------------------------------
+#
+print_info_msg "
+========================================================================
+Entering script:  \"${scrfunc_fn}\"
+In directory:     \"${scrfunc_dir}\"
+
+This is the ex-script for the task that runs the METplus ${met_tool_pc}
+tool either to generate ensemble products without performing verification
+(if running the GenEnsProd tool) or to perform ensemble-based verification
+(if running the EnsembleStat tool).
+========================================================================"
+#
+#-----------------------------------------------------------------------
+#
+# Get the cycle date and time in YYYYMMDDHH format.
+#
+#-----------------------------------------------------------------------
+#
+CDATE="${PDY}${cyc}"
+#
+#-----------------------------------------------------------------------
+#
+# Set various verification parameters associated with the field to be
+# verified.  Not all of these are necessarily used later below but are
+# set here for consistency with other verification ex-scripts.
+#
+#-----------------------------------------------------------------------
+#
+FIELDNAME_IN_OBS_INPUT=""
+FIELDNAME_IN_FCST_INPUT=""
+FIELDNAME_IN_MET_OUTPUT=""
+FIELDNAME_IN_MET_FILEDIR_NAMES=""
+
+set_vx_params \
+  obtype="${OBTYPE}" \
+  field="$VAR" \
+  accum_hh="${ACCUM_HH}" \
+  outvarname_grid_or_point="grid_or_point" \
+  outvarname_field_is_APCPgt01h="field_is_APCPgt01h" \
+  outvarname_fieldname_in_obs_input="FIELDNAME_IN_OBS_INPUT" \
+  outvarname_fieldname_in_fcst_input="FIELDNAME_IN_FCST_INPUT" \
+  outvarname_fieldname_in_MET_output="FIELDNAME_IN_MET_OUTPUT" \
+  outvarname_fieldname_in_MET_filedir_names="FIELDNAME_IN_MET_FILEDIR_NAMES"
+#
+#-----------------------------------------------------------------------
+#
+# Set additional field-dependent verification parameters.
+#
+#-----------------------------------------------------------------------
+#
+if [ "${grid_or_point}" = "grid" ]; then
+
+  case "${FIELDNAME_IN_MET_FILEDIR_NAMES}" in
+    "APCP01h")
+      FIELD_THRESHOLDS="gt0.0, ge0.254, ge0.508, ge2.54"
+      ;;
+    "APCP03h")
+      FIELD_THRESHOLDS="gt0.0, ge0.508, ge2.54, ge6.350"
+      ;;
+    "APCP06h")
+      FIELD_THRESHOLDS="gt0.0, ge2.54, ge6.350, ge12.700"
+      ;;
+    "APCP24h")
+      FIELD_THRESHOLDS="gt0.0, ge6.350, ge12.700, ge25.400"
+      ;;
+    "REFC")
+      FIELD_THRESHOLDS="ge20, ge30, ge40, ge50"
+      ;;
+    "RETOP")
+      FIELD_THRESHOLDS="ge20, ge30, ge40, ge50"
+      ;;
+    *)
+      print_err_msg_exit "\
+Verification parameters have not been defined for this field
+(FIELDNAME_IN_MET_FILEDIR_NAMES):
+  FIELDNAME_IN_MET_FILEDIR_NAMES = \"${FIELDNAME_IN_MET_FILEDIR_NAMES}\""
+      ;;
+  esac
+
+elif [ "${grid_or_point}" = "point" ]; then
+
+  FIELD_THRESHOLDS=""
+
+fi
+#
+#-----------------------------------------------------------------------
+#
+# Set paths and file templates for input to and output from the MET/
+# METplus tool to be run as well as other file/directory parameters.
+#
+#-----------------------------------------------------------------------
+#
+if [ "${grid_or_point}" = "grid" ]; then
+
+  OBS_INPUT_FN_TEMPLATE=""
+  if [ "${field_is_APCPgt01h}" = "TRUE" ]; then
+    OBS_INPUT_DIR="${VX_OUTPUT_BASEDIR}/metprd/PcpCombine_obs"
+    OBS_INPUT_FN_TEMPLATE=$( eval echo ${OBS_CCPA_APCPgt01h_FN_TEMPLATE} )
+    FCST_INPUT_DIR="${VX_OUTPUT_BASEDIR}"
+  else
+    OBS_INPUT_DIR="${OBS_DIR}"
+    case "${FIELDNAME_IN_MET_FILEDIR_NAMES}" in
+      "APCP01h")
+        OBS_INPUT_FN_TEMPLATE="${OBS_CCPA_APCP01h_FN_TEMPLATE}"
+        ;;
+      "REFC")
+        OBS_INPUT_FN_TEMPLATE="${OBS_MRMS_REFC_FN_TEMPLATE}"
+        ;;
+      "RETOP")
+        OBS_INPUT_FN_TEMPLATE="${OBS_MRMS_RETOP_FN_TEMPLATE}"
+        ;;
+    esac
+    OBS_INPUT_FN_TEMPLATE=$( eval echo ${OBS_INPUT_FN_TEMPLATE} )
+    FCST_INPUT_DIR="${VX_FCST_INPUT_BASEDIR}"
+  fi
+
+elif [ "${grid_or_point}" = "point" ]; then
+
+  OBS_INPUT_DIR="${VX_OUTPUT_BASEDIR}/metprd/Pb2nc_obs"
+  OBS_INPUT_FN_TEMPLATE=$( eval echo ${OBS_NDAS_SFCorUPA_FN_METPROC_TEMPLATE} )
+  FCST_INPUT_DIR="${VX_FCST_INPUT_BASEDIR}"
+
+fi
+#
+# Construct variable that contains a METplus template of the paths to
+# the files that the PcpCombine tool has generated (in previous workflow
+# tasks).  This will be exported to the environment and read by the
+# METplus configuration files.
+#
+NDIGITS_ENSMEM_NAMES=3
+FCST_INPUT_FN_TEMPLATE=""
+for (( i=0; i<${NUM_ENS_MEMBERS}; i++ )); do
+
+  mem_indx=$(($i+1))
+  mem_indx_fmt=$(printf "%0${NDIGITS_ENSMEM_NAMES}d" "${mem_indx}")
+  time_lag=$( bc -l <<< "${ENS_TIME_LAG_HRS[$i]}*${SECS_PER_HOUR}" )
+
+  SLASH_ENSMEM_SUBDIR_OR_NULL="/mem${mem_indx_fmt}"
+  if [ "${field_is_APCPgt01h}" = "TRUE" ]; then
+    template="${CDATE}${SLASH_ENSMEM_SUBDIR_OR_NULL}/metprd/PcpCombine_fcst/${FCST_FN_METPROC_TEMPLATE}"
+  else
+    template="${FCST_SUBDIR_TEMPLATE}/${FCST_FN_TEMPLATE}"
+  fi
+
+  if [ -z "${FCST_INPUT_FN_TEMPLATE}" ]; then
+    FCST_INPUT_FN_TEMPLATE="  $(eval echo ${template})"
+  else
+    FCST_INPUT_FN_TEMPLATE="\
+${FCST_INPUT_FN_TEMPLATE},
+  $(eval echo ${template})"
+  fi
+
+done
+
+OUTPUT_BASE="${VX_OUTPUT_BASEDIR}/${CDATE}"
+OUTPUT_DIR="${OUTPUT_BASE}/metprd/${met_tool_pc}"
+STAGING_DIR="${OUTPUT_BASE}/stage/${FIELDNAME_IN_MET_FILEDIR_NAMES}"
+LOG_SUFFIX="_${FIELDNAME_IN_MET_FILEDIR_NAMES}_${CDATE}"
+#
+#-----------------------------------------------------------------------
+#
+# Set the array of forecast hours for which to run the MET/METplus tool.
+#
+#-----------------------------------------------------------------------
+#
+set_vx_fhr_list \
+  cdate="${CDATE}" \
+  fcst_len_hrs="${FCST_LEN_HRS}" \
+  field="$VAR" \
+  accum_hh="${ACCUM_HH}" \
+  base_dir="${OBS_INPUT_DIR}" \
+  fn_template="${OBS_INPUT_FN_TEMPLATE}" \
+  check_hourly_files="FALSE" \
+  outvarname_fhr_list="FHR_LIST"
+#
+#-----------------------------------------------------------------------
+#
+# Make sure the MET/METplus output directory(ies) exists.
+#
+#-----------------------------------------------------------------------
+#
+mkdir_vrfy -p "${OUTPUT_DIR}"
+#
+#-----------------------------------------------------------------------
+#
+# Check for existence of top-level OBS_DIR.
+#
+#-----------------------------------------------------------------------
+#
+if [ ! -d "${OBS_DIR}" ]; then
+  print_err_msg_exit "\
+OBS_DIR does not exist or is not a directory:
+  OBS_DIR = \"${OBS_DIR}\""
+fi
+#
+#-----------------------------------------------------------------------
+#
+# Export variables needed in the common METplus configuration file (at
+# ${METPLUS_CONF}/common.conf).
+#
+#-----------------------------------------------------------------------
+#
+export MET_INSTALL_DIR
+export METPLUS_PATH
+export MET_BIN_EXEC
+export METPLUS_CONF
+export LOGDIR
+#
+#-----------------------------------------------------------------------
+#
+# Do not run METplus if there isn't at least one valid forecast hour for
+# which to run it.
+#
+#-----------------------------------------------------------------------
+#
+if [ -z "${FHR_LIST}" ]; then
+  print_err_msg_exit "\
+The list of forecast hours for which to run METplus is empty:
+  FHR_LIST = [${FHR_LIST}]"
+fi
+#
+#-----------------------------------------------------------------------
+#
+# Set the names of the template METplus configuration file, the METplus
+# configuration file generated from this template, and the METplus log
+# file.
+#
+#-----------------------------------------------------------------------
+#
+# First, set the base file names.
+#
+if [ "${field_is_APCPgt01h}" = "TRUE" ]; then
+  metplus_config_tmpl_fn="APCPgt01h"
+else
+  metplus_config_tmpl_fn="${FIELDNAME_IN_MET_FILEDIR_NAMES}"
+fi
+metplus_config_tmpl_fn="${met_tool_pc}_${metplus_config_tmpl_fn}"
+metplus_config_fn="${met_tool_pc}_${FIELDNAME_IN_MET_FILEDIR_NAMES}"
+metplus_log_fn="${metplus_config_fn}"
+#
+# Add prefixes and suffixes (extensions) to the base file names.
+#
+metplus_config_tmpl_fn="${metplus_config_tmpl_fn}.conf"
+metplus_config_fn="${metplus_config_fn}.conf"
+metplus_log_fn="metplus.log.${metplus_log_fn}"
+#
+#-----------------------------------------------------------------------
+#
+# Generate the METplus configuration file from its jinja template.
+#
+#-----------------------------------------------------------------------
+#
+# Set the full paths to the jinja template METplus configuration file
+# (which already exists) and the METplus configuration file that will be
+# generated from it.
+#
+metplus_config_tmpl_fp="${METPLUS_CONF}/${metplus_config_tmpl_fn}"
+metplus_config_fp="${OUTPUT_DIR}/${metplus_config_fn}"
+#
+# Define variables that appear in the jinja template.
+#
+settings="\
+#
+# Date and forecast hour information.
+#
+  'cdate': '$CDATE'
+  'fhr_list': '${FHR_LIST}'
+#
+# Input and output directory/file information.
+#
+  'metplus_config_fn': '${metplus_config_fn:-}'
+  'metplus_log_fn': '${metplus_log_fn:-}'
+  'obs_input_dir': '${OBS_INPUT_DIR:-}'
+  'obs_input_fn_template': '${OBS_INPUT_FN_TEMPLATE:-}'
+  'fcst_input_dir': '${FCST_INPUT_DIR:-}'
+  'fcst_input_fn_template': '${FCST_INPUT_FN_TEMPLATE:-}'
+  'output_base': '${OUTPUT_BASE}'
+  'output_dir': '${OUTPUT_DIR}'
+  'output_fn_template': '${OUTPUT_FN_TEMPLATE:-}'
+  'staging_dir': '${STAGING_DIR}'
+  'vx_fcst_model_name': '${VX_FCST_MODEL_NAME}'
+#
+# Ensemble and member-specific information.
+#
+  'num_ens_members': '${NUM_ENS_MEMBERS}'
+  'uscore_ensmem_name_or_null': '${USCORE_ENSMEM_NAME_OR_NULL:-}'
+  'time_lag': '${time_lag:-}'
+#
+# Field information.
+#
+  'fieldname_in_obs_input': '${FIELDNAME_IN_OBS_INPUT}'
+  'fieldname_in_fcst_input': '${FIELDNAME_IN_FCST_INPUT}'
+  'fieldname_in_met_output': '${FIELDNAME_IN_MET_OUTPUT}'
+  'fieldname_in_met_filedir_names': '${FIELDNAME_IN_MET_FILEDIR_NAMES}'
+  'obtype': '${OBTYPE}'
+  'accum_hh': '${ACCUM_HH:-}'
+  'accum_no_pad': '${ACCUM_NO_PAD:-}'
+  'field_thresholds': '${FIELD_THRESHOLDS:-}'
+"
+#
+# Call the python script to generate the METplus configuration file from
+# the jinja template.
+#
+$USHdir/fill_jinja_template.py -q \
+                               -u "${settings}" \
+                               -t ${metplus_config_tmpl_fp} \
+                               -o ${metplus_config_fp} || \
+print_err_msg_exit "\
+Call to python script fill_jinja_template.py to generate a METplus
+configuration file from a jinja template failed.  Parameters passed
+to this script are:
+  Full path to template METplus configuration file:
+    metplus_config_tmpl_fp = \"${metplus_config_tmpl_fp}\"
+  Full path to output METplus configuration file:
+    metplus_config_fp = \"${metplus_config_fp}\"
+  Namelist settings specified on command line:
+    settings =
+$settings"
+#
+#-----------------------------------------------------------------------
+#
+# Call METplus.
+#
+#-----------------------------------------------------------------------
+#
+print_info_msg "$VERBOSE" "
+Calling METplus to run MET's ${met_tool_sc} tool for field(s): ${FIELDNAME_IN_MET_FILEDIR_NAMES}"
+${METPLUS_PATH}/ush/run_metplus.py \
+  -c ${METPLUS_CONF}/common.conf \
+  -c ${metplus_config_fp} || \
+print_err_msg_exit "
+Call to METplus failed with return code: $?
+METplus configuration file used is:
+  metplus_config_fp = \"${metplus_config_fp}\""
+#
+#-----------------------------------------------------------------------
+#
+# Print message indicating successful completion of script.
+#
+#-----------------------------------------------------------------------
+#
+print_info_msg "
+========================================================================
+METplus ${met_tool_pc} tool completed successfully.
+
+Exiting script:  \"${scrfunc_fn}\"
+In directory:    \"${scrfunc_dir}\"
+========================================================================"
+#
+#-----------------------------------------------------------------------
+#
+# Restore the shell options saved at the beginning of this script/func-
+# tion.
+#
+#-----------------------------------------------------------------------
+#
+{ restore_shell_opts; } > /dev/null 2>&1
