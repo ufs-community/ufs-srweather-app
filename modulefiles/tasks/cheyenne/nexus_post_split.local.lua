709c941c5eb02bab0d26d82f33699424e1d7e57b
Paddy Mccarthy <paddy@cheyenne6.cheyenne.ucar.edu>
2023-03-20 11:50:33 -0600

    [develop] Updated cheyenne configs to use new shared conda env for Tier-1 platforms. See isue #684.

--- modulefiles/tasks/cheyenne/nexus_post_split.local.lua
+++ modulefiles/tasks/cheyenne/nexus_post_split.local.lua
@@ -1,4 +1,3 @@
--- Note that nco/5.0.3 is available on cheyenne
 load(pathJoin("nco", os.getenv("nco_ver") or "4.9.5"))
 load("ncarenv")
-load("miniconda_regional_workflow-cmaq")
+load("miniconda_regional_workflow_cmaq")
--- modulefiles/tasks/cheyenne/pre_post_stat.local.lua
+++ modulefiles/tasks/cheyenne/pre_post_stat.local.lua
@@ -1,4 +1,2 @@
--- Note that nco/5.0.3 is available on cheyenne
--- load(pathJoin("nco", os.getenv("nco_ver") or "4.9.5"))
 load("nco/4.9.5")
-load("miniconda_regional_workflow-cmaq")
+load("miniconda_regional_workflow_cmaq")
