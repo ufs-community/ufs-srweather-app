9cad1db374e119bdf67e8def123915ab72707d95
ulmononian <cameron_book@alumni.brown.edu>
2023-03-21 23:26:48 -0500

    More modulefile stuff.

--- modulefiles/COMMITED_build_orion_intel.lua
+++ modulefiles/COMMITED_build_orion_intel.lua
@@ -8,6 +8,7 @@ whatis([===[Loads libraries needed for building the UFS SRW App on Orion ]===])
 load("contrib")
 load("noaatools")
 
+
 prepend_path("MODULEPATH", "/work/noaa/epic-ps/role-epic-ps/spack-stack/spack-stack-1.3.0/envs/unified-env/install/modulefiles/Core")
 load("stack-intel/2022.0.2")
 load("stack-intel-oneapi-mpi/2021.5.1")
@@ -17,9 +18,8 @@ prepend_path("MODULEPATH", "/work/noaa/da/role-da/spack-stack/modulefiles")
 miniconda3_ver=os.getenv("miniconda3_ver") or "3.9.7"
 load(pathJoin("stack-python", miniconda3_ver))
 
---load("ufs-srw-app-env/unified-dev")
+load("ufs-srw-app-env/unified-dev")
 
-load("srw_common_spack")
 load("nccmp/1.9.0.1")
 load("nco/5.0.6")
 
