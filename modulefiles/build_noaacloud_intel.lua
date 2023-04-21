3a478f546e5e87935e2d1abf11d3a27e46bb848f
ulmononian <cameron_book@alumni.brown.edu>
2023-04-21 12:51:36 -0500

    Update noaacloud build modulefiles.

--- modulefiles/build_noaacloud_intel.lua
+++ modulefiles/build_noaacloud_intel.lua
@@ -5,13 +5,14 @@ the NOAA cloud using Intel-oneapi
 
 whatis([===[Loads libraries needed for building the UFS SRW App on NOAA cloud ]===])
 
-prepend_path("MODULEPATH", "/contrib/EPIC/spack-stack/envs/srw-develop-intel/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/apps/modules/modulefiles")
-prepend_path("PATH", "/contrib/EPIC/bin")
-load("intel/2021.3.0")
-load("impi/2021.3.0")
-load("stack-intel")
-load("stack-intel-oneapi-mpi")
+prepend_path("MODULEPATH", "/contrib/EPIC/spack-stack/spack-stack-1.3.0/envs/unified-dev/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/modulefiles/core")
+
+load("stack-intel/2021.3.0")
+load("stack-intel-oneapi-mpi/2021.3.0")
+load("stack-python/3.9.12")
 load("cmake/3.22.1") 
 
 load("srw_common_spack")
+
+load("ufs-pyenv")
