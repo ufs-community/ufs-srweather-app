b44d65fe61047325a5b2a24258a6983a42f92e86
yichengt90 <clouden90@hotmail.com>
2023-04-26 13:08:33 -0500

    Add Skill-score metric based on Indy-Severe-Weather case

---
+++ .cicd/scripts/srw_metric_example.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+#
+# The goal of this script is to provide an example to perform Indy-Severe-Weather run and compare with baseline using
+# skill-score metric.
+#
+# Required:
+#    WORKSPACE=</full/path/to/ufs-srweather-app>
+#    SRW_PLATFORM=<supported_platform_host>
+#    SRW_COMPILER=<intel|gnu>
+#
+# Optional:
+[[ -n ${ACCOUNT} ]] || ACCOUNT="no_account"
+[[ -n ${FORGIVE_CONDA} ]] || FORGIVE_CONDA=true
+set -e -u -x
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+
+# Get repository root from Jenkins WORKSPACE variable if set, otherwise, set
+# relative to script directory.
+declare workspace
+if [[ -n "${WORKSPACE}" ]]; then
+    workspace="${WORKSPACE}"
+else
+    workspace="$(cd -- "${script_dir}/../.." && pwd)"
+fi
+
+# Normalize Parallel Works cluster platform value.
+declare platform
+if [[ "${SRW_PLATFORM}" =~ ^(az|g|p)clusternoaa ]]; then
+    platform='noaacloud'
+else
+    platform="${SRW_PLATFORM}"
+fi
+
+# Test directories
+we2e_experiment_base_dir="${workspace}/../expt_dirs/metric_test"
+we2e_test_dir="${workspace}/tests/WE2E"
+we2e_test_name="grid_SUBCONUS_Ind_3km_ics_FV3GFS_lbcs_FV3GFS_suite_GFS_v16"
+
+pwd
+
+# Activate the workflow environment ...
+source etc/lmod-setup.sh ${platform,,}
+module use modulefiles
+module load build_${platform,,}_${SRW_COMPILER}
+module load wflow_${platform,,}
+
+[[ ${FORGIVE_CONDA} == true ]] && set +e +u    # Some platforms have incomplete python3 or conda support, but wouldn't necessarily block workflow tests
+conda activate regional_workflow
+set -e -u
+
+# build srw
+cd ${WORKSPACE}/tests
+./build.sh ${platform,,} ${SRW_COMPILER}
+cd ${WORKSPACE}
+
+# run test
+[[ -d ${we2e_experiment_base_dir} ]] && rm -rf ${we2e_experiment_base_dir}
+cd ${WORKSPACE}/tests/WE2E
+./run_WE2E_tests.py -t ${we2e_test_name} -m ${platform,,} -a ${ACCOUNT} --expt_basedir "metric_test" --exec_subdir=install_intel/exec -q
+cd ${WORKSPACE}
+
+# run metplus skill-score check
+# first load MET env variables
+source ${we2e_experiment_base_dir}/${we2e_test_name}/var_defns.sh
+[[ ! -f Indy-Severe-Weather.tgz ]] && wget https://noaa-ufs-srw-pds.s3.amazonaws.com/sample_cases/release-public-v2.1.0/Indy-Severe-Weather.tgz
+[[ ! -d Indy-Severe-Weather ]] && tar xvfz Indy-Severe-Weather.tgz
+[[ -f skill-score.out ]] && rm skill-score.out
+cp ${we2e_experiment_base_dir}/${we2e_test_name}/2019061500/mem000/metprd/PointStat/*.stat ${WORKSPACE}/Indy-Severe-Weather/metprd/point_stat/
+${MET_INSTALL_DIR}/${MET_BIN_EXEC}/stat_analysis -config .cicd/scripts/STATAnalysisConfig_skill_score -lookin ${WORKSPACE}/Indy-Severe-Weather/metprd/point_stat -v 2 -out skill-score.out
+
+# check skill-score.out
+cat skill-score.out
