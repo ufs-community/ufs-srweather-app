9ece3691cc476e1cf5af661f0b672b423b18536f
Cameron Book <bcameron@cheyenne4.cheyenne.ucar.edu>
2023-04-28 16:05:29 -0600

    Update cheyenne intel with unload statements.

--- modulefiles/build_cheyenne_gnu.lua
+++ modulefiles/build_cheyenne_gnu.lua
@@ -21,13 +21,7 @@ load("cmake/3.22.0")
 
 
 load("srw_common_spack")
-
---load(pathJoin("g2", os.getenv("g2_ver") or "3.4.5"))
---load(pathJoin("esmf", os.getenv("esmf_ver") or "8.3.0b09"))
---load(pathJoin("netcdf", os.getenv("netcdf_ver") or "4.7.4"))
---load(pathJoin("libpng", os.getenv("libpng_ver") or "1.6.37"))
---load(pathJoin("pio", os.getenv("pio_ver") or "2.5.7"))
---load(pathJoin("fms", os.getenv("fms_ver") or "2022.04"))
+load("ufs-pyenv")
 
 setenv("CMAKE_C_COMPILER","mpicc")
 setenv("CMAKE_CXX_COMPILER","mpic++")
--- modulefiles/build_cheyenne_intel.lua
+++ modulefiles/build_cheyenne_intel.lua
@@ -5,6 +5,11 @@ the CISL machine Cheyenne using Intel-19.1.1.217
 
 whatis([===[Loads libraries needed for building the UFS SRW App on Cheyenne ]===])
 
+unload("ncarenv/1.3")
+unload("intel/19.1.1")
+unload("ncarcompilers/0.5.0")
+unload("mpt/2.25")
+unload("netcdf/4.8.1")
 
 prepend_path("MODULEPATH","/glade/work/epicufsrt/contrib/spack-stack/spack-stack-1.3.0/envs/unified-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/glade/work/jedipara/cheyenne/spack-stack/modulefiles/misc")
@@ -15,14 +20,6 @@ load("stack-python/3.9.12")
 load("cmake/3.22.0")
 
 load("srw_common_spack")
-
-load("g2/3.4.5")
-load("esmf/8.3.0b09")
-load("netcdf-c/4.9.2")
-load("netcdf-fortran/4.6.0")
-load("libpng/1.6.37")
-load("parallelio/2.5.9")
-load("fms/2022.04")
 load("ufs-pyenv")
 
 setenv("CMAKE_C_COMPILER","mpiicc")
