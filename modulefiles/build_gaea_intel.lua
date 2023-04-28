59b8b0b08524bfc19cb6284879dad6c399ae3f40
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-04-28 11:22:08 -0700

    Update build_gaea_intel.lua

--- modulefiles/build_gaea_intel.lua
+++ modulefiles/build_gaea_intel.lua
@@ -5,17 +5,23 @@ the NOAA RDHPC machine Gaea using Intel-2022.1.2
 
 whatis([===[Loads libraries needed for building the UFS SRW App on Gaea ]===])
 
-load(pathJoin("cmake", os.getenv("cmake_ver") or "3.20.1"))
+unload("intel")
+unload("cray-mpich")
+unload("cray-python")
+unload("darshan")
 
-prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-2021.3.0_noarch/modulefiles/stack")
-load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
-load(pathJoin("intel", os.getenv("intel_ver") or "2021.3.0"))
-load(pathJoin("hpc-intel", os.getenv("hpc_intel_ver") or "2021.3.0"))
-load(pathJoin("hpc-cray-mpich", os.getenv("hpc_cray_mpich_ver") or "7.7.11"))
-load(pathJoin("gcc", os.getenv("gcc_ver") or "8.3.0"))
-load(pathJoin("libpng", os.getenv("libpng_ver") or "1.6.37"))
+prepend_path("MODULEPATH", "/lustre/f2/dev/wpo/role.epic/contrib/spack-stack/spack-stack-1.3.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/lustre/f2/pdata/esrl/gsd/spack-stack/modulefiles")
 
-load("srw_common")
+load("stack-intel/2021.3.0")
+load("stack-cray-mpich/7.7.11")
+load("stack-python/3.9.12")
+load("cmake/3.23.1")
+
+load("srw_common_spack")
+load("ufs-pyenv")
+
+load("gcc/8.3.0")
 
 setenv("CC","cc")
 setenv("FC","ftn")
@@ -24,4 +30,3 @@ setenv("CMAKE_C_COMPILER","cc")
 setenv("CMAKE_CXX_COMPILER","CC")
 setenv("CMAKE_Fortran_COMPILER","ftn")
 setenv("CMAKE_Platform","gaea.intel")
-
