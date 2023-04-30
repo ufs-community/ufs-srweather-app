d44d6f7d60d468005e29a571d719fc75cbac721a
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-04-29 21:50:20 -0700

    Update wflow_gaea.lua

--- modulefiles/wflow_gaea.lua
+++ modulefiles/wflow_gaea.lua
@@ -5,15 +5,19 @@ the NOAA RDHPC machine Gaea
 
 whatis([===[Loads libraries needed for running the UFS SRW App on gaea ]===])
 
+unload("intel")
+unload("cray-mpich")
+unload("cray-python")
+unload("darshan")
+
+prepend_path("MODULEPATH", "/lustre/f2/dev/wpo/role.epic/contrib/spack-stack/spack-stack-1.3.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/lustre/f2/pdata/esrl/gsd/spack-stack/modulefiles")
 prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
+
+load("stack-intel/2021.3.0")
+load("stack-cray-mpich/7.7.11")
+load("stack-python/3.9.12")
+
+load("ufs-pyenv")
 load("rocoto")
 load("alps")
-
-setenv("PROJ_LIB", "/lustre/f2/dev/role.epic/contrib/miniconda3/4.12.0/envs/regional_workflow/share/proj")
-
-if mode() == "load" then
-   LmodMsgRaw([===[Please do the following to activate conda:
-       > conda activate regional_workflow
-]===])
-end
