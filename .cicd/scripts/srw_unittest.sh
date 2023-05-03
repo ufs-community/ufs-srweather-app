f5025471c72fdaba6236e29b9124b3bca6c640f2
Christina.Holt <Christina.Holt@noaa.gov>
2023-05-03 15:33:35 +0000

    Make changes to script found in debugging.

--- .cicd/scripts/srw_unittest.sh
+++ .cicd/scripts/srw_unittest.sh
@@ -10,15 +10,16 @@ script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)
 # Get repository root from Jenkins WORKSPACE variable if set, otherwise,
 # set relative to script directory.
 declare workspace
-if [[ -n "${WORKSPACE}" ]]; then
+if [[ -n "${WORKSPACE:-}" ]]; then
   workspace="${WORKSPACE}"
 else
   workspace="$(cd -- "${script_dir}/../.." && pwd)"
 fi
 
+cd $workspace
 # Only run this on machines with hpss access
 hpss_machines=( jet hera )
-if [ $hpss_machines =~ ${SRW_PLATFORM} ] ; then
+if [[ ${hpss_machines[@]} =~ ${SRW_PLATFORM} ]] ; then
 
   source ${workspace}/ush/load_modules_wflow.sh ${SRW_PLATFORM}
   module load hpss
