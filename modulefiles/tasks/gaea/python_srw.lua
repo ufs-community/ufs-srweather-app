7fdb1176b5e4d099dc5150da5d738b35e5376550
Christina.Holt <Christina.Holt@noaa.gov>
2023-05-08 15:30:08 +0000

    Add pythonpath for uwtools package items.

--- modulefiles/tasks/gaea/python_srw.lua
+++ modulefiles/tasks/gaea/python_srw.lua
@@ -2,3 +2,10 @@ prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 setenv("SRW_ENV", "regional_workflow")
+
+local mod_path, mod_file = splitFileName(myFileName())
+local uwtools_scripts_path =  pathJoin(mod_path, "../../../ush/python_utils/uwtools")
+local uwtools_package_path =  pathJoin(mod_path, "../../../ush/python_utils/uwtools/src/")
+
+prepend_path("PYTHONPATH", uwtools_scripts_path)
+prepend_path("PYTHONPATH", uwtools_package_path)
--- modulefiles/tasks/wcoss2/python_srw.lua
+++ modulefiles/tasks/wcoss2/python_srw.lua
@@ -1,3 +1,10 @@
 load(pathJoin("intel", os.getenv("intel_ver")))
 load(pathJoin("python", os.getenv("python_ver")))
 load(pathJoin("prod_util", os.getenv("prod_util_ver")))
+
+local mod_path, mod_file = splitFileName(myFileName())
+local uwtools_scripts_path =  pathJoin(mod_path, "../../../ush/python_utils/uwtools")
+local uwtools_package_path =  pathJoin(mod_path, "../../../ush/python_utils/uwtools/src/")
+
+prepend_path("PYTHONPATH", uwtools_scripts_path)
+prepend_path("PYTHONPATH", uwtools_package_path)
