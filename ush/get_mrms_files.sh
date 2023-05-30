c36dccc0bed91457fb98198bdf0a800984e3cc55
Christina.Holt <Christina.Holt@noaa.gov>
2023-05-30 15:00:28 +0000

    Addressing Eddie's comments.

--- ush/get_mrms_files.sh
+++ ush/get_mrms_files.sh
@@ -42,20 +42,21 @@ function get_mrms_files () {
   for min in ${RADARREFL_MINS[@]}
   do
     min=$( printf %2.2i $((timelevel+min)) )
-    echo "Looking for data valid:"${YYYY}"-"${MM}"-"${DD}" "${cyc}":"${min}
+    echo "Looking for data valid:${YYYY}-${MM}-${DD} ${cyc}:${min}"
     nsslfiles=${NSSL}/*${mrms}_00.50_${YYYY}${MM}${DD}-${cyc}${min}??.${OBS_SUFFIX}
     for nsslfile in ${nsslfiles} ; do
       if [ -s $nsslfile ]; then
         echo 'Found '${nsslfile}
-        nsslfile1=*${mrms}_*_${YYYY}${MM}${DD}-${cyc}${min}*.${OBS_SUFFIX}
-        numgrib2=${#nsslfiles1}
+        file_matches=*${mrms}_*_${YYYY}${MM}${DD}-${cyc}${min}*.${OBS_SUFFIX}
+        nsslfile1=${NSSL}/$file_matches
+        numgrib2=${#nsslfile1}
         echo 'Number of GRIB-2 files: '${numgrib2}
 
         # 10 represents a significant number of vertical levels of data
         if [ ${numgrib2} -ge 10 ] && [ ! -e filelist_mrms ]; then
-          cp_vrfy ${NSSL}/${nsslfile1} ${output_path}
-          ls ${nsslfile1} > ${output_path}/filelist_mrms
-          echo 'Copying mrms files for ${YYYY}${MM}${DD}-${cyc}${min}'
+          cp_vrfy ${nsslfile1} ${output_path}
+          ls ${output_path}/${file_matches} > ${output_path}/filelist_mrms
+          echo "Copying mrms files for ${YYYY}${MM}${DD}-${cyc}${min}"
         fi
       fi
     done
