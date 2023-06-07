993b61943733e38d41beaf41b2ae7beeabaad853
chan-hoo <chan-hoo.jeon@noaa.gov>
2023-06-07 01:23:34 +0000

    update aqm_manager script

--- scripts/exaqm_manager.sh
+++ scripts/exaqm_manager.sh
@@ -78,7 +78,7 @@ while [ $h_try -lt 70 ]; do
   #   current cycle fcst restart file exist
   ####################################
   if [ ${found_required_file_for_next_cycle} = "NO" ] && [ -d "${COMIN}/RESTART" ]; then
-    if [ "$(ls -A ${COMIN}/RESTART )" ] && [-f ${gefs_check} ] && [ -f ${gfs_check} ]; then
+    if [[ "$(ls -A ${COMIN}/RESTART )" && -f ${gefs_check} && -f ${gfs_check} ]]; then
       ecflow_client --event release_next_cycle
       found_required_file_for_next_cycle=YES
     fi
