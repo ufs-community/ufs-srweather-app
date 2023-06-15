cd3778fdfa06b73a76e1e1bc026ec86b355718d2
Natalie Perlin <Natalie.Perlin@noaa.gov>
2023-06-15 11:46:25 -0400

    update Gaea modulefile

--- modulefiles/build_gaea_intel.lua
+++ modulefiles/build_gaea_intel.lua
@@ -7,12 +7,10 @@ whatis([===[Loads libraries needed for building the UFS SRW App on Gaea ]===])
 
 load(pathJoin("cmake", os.getenv("cmake_ver") or "3.20.1"))
 
-prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-2021.3.0_noarch/modulefiles/stack")
+prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-classic-2022.0.2/modulefiles/stack")
 load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
-load(pathJoin("intel", os.getenv("intel_ver") or "2021.3.0"))
-load(pathJoin("hpc-intel", os.getenv("hpc_intel_ver") or "2021.3.0"))
-load(pathJoin("hpc-cray-mpich", os.getenv("hpc_cray_mpich_ver") or "7.7.11"))
-load(pathJoin("gcc", os.getenv("gcc_ver") or "8.3.0"))
+load(pathJoin("hpc-intel-classic", os.getenv("hpc_intel_classic_ver") or "2022.0.2"))
+load(pathJoin("hpc-cray-mpich", os.getenv("hpc_cray_mpich_ver") or "7.7.20"))
 load(pathJoin("libpng", os.getenv("libpng_ver") or "1.6.37"))
 
 load("srw_common")
