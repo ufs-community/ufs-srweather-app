d3be7eb854288bb524bf1b80ba9cb0804c55e50a
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2023-06-22 19:11:13 +0000

    replace python env with shared loc

--- modulefiles/tasks/hera/miniconda_online-cmaq.lua
+++
@@ -1,5 +0,0 @@
-prepend_path("MODULEPATH", "/contrib/miniconda3/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
-
-setenv("AQM_ENV_FP", "/scratch2/NCEPDEV/naqfc/RRFS_CMAQ/PY_VENV")
-setenv("AQM_ENV", "online-cmaq")
