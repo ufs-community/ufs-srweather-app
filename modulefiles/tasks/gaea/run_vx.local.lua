88ed69a4143e9051dc6dc6ce844fd90b17d116f6
Natalie Perlin <Natalie.Perlin@noaa.gov>
2023-07-22 13:14:54 -0400

    update met, metplus versions

--- modulefiles/tasks/gaea/run_vx.local.lua
+++ modulefiles/tasks/gaea/run_vx.local.lua
@@ -1,6 +1,6 @@
 --[[
 Compiler-specific modules are used for met and metplus libraries
 --]]
-load(pathJoin("met", os.getenv("met_ver") or "10.1.2"))
-load(pathJoin("metplus", os.getenv("metplus_ver") or "4.1.3"))
+load(pathJoin("met", os.getenv("met_ver") or "11.0.2"))
+load(pathJoin("metplus", os.getenv("metplus_ver") or "5.0.2"))
 load("python_srw")
