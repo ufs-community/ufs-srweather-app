adec3be7c072fb31ac3621e939494f27d594b8f6
Natalie Perlin <Natalie.Perlin@noaa.gov>
2023-07-22 10:32:03 -0400

    Updating modulefiles for Gaea

--- modulefiles/build_gaea_intel.lua
+++ modulefiles/build_gaea_intel.lua
@@ -11,9 +11,17 @@ prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-clas
 load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
 load(pathJoin("hpc-intel-classic", os.getenv("hpc_intel_classic_ver") or "2022.0.2"))
 load(pathJoin("hpc-cray-mpich", os.getenv("hpc_cray_mpich_ver") or "7.7.20"))
-load("alps")
 
 load("srw_common")
+-- Need at runtime
+load("alps")
+
+local MKLROOT="/opt/intel/oneapi/mkl/2022.0.2/"
+prepend_path("LD_LIBRARY_PATH",pathJoin(MKLROOT,"lib/intel64"))
+pushenv("MKLROOT", MKLROOT)
+
+pushenv("GSI_BINARY_SOURCE_DIR", "/lustre/f2/dev/role.epic/contrib/GSI_data/fix/20230601")
+pushenv("CRAYPE_LINK_TYPE","dynamic")
 
 setenv("CC","cc")
 setenv("FC","ftn")
--- modulefiles/wflow_gaea.lua
+++ modulefiles/wflow_gaea.lua
@@ -7,10 +7,14 @@ whatis([===[Loads libraries needed for running the UFS SRW App on gaea ]===])
 
 load("set_pythonpath")
 prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/miniconda3/modulefiles")
-prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/rocoto/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
+prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/rocoto/modulefiles")
 load("rocoto")
 load("alps")
+
+pushenv("MKLROOT", "/opt/intel/oneapi/mkl/2022.0.2/")
+pushenv("GSI_BINARY_SOURCE_DIR", "/lustre/f2/dev/role.epic/contrib/GSI_data/fix/20230601")
+setenv("PMI_NO_PREINITIALIZE","1")
 
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
