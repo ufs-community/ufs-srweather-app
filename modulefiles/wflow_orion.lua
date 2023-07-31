9d327015d2a278f424c00da06e5d9e18af732e24
Natalie Perlin <natalie.perlin@noaa.gov>
2023-07-31 16:01:11 -0500

    update Orion stacks to use new role-epic account space

--- modulefiles/wflow_orion.lua
+++ modulefiles/wflow_orion.lua
@@ -11,7 +11,7 @@ load("wget")
 load("set_pythonpath")
 
 unload("python")
-append_path("MODULEPATH","/work/noaa/epic-ps/role-epic-ps/miniconda3/modulefiles")
+append_path("MODULEPATH","/work/noaa/epic/role-epic/contrib/orion/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 if mode() == "load" then
