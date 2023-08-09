f2abb865ce4e380e824dcd450e4a2ac511052bca
Natalie Perlin <68030316+natalie-perlin@users.noreply.github.com>
2023-08-09 07:16:02 -0600

    [develop] MET_verification: use modules met, metplus  (#826)
    * Use modules met and metplus installed in a software stack for MET verification tasks. Do not use explicitly set paths in machine files or config.yaml files. Use env. variables METPLUS_PATH, MET_INSTALL_DIR from the modulefiles. Retire MET_BIN_EXEC variables, use standard "bin". Modules met/10.1.2 and metplus/4.1.3 are used. Newer versions, met/11.0.2 and metplus/5.0.2, require additional code changes.
    
    * Updated Orion stack location due to a mandatory transition to a new role account and space, under /work/noaa/epic/role-epic/. New stack is under /work/noaa/epic/role-epic/contrib/orion/hpc-stack/intel-2022.1.2/. SRW fundamental tests have been run successfully with the updated stack, as well as GSI regression tests and UFS-WM regression tests.
    Miniconda3 with all the environments has been installed in a new Orion location, under /work/noaa/epic/role-epic/contrib/orion/miniconda3/ and updated in modulefiles.
    
    ---------
    
    Co-authored-by: Natalie Perlin <perlin.natalie@gmail.com>
    Co-authored-by: Natalie Perlin <nperlin@Orion-login-2.HPC.MsState.Edu>
    Co-authored-by: Natalie Perlin <natalie.perlin@noaa.gov>

--- modulefiles/tasks/orion/python_srw.lua
+++ modulefiles/tasks/orion/python_srw.lua
@@ -1,5 +1,5 @@
 unload("python")
-append_path("MODULEPATH","/work/noaa/epic-ps/role-epic-ps/miniconda3/modulefiles")
+append_path("MODULEPATH","/work/noaa/epic/role-epic/contrib/orion/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
 setenv("SRW_ENV", "workflow_tools")
