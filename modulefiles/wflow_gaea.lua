8c9b560a82e025b3cb4dbec378fb8a2539e5315b
Natalie Perlin <Natalie.Perlin@noaa.gov>
2023-08-14 10:52:32 -0400

    unload standard python in wflow_gaea.lua on compute nodes

--- modulefiles/wflow_gaea.lua
+++ modulefiles/wflow_gaea.lua
@@ -5,6 +5,7 @@ the NOAA RDHPC machine Gaea
 
 whatis([===[Loads libraries needed for running the UFS SRW App on gaea ]===])
 
+unload("python")
 load("set_pythonpath")
 prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
