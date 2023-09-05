25bdcab71c0260a0d1bb9b590566371691122eb3
Natalie Perlin <Natalie.Perlin@noaa.gov>
2023-09-05 09:29:29 -0400

    update for for Gaea C5 modulefile

--- modulefiles/build_gaea_c5_intel.lua
+++ modulefiles/build_gaea_c5_intel.lua
@@ -3,7 +3,7 @@ This module loads libraries for building the UFS SRW App on
 the NOAA RDHPC machine Gaea C5 using Intel-2023.1.0
 ]])
 
-whatis([===[Loads libraries needed for building the UFS SRW App on Gaea ]===])
+whatis([===[Loads libraries needed for building the UFS SRW App on Gaea C5 ]===])
 
 load(pathJoin("cmake", os.getenv("cmake_ver") or "3.23.1"))
 
@@ -16,17 +16,13 @@ load(pathJoin("hpc-cray-mpich", os.getenv("hpc_cray_mpich_ver") or "8.1.25"))
 
 load("srw_common")
 
---local MKLROOT="/opt/intel/oneapi/mkl/2023.1.0/"
---prepend_path("LD_LIBRARY_PATH",pathJoin(MKLROOT,"lib/intel64"))
---pushenv("MKLROOT", MKLROOT)
-
---pushenv("GSI_BINARY_SOURCE_DIR", "/lustre/f2/dev/role.epic/contrib/GSI_data/fix/20230601")
---pushenv("CRAYPE_LINK_TYPE","dynamic")
 unload("darshan-runtime/3.4.0")
-
 setenv("CFLAGS","-diag-disable=10441")
 setenv("FFLAGS","-diag-disable=10441")
 
+setenv("CC","cc")
+setenv("FC","ftn")
+setenv("CXX","CC")
 setenv("CMAKE_C_COMPILER","cc")
 setenv("CMAKE_Fortran_COMPILER","ftn")
 setenv("CMAKE_CXX_COMPILER","CC")
