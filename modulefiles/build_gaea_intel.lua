c00ca7353efff811d9110a966bdc7bda96c09df3
Natalie Perlin <68030316+natalie-perlin@users.noreply.github.com>
2023-09-15 09:09:43 -0400

    [develop] Update modulefiles to use hdf/1.14.0 and netcdf/4.9.2-based software stacks on Tier 1 systems (#889)
     * Update SRW modulefiles to use hpc-stacks with higher versions of software modules on Tier 1 platforms, similar to currently used for UFS-WM.
     * Update UFS_SRW_data location on Orion to use new dedicated role-epic space
    
    ---------
    
    Co-authored-by: Natalie Perlin <Natalie@Natalies-MacBook-Air.local>
    Co-authored-by: Natalie Perlin <Natalie.Perlin@noaa.gov>

--- modulefiles/build_gaea_intel.lua
+++ modulefiles/build_gaea_intel.lua
@@ -7,9 +7,9 @@ whatis([===[Loads libraries needed for building the UFS SRW App on Gaea ]===])
 
 load(pathJoin("cmake", os.getenv("cmake_ver") or "3.20.1"))
 
-prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-classic-2022.0.2/modulefiles/stack")
+prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/hpc-stack/intel-classic-2023.1.0/modulefiles/stack")
 load(pathJoin("hpc", os.getenv("hpc_ver") or "1.2.0"))
-load(pathJoin("hpc-intel-classic", os.getenv("hpc_intel_classic_ver") or "2022.0.2"))
+load(pathJoin("hpc-intel-classic", os.getenv("hpc_intel_classic_ver") or "2023.1.0"))
 load(pathJoin("hpc-cray-mpich", os.getenv("hpc_cray_mpich_ver") or "7.7.20"))
 
 load("srw_common")
@@ -23,4 +23,8 @@ setenv("CMAKE_C_COMPILER","cc")
 setenv("CMAKE_CXX_COMPILER","CC")
 setenv("CMAKE_Fortran_COMPILER","ftn")
 setenv("CMAKE_Platform","gaea.intel")
+
+setenv("CFLAGS","-diag-disable=10441")
+setenv("FFLAGS","-diag-disable=10441 -fp-model source")
+
 
