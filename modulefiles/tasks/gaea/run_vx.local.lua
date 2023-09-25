8b411f72e23c9361837a70df918c40f7a147629f
EdwardSnyder-NOAA <edward.snyder@noaa.gov>
2023-09-25 13:22:30 +0000

    Added met/metplus vars and updated met/metplus version

--- modulefiles/tasks/gaea/run_vx.local.lua
+++ modulefiles/tasks/gaea/run_vx.local.lua
@@ -1,6 +1,10 @@
 --[[
 Compiler-specific modules are used for met and metplus libraries
 --]]
-load(pathJoin("met", os.getenv("met_ver") or "10.1.2"))
-load(pathJoin("metplus", os.getenv("metplus_ver") or "4.1.3"))
+load(pathJoin("met", os.getenv("met_ver") or "10.1.1"))
+load(pathJoin("metplus", os.getenv("metplus_ver") or "4.1.1"))
 load("python_srw")
+
+setenv("MET_INSTALL_DIR", os.getenv("met_ROOT"))
+setenv("MET_BASE", pathJoin(os.getenv("met_ROOT"), "share", "met"))
+setenv("METPLUS_PATH", os.getenv("metplus_ROOT"))
--- modulefiles/tasks/hera/run_vx.local.lua
+++ modulefiles/tasks/hera/run_vx.local.lua
@@ -4,3 +4,8 @@ Compiler-specific modules are used for met and metplus libraries
 load(pathJoin("met", os.getenv("met_ver") or "10.1.1"))
 load(pathJoin("metplus", os.getenv("metplus_ver") or "4.1.1"))
 load("python_srw")
+
+setenv("MET_INSTALL_DIR", os.getenv("met_ROOT"))
+setenv("MET_BASE", pathJoin(os.getenv("met_ROOT"), "share", "met"))
+setenv("METPLUS_PATH", os.getenv("metplus_ROOT"))
+--
