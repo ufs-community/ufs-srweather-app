b263a62c57e847a3fea0c24c8ecc47d3596d4286
Jianping.Huang <Jianping.Huang@noaa.gov>
2023-11-11 03:03:25 +0000

    adding the env variable definitions to the JAQM scripts

--- ush/job_preamble.sh
+++ ush/job_preamble.sh
@@ -2,9 +2,7 @@
 
 #
 #-----------------------------------------------------------------------
-#
 # If requested to share data with next task, override jobid
-#
 #-----------------------------------------------------------------------
 #
 if [ "${WORKFLOW_MANAGER}" = "ecflow" ]; then
@@ -19,9 +17,7 @@ if [ $# -ne 0 ]; then
 fi
 #
 #-----------------------------------------------------------------------
-#
 # Set NCO standard environment variables
-#
 #-----------------------------------------------------------------------
 #
 export envir="${envir:-${envir_dfv}}"
@@ -30,105 +26,28 @@ export RUN="${RUN:-${RUN_dfv}}"
 export model_ver="${model_ver:-${model_ver_dfv}}"
 export COMROOT="${COMROOT:-${COMROOT_dfv}}"
 export DATAROOT="${DATAROOT:-${DATAROOT_dfv}}"
-export DCOMROOT="${DCOMROOT:-${DCOMROOT_dfv}}"
 export LOGBASEDIR="${LOGBASEDIR:-${LOGBASEDIR_dfv}}"
 
-export DBNROOT="${DBNROOT:-${DBNROOT_dfv}}"
-export SENDECF="${SENDECF:-${SENDECF_dfv}}"
-export SENDDBN="${SENDDBN:-${SENDDBN_dfv}}"
-export SENDDBN_NTC="${SENDDBN_NTC:-${SENDDBN_NTC_dfv}}"
-export SENDCOM="${SENDCOM:-${SENDCOM_dfv}}"
-export SENDWEB="${SENDWEB:-${SENDWEB_dfv}}"
 export KEEPDATA="${KEEPDATA:-${KEEPDATA_dfv}}"
 export MAILTO="${MAILTO:-${MAILTO_dfv}}"
 export MAILCC="${MAILCC:-${MAILCC_dfv}}"
 
-[[ "$WORKFLOW_MANAGER" = "rocoto" ]] && export COMROOT=$COMROOT
-export COMIN="${COMIN:-$(compath.py -o ${NET}/${model_ver}/${RUN}.${PDY})}"
-export COMOUT="${COMOUT:-$(compath.py -o ${NET}/${model_ver}/${RUN}.${PDY}/${cyc})}"
-export COMINgfs="${COMINgfs:-$(compath.py ${envir}/gfs/${gfs_ver})}"
-export COMINgefs="${COMINgefs:-$(compath.py ${envir}/gefs/${gefs_ver})}"
-export COMOUTwmo="${COMOUTwmo:-${COMOUT}/wmo}"
-
-export FIXaqmconfig="${FIXaqmbio:-${HOMEaqm}/fix/aqm/epa/data}"
-export FIXaqmbio="${FIXaqmbio:-${HOMEaqm}/fix/bio}"
-export FIXaqmdust="${FIXaqmdust:-${HOMEaqm}/fix/dust}"
-export FIXaqmcanopy="${FIXaqmcanopy:-${HOMEaqm}/fix/canopy}"
-export FIXaqmchem_lbcs="${FIXaqmchem_lbcs:-${HOMEaqm}/fix/chem_lbcs}"
-export FIXaqmnexus="${FIXaqmnexus:-${HOMEaqm}/fix/nexus}"
-export FIXaqmnexus_gfs_sfc="${FIXaqmnexus:-${HOMEaqm}/fix/gfs}"
-export FIXaqmfire="${FIXaqmfire:-${HOMEaqm}/fix/fire}"
-export COMINgefs="${COMINgefs:-${COMROOT}/gefs/${gefs_ver})}"
-export COMINgfs="${COMINgfs:-$(compath.py  ${envir}/gfs/${gfs_ver})}"
-export COMINemispt="${COMINemispt:-${COMIN}/emission/pt}"
-export COMINemis="${COMINemis:-${COMIN}/emission}"
-export DCOMINfire="${DCOMINfire:-${DCOMROOT}/${PDY}/rave}"
-export DCOMINairnow="${DCOMINairnow:-${DCOMROOT}}"
-
-#
 #-----------------------------------------------------------------------
-#
-# Change YES/NO (NCO standards; job card) to TRUE/FALSE (workflow standards)
-# for NCO environment variables
-#
-#-----------------------------------------------------------------------
-#
-export KEEPDATA=$(boolify "${KEEPDATA}")
-export SENDCOM=$(boolify "${SENDCOM}")
-export SENDDBN=$(boolify "${SENDDBN}")
-export SENDDBN_NTC=$(boolify "${SENDDBN_NTC}")
-export SENDECF=$(boolify "${SENDECF}")
-export SENDWEB=$(boolify "${SENDWEB}")
-
-#
-#-----------------------------------------------------------------------
-#
 # Set cycle and ensemble member names in file/diectory names
-#
 #-----------------------------------------------------------------------
-#
 if [ $subcyc -eq 0 ]; then
     export cycle="t${cyc}z"
 else
     export cycle="t${cyc}${subcyc}z"
 fi
-if [ "${DO_ENSEMBLE}" = "TRUE" ] && [ ! -z $ENSMEM_INDX ]; then
-    export dot_ensmem=".mem${ENSMEM_INDX}"
-else
-    export dot_ensmem=
-fi
-#
-#-----------------------------------------------------------------------
-#
-# Create a temp working directory (DATA) and cd into it.
-#
-#-----------------------------------------------------------------------
-#
-export DATA=
-export DATA=${DATAROOT}/${jobid}
-mkdir -p $DATA
-cd $DATA
-#
-#-----------------------------------------------------------------------
-#
-# Run setpdy to initialize PDYm and PDYp variables
-#
-#-----------------------------------------------------------------------
-#
-if [ ! -z $(command -v setpdy.sh) ]; then
-    COMROOT=$COMROOT setpdy.sh
-    . ./PDY
-fi
-export COMINm1="${COMINm1:-$(compath.py -o ${NET}/${model_ver}/${RUN}.${PDYm1})}"
-export COMINm2="${COMINm1:-$(compath.py -o ${NET}/${model_ver}/${RUN}.${PDYm2})}"
+
+export dot_ensmem=
+
 export CDATE=${PDY}${cyc}
 #
 #-----------------------------------------------------------------------
-#
 # Set pgmout and pgmerr files
-#
 #-----------------------------------------------------------------------
-#
 export pgmout="${DATA}/OUTPUT.$$"
 export pgmerr="${DATA}/errfile"
 export REDIRECT_OUT_ERR=">>${pgmout} 2>${pgmerr}"
@@ -166,10 +85,8 @@ export -f PREP_STEP
 export -f POST_STEP
 #
 #-----------------------------------------------------------------------
-#
 # Create symlinks to log files in the experiment directory. Makes viewing
 # log files easier in NCO mode, as well as make CIs work
-#
 #-----------------------------------------------------------------------
 #
 if [ "${WORKFLOW_MANAGER}" != "ecflow" ]; then
@@ -182,9 +99,7 @@ if [ "${WORKFLOW_MANAGER}" != "ecflow" ]; then
 fi
 #
 #-----------------------------------------------------------------------
-#
 # Add a postamble function
-#
 #-----------------------------------------------------------------------
 #
 function job_postamble() {
