a257d5fa525685ceba6d273cb235f74dc5cfdff4
Ratko Vasic <ratko.vasic@noaa.gov>
2023-11-15 22:01:05 +0000

    Per @MichaelLuken's suggestion made changes in loading modules and in Jenkinsfile

--- modulefiles/tasks/hera/miniconda_regional_workflow_cmaq.lua
+++ modulefiles/tasks/hera/miniconda_regional_workflow_cmaq.lua
@@ -1,3 +1,4 @@
+unload("python")
 prepend_path("MODULEPATH","/scratch1/NCEPDEV/nems/role.epic/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
--- modulefiles/tasks/orion/miniconda_regional_workflow_cmaq.lua
+++ modulefiles/tasks/orion/miniconda_regional_workflow_cmaq.lua
@@ -1,3 +1,4 @@
+unload("python")
 prepend_path("MODULEPATH","/work/noaa/epic/role-epic/contrib/orion/miniconda3/modulefiles")
 load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
 
