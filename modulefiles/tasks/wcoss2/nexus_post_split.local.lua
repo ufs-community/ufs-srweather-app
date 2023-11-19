6a09a21bbd454ba76f3b29e1062d6400fc7e2110
Jianping.Huang <Jianping.Huang@noaa.gov>
2023-11-19 15:47:40 +0000

    revised ecflow scripts and update J-job and ex-scripts and delete unreleated ufs files

--- modulefiles/tasks/wcoss2/nexus_post_split.local.lua
+++
@@ -1,8 +0,0 @@
-load("python_regional_workflow")
-
-load(pathJoin("hdf5", os.getenv("hdf5_ver")))
-load(pathJoin("netcdf", os.getenv("netcdf_ver")))
-
-load(pathJoin("udunits", os.getenv("udunits_ver")))
-load(pathJoin("gsl", os.getenv("gsl_ver")))
-load(pathJoin("nco", os.getenv("nco_ver")))
--- modulefiles/tasks/wcoss2/python_regional_workflow.lua
+++
@@ -1,5 +0,0 @@
-load(pathJoin("PrgEnv-intel", os.getenv("PrgEnv_intel_ver")))
-load(pathJoin("craype", os.getenv("craype_ver")))
-load(pathJoin("intel", os.getenv("intel_ver")))
-load(pathJoin("python", os.getenv("python_ver")))
-load(pathJoin("prod_util", os.getenv("prod_util_ver")))
