780671c0f3a34b7b4a8a2e56dccb0133c5c00252
gerard ketefian <gerard.ketefian@noaa.gov>
2023-11-27 09:23:42 -0700

    Remove extraneous field suffix in name of PcpCombine METplus conf files.

--- scripts/exregional_run_met_pcpcombine.sh
+++ scripts/exregional_run_met_pcpcombine.sh
@@ -287,7 +287,7 @@ fi
 # Add prefixes and suffixes (extensions) to the base file names.
 #
 metplus_config_tmpl_fn="${metplus_config_tmpl_fn}_${field}.conf"
-metplus_config_fn="${metplus_config_fn}_${field}.conf"
+metplus_config_fn="${metplus_config_fn}.conf"
 metplus_log_fn="metplus.log.${metplus_log_fn}"
 #
 #-----------------------------------------------------------------------
