e781d7f2720d466ae5671af46307f394016536a1
chan-hoo <chan-hoo.jeon@noaa.gov>
2023-11-27 21:56:23 +0000

    change dependency for run_post

--- scripts/exaqm_run_post.sh
+++ scripts/exaqm_run_post.sh
@@ -176,7 +176,7 @@ else
   DATAFCST=$DATAROOT/run_fcst${dot_ensmem/./_}.${share_pid}
 fi	  
 
-if [ "${CPL_AQM}" = "TRUE" ]; then
+if [ "${WORKFLOW_MANAGER}" = "ecflow" ]; then
   dyn_file="${COMIN}/${cyc}/${NET}.${cycle}${dot_ensmem}.dyn.f${fhr}${mnts_secs_str}.nc"
   phy_file="${COMIN}/${cyc}/${NET}.${cycle}${dot_ensmem}.phy.f${fhr}${mnts_secs_str}.nc"
 else
