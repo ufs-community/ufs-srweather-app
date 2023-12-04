f852094f6d7585835484ce6604380a7e621fe5c8
Jianping.Huang <Jianping.Huang@noaa.gov>
2023-12-04 22:09:29 +0000

    update auto_build_exec.sh

--- sorc/auto_build_exec.sh
+++ sorc/auto_build_exec.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-
-#=====Step 1: check out source code and  external compoments =======
+#-------------------------------------------------------------------
+#=====  Step 1: check out source code and  external compoments  ====
+#-------------------------------------------------------------------
 
 ./manage_externals/checkout_externals
 
-
-#=====Step 2: create symbolic links for parm and ush  ==============
+#-------------------------------------------------------------------
+#=====  Step 2: create symbolic links for parm and ush  ============
+#-------------------------------------------------------------------
 
 cd ../parm
  rm -rf aqm_utils nexus_config ufs_utils upp
@@ -20,8 +22,9 @@ cd ../ush
  cp -rp ../sorc/AQM-utils/python_utils  aqm_utils_python	
  cp -rp ../sorc/arl_nexus/utils  nexus_utils
 
-
-#=====Step 3: create symbolic links for sou3ce codes  ==============
+#-------------------------------------------------------------------
+#=====  Step 3: create symbolic links for source codes  ============
+#-------------------------------------------------------------------
 cd ../sorc
 
 for src in aqm_bias_correct aqm_bias_interpolate aqm_post_bias_cor_grib2 aqm_post_grib2 aqm_post_maxi_bias_cor_grib2 aqm_post_maxi_grib2  convert_airnow_csv gefs2clbcs_para
@@ -35,8 +38,9 @@ ln -s ufs-weather-model ufs-model.fd
 
 ln -s UPP/sorc/ncep_post.fd upp.fd
 
-
-#=====Step 4: create executable  codes  ==========================
+#-------------------------------------------------------------------
+#=====  Step 4: create executable  codes             ===============
+#-------------------------------------------------------------------
 
 #./app_build.sh -p=wcoss2 --clean
 
