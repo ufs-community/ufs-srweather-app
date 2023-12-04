ca67512e6c3eb7ce1f2398a57ee3ac9546736179
Jianping.Huang <Jianping.Huang@noaa.gov>
2023-12-04 16:05:40 +0000

    update PARMaqm_utils for 4 J-job scripts and auto_build.sh

--- sorc/auto_build_exec.sh
+++ sorc/auto_build_exec.sh
@@ -1,5 +1,23 @@
+#!/bin/bash
+
+./manage_externals/checkout_externals
+
+cd ../parm
+ rm -rf aqm_utils nexus_config ufs_utils upp
+ cp -rp ../sorc/AQM-utils/parm  aqm_utils
+ cp -rp ../sorc/arl_nexus/config nexus_config
+ cp -rp ../sorc/UFS_UTILS/parm  ufs_utils
+ cp -rp ../sorc/UPP/parm upp	
+ 
+cd ../ush
+ rm -rf aqm_utils_python nexus_utils
+ cp -rp ../sorc/AQM-utils/python_utils  aqm_utils_python	
+ cp -rp ../sorc/arl_nexus/utils  nexus_utils
+
+cd ../sorc
+
 #./app_build.sh -p=wcoss2 --clean
 
-./app_build.sh -p=wcoss2 -a=ATMAQ  --extrn |& tee buildup.log
+./app_build.sh -p=wcoss2 -a=ATMAQ  |& tee buildup.log
 
 #./app_build.sh -p=wcoss2 -a=ATMAQ --build-type=DEBUG |& tee build_debug.log
