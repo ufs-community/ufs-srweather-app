0cf89e3765d235656c9ca209bb2648a75df05a8c
Ratko Vasic <Ratko.Vasic@noaa.gov>
2024-01-02 18:38:26 +0000

    Gaea-C5, special case when loading conda environment. Replace srw_app with workflow_tools in .cicd/scripts/srw_ftest.sh

--- .cicd/scripts/srw_ftest.sh
+++ .cicd/scripts/srw_ftest.sh
@@ -85,7 +85,12 @@ module load build_${platform,,}_${SRW_COMPILER}
 module load wflow_${platform,,}
 
 [[ ${FORGIVE_CONDA} == true ]] && set +e +u    # Some platforms have incomplete python3 or conda support, but wouldn't necessarily block workflow tests
-conda activate srw_app
+# Gaea-C5 special case missing jinja2
+if [ "${platform}" == "gaea-c5" ]; then
+  conda activate workflow_tools
+else
+  conda activate srw_app
+fi
 set -e -u
 
 export PYTHONPATH=${workspace}/ush/python_utils/workflow-tools:${workspace}/ush/python_utils/workflow-tools/src
