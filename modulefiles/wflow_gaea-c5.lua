869761c95e82516b6a845f36a7fd4a450cfd0179
RatkoVasic-NOAA <37597874+RatkoVasic-NOAA@users.noreply.github.com>
2024-02-15 16:42:28 -0500

    [develop] Update SRW with spack-stack version 1.5.0 (from 1.4.1) (#969)
    * Update SRW with spack-stack 1.5.0
    * Remove Gaea-C4 (gaea) from SRW App
    * Update run_vx.local.lua on Orion to allow verifications tests to run successfully
    * Temporarily comment out Gaea-C5 (gaeac5) from SRW App
    
    ---------
    
    Co-authored-by: Parallel Works app-run user <Natalie.Perlin@mgmt-natalieperlin-gclusternoaav2usc1-00070.us-central1-c.c.noaa-users.internal>
    Co-authored-by: Natalie Perlin <Natalie.Perlin@noaa.gov>
    Co-authored-by: michael.lueken <Michael.Lueken@noaa.gov>
    Co-authored-by: Natalie Perlin <perlin.natalie@gmail.com>

--- modulefiles/wflow_gaea-c5.lua
+++ modulefiles/wflow_gaea-c5.lua
@@ -6,15 +6,16 @@ the NOAA RDHPC machine Gaea C5
 whatis([===[Loads libraries needed for running the UFS SRW App on gaea ]===])
 
 unload("python")
-
-load("conda")
-prepend_path("MODULEPATH","/lustre/f2/dev/role.epic/contrib/C5/rocoto/modulefiles")
+load("set_pythonpath")
+prepend_path("MODULEPATH","/ncrc/proj/epic/miniconda3/modulefiles/")
+load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
+prepend_path("MODULEPATH","/ncrc/proj/epic/rocoto/modulefiles/")
 load("rocoto")
 
 pushenv("MKLROOT", "/opt/intel/oneapi/mkl/2023.1.0/")
 
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
-       > conda activate srw_app
+       > conda activate workflow_tools
 ]===])
 end
