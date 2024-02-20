2eb3f12103a72a672ddd77286256e6e68e6e9210
RatkoVasic-NOAA <37597874+RatkoVasic-NOAA@users.noreply.github.com>
2024-02-20 09:17:36 -0500

    Update cmake version

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -20,7 +20,7 @@ load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 stack_python_ver=os.getenv("stack_python_ver") or "3.10.8"
 load(pathJoin("stack-python", stack_python_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.20.1"
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
 load("srw_common")
