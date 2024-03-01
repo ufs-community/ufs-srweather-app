c33c8f34d91db6cc077bf2e3af2880e850015d56
RatkoVasic-NOAA <37597874+RatkoVasic-NOAA@users.noreply.github.com>
2024-03-01 14:59:48 -0500

    Add description of usage Rocky OS on Jet

--- modulefiles/build_jet_intel.lua
+++ modulefiles/build_jet_intel.lua
@@ -6,7 +6,10 @@ the NOAA RDHPC machine Jet using Intel-2021.5.0
 whatis([===[Loads libraries needed for building the UFS SRW App on Jet ]===])
 
 -- When Jet switches from CentOS to Rocky, replace line withh correct path to spack-stack
--- If you want to use Rocky OS now, use line below
+-- If you want to use Rocky OS now on Jet:
+-- 1. login to one of Rocky Jet nodes: fe5 - fe8
+-- 2. uncomment line to Rocky spack-stack, and comment current line with path to spack-stack
+-- 3. in file  ush/machine/jet.yaml set PARTITION_FCST=xjet
 --prepend_path("MODULEPATH","/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.5.0/envs/unified-env-rocky8/install/modulefiles/Core")
 prepend_path("MODULEPATH","/mnt/lfs4/HFIP/hfv3gfs/role.epic/spack-stack/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/lfs4/HFIP/hfv3gfs/spack-stack/modulefiles")
