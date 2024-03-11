51341cb236905f0415b73d247f89cab410c3f11f
Michael Lueken <michael.lueken@noaa.gov>
2024-03-11 13:42:45 +0000

    [feature/hash_update] Reintroduce threading to the SRW to allow high resolution tests to run successfully on Jet

--- ush/create_ufs_configure_file.py
+++ ush/create_ufs_configure_file.py
@@ -53,7 +53,6 @@ def create_ufs_configure_file(run_dir):
     # Set output file path
     #
     ufs_config_fp = os.path.join(run_dir, UFS_CONFIG_FN)
-    pe_member01_m1 = str(int(PE_MEMBER01)-1)
     #
     #-----------------------------------------------------------------------
     #
@@ -66,9 +65,7 @@ def create_ufs_configure_file(run_dir):
     settings = {
       "dt_atmos": DT_ATMOS,
       "print_esmf": PRINT_ESMF,
-      "cpl_aqm": CPL_AQM,
-      "pe_member01_m1": pe_member01_m1,
-      "atm_omp_num_threads": OMP_NUM_THREADS_RUN_FCST,
+      "cpl_aqm": CPL_AQM
     }
     settings_str = cfg_to_yaml_str(settings)
 
