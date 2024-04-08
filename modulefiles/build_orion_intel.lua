bf7b49058709b7bae3c02d49300127203b5266ff
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2024-04-08 09:01:43 -0400

    [develop] Port SRW-AQM to Orion and Hercules (#1067)
    * Port SRW-AQM to Orion and Hercules

--- modulefiles/build_orion_intel.lua
+++ modulefiles/build_orion_intel.lua
@@ -18,6 +18,7 @@ load("srw_common")
 load("nccmp/1.9.0.1")
 load("nco/5.0.6")
 load("wget")
+load(pathJoin("prod_util", os.getenv("prod_util_ver") or "1.2.2"))
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER","mpiicpc")
