2b649286d22600b62ac909fa407e823d3e2ad93d
RatkoVasic-NOAA <37597874+RatkoVasic-NOAA@users.noreply.github.com>
2024-04-10 11:18:04 -0400

    Update build_orion_intel.lua

--- modulefiles/build_orion_intel.lua
+++ modulefiles/build_orion_intel.lua
@@ -5,20 +5,20 @@ the MSU machine Orion using Intel-2022.1.2
 
 whatis([===[Loads libraries needed for building the UFS SRW App on Orion ]===])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.5.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.7.0/envs/ue-intel-centos/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/work/noaa/da/role-da/spack-stack/modulefiles")
 
 load("stack-intel/2022.0.2")
 load("stack-intel-oneapi-mpi/2021.5.1")
-load("stack-python/3.10.8")
-load("cmake/3.22.1")
+load("stack-python/3.10.13")
+load("cmake/3.23.1")
 
 load("srw_common")
 
 load("nccmp/1.9.0.1")
-load("nco/5.0.6")
+load("nco/5.1.6")
 load("wget")
-load(pathJoin("prod_util", os.getenv("prod_util_ver") or "1.2.2"))
+load(pathJoin("prod_util", os.getenv("prod_util_ver") or "2.1.1"))
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER","mpiicpc")
