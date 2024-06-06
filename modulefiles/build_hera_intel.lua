fe586005348464e2534e51b088900f8bca893064
Ratko Vasic <ratko.vasic@noaa.gov>
2024-06-06 17:41:39 +0000

    Revert develop branch

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -8,7 +8,8 @@ whatis([===[Loads libraries needed for building the UFS SRW App on Hera ]===])
 prepend_path("MODULEPATH","/contrib/sutils/modulefiles")
 load("sutils")
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.6.0/envs/unified-env-rocky8/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/unified-env-rocky8/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/jcsda/jedipara/spack-stack/modulefiles")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -16,7 +17,7 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-stack_python_ver=os.getenv("stack_python_ver") or "3.10.13"
+stack_python_ver=os.getenv("stack_python_ver") or "3.10.8"
 load(pathJoin("stack-python", stack_python_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
@@ -26,7 +27,7 @@ load("srw_common")
 
 load(pathJoin("nccmp", os.getenv("nccmp_ver") or "1.9.0.1"))
 load(pathJoin("nco", os.getenv("nco_ver") or "5.0.6"))
-load(pathJoin("prod_util", os.getenv("prod_util_ver") or "2.1.1"))
+load(pathJoin("prod_util", os.getenv("prod_util_ver") or "1.2.2"))
 
 setenv("CMAKE_C_COMPILER","mpiicc")
 setenv("CMAKE_CXX_COMPILER","mpiicpc")
