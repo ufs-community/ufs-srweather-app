95d72b478217ef8f7beddda64bea4d4f4c6eebcb
EdwardSnyder-NOAA <Edward.Snyder@noaa.gov>
2024-06-11 20:00:07 +0000

    add logic to handle missing gnu binary on Azure

--- modulefiles/wflow_noaacloud.lua
+++ modulefiles/wflow_noaacloud.lua
@@ -17,6 +17,11 @@ setenv("OPT","/contrib/EPIC/hpc-modules")
 append_path("PATH","/contrib/EPIC/miniconda3/4.12.0/envs/regional_workflow/bin")
 prepend_path("PATH","/contrib/EPIC/bin")
 
+-- Add missing libstdc binary for Azure
+if os.getenv("PW_CSP") == "azure" then
+   setenv("LD_PRELOAD","/opt/nvidia/nsight-systems/2023.1.2/host-linux-x64/libstdc++.so.6")
+end
+
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
        > conda activate srw_app
