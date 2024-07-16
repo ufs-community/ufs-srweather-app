35530abd74948fc50d1e6ebc25a25a8a7cd2f8c2
gerard ketefian <gerard.ketefian@noaa.gov>
2024-07-16 11:01:43 -0600

    Bug fix:  Append cycle date to names of deterministic GridStat and PointStat tasks' METplus log files.

--- scripts/exregional_run_met_gridstat_or_pointstat_vx.sh
+++ scripts/exregional_run_met_gridstat_or_pointstat_vx.sh
@@ -277,7 +277,7 @@ fi
 #
 metplus_config_tmpl_bn="GridStat_or_PointStat"
 metplus_config_bn="${MetplusToolName}_${FIELDNAME_IN_MET_FILEDIR_NAMES}_${ensmem_name}"
-metplus_log_bn="${metplus_config_bn}"
+metplus_log_bn="${metplus_config_bn}_$CDATE"
 #
 # Add prefixes and suffixes (extensions) to the base file names.
 #
