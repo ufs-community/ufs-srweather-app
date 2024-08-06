499375f88f32aaed2293f4430ceed79a31096332
Jianping.Huang <Jianping.Huang@noaa.gov>
2024-08-06 12:40:50 +0000

    update aqm_utils hash and update exaqm_fire scripts for RAVE v2

--- scripts/exaqm_fire_emission.sh
+++ scripts/exaqm_fire_emission.sh
@@ -170,16 +170,6 @@ else
 
 fi
 #
- cd ${FIRE_EMISSION_STAGING_DIR}
- mv ${aqm_fire_file_fn}  temp.nc
- ncrename -v PM2.5,PM25 temp.nc temp1.nc
- ncap2 -s 'where(Latitude > 30 && Latitude <=49 && land_cover == 1 ) PM25 = PM25 * 0.5' temp1.nc temp2.nc
- ncap2 -s 'where(Latitude <=30 && land_cover == 1 ) PM25 = PM25 * 0.5'       temp2.nc temp3.nc
- ncap2 -s 'where(Latitude <=49 && land_cover == 3 ) PM25 = PM25 * 0.5'   temp3.nc temp4.nc
- ncap2 -s 'where(Latitude <=49 && land_cover == 4 ) PM25 = PM25 * 0.5'   temp4.nc temp5.nc
- ncrename -v PM25,PM2.5 temp5.nc temp6.nc
- mv temp6.nc ${aqm_fire_file_fn}
- rm -rf temp*nc
 #-----------------------------------------------------------------------
 #
 # Restore the shell options saved at the beginning of this script/function.
