cb2a7139db9d1f2362d6ebb377909f37c76c343a
Michael Lueken <63728921+MichaelLueken@users.noreply.github.com>
2024-08-12 09:29:41 -0400

    [develop] Update ufs-weather-model hash and further clean the machines tested in PULL_REQUEST_TEMPLATE (#1096)
    * Update ufs-weather-model hash to b5a1976 (July 30)
    * Add hera.gnu, remove cheyenne.intel, cheyenne.gnu, and gaeac5.intel, and alphabetize the machines in the TESTS CONDUCTED section of the PULL_REQUEST_TEMPLATE
    * Correct behavior of Jenkins Functional WorkflowTaskTests. Currently, TASK_DEPTH is set to null, resulting in no tests being run during the Functional WorkflowTaskTests stage. Replaced env with params in Jenkinsfile for setting TASK_DEPTH. Testing shows that this will correctly set TASK_DEPTH to the default value of 9 and allow the tests to run
    * Removed extraneous entries from the verification scripts to remove KeyError messages in the associated verification log files
    * Reapplied necessary modification to modulefiles/tasks/noaacloud/plot_allvars.local.lua to allow plotting tasks to run on NOAA cloud platforms

--- modulefiles/tasks/noaacloud/plot_allvars.local.lua
+++ modulefiles/tasks/noaacloud/plot_allvars.local.lua
@@ -1,5 +1,2 @@
-unload("python")
-append_path("MODULEPATH","/contrib/EPIC/miniconda3/modulefiles")
-load(pathJoin("miniconda3", os.getenv("miniconda3_ver") or "4.12.0"))
-
-setenv("SRW_GRAPHICS_ENV", "regional_workflow")
+load("conda")
+setenv("SRW_GRAPHICS_ENV", "srw_graphics")
