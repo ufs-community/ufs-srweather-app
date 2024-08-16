c6bf03f26ed9514ee715d32f829853eb6117081a
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2024-08-16 17:52:15 +0000

    modify ex-script for smoke/dust

--- scripts/exsrw_smoke_dust.sh
+++ scripts/exsrw_smoke_dust.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#
 #
 #-----------------------------------------------------------------------
 #
@@ -8,8 +7,12 @@
 #
 #-----------------------------------------------------------------------
 #
-. ${GLOBAL_VAR_DEFNS_FP}
-. $USHdir/source_util_funcs.sh
+. ${USHsrw}/source_util_funcs.sh
+for sect in user nco platform workflow nco global verification cpl_aqm_parm \
+  constants fixed_files grid_params \
+  task_point_source task_run_fcst ; do
+  source_yaml ${GLOBAL_VAR_DEFNS_FP} ${sect}
+done
 #
 #-----------------------------------------------------------------------
 #
@@ -18,7 +21,7 @@
 #
 #-----------------------------------------------------------------------
 #
-{ save_shell_opts; set -u -x; } > /dev/null 2>&1
+{ save_shell_opts; set -xue; } > /dev/null 2>&1
 #
 #-----------------------------------------------------------------------
 #
@@ -28,7 +31,7 @@
 #
 #-----------------------------------------------------------------------
 #
-scrfunc_fp=$( readlink -f "${BASH_SOURCE[0]}" )
+scrfunc_fp=$( $READLINK -f "${BASH_SOURCE[0]}" )
 scrfunc_fn=$( basename "${scrfunc_fp}" )
 scrfunc_dir=$( dirname "${scrfunc_fp}" )
 #
@@ -43,7 +46,7 @@ print_info_msg "
 Entering script:  \"${scrfunc_fn}\"
 In directory:     \"${scrfunc_dir}\"
 
-This is the script for the task that runs smoke emissions preprocessing.
+This is the ex-script for the task that runs Smoke and Dust.
 ========================================================================"
 #
 #-----------------------------------------------------------------------
@@ -64,79 +67,58 @@ mkdir -p "${hourly_hwpdir}"
 # is reused
 #
 #-----------------------------------------------------------------------
-ECHO=/bin/echo
-SED=/bin/sed
-DATE=/bin/date
-LN=/bin/ln
-ebb_dc=${EBB_DCYCLE}
-
-# Start date manipulation
-START_DATE=$(${ECHO} "${CDATE}" | ${SED} 's/\([[:digit:]]\{2\}\)$/ \1/')
-YYYYMMDDHH=$(${DATE} +%Y%m%d%H -d "${START_DATE}")
-YYYYMMDD=${YYYYMMDDHH:0:8}
-HH=${YYYYMMDDHH:8:2}
-${ECHO} ${YYYYMMDD}
-${ECHO} ${HH}
-
+#
 # Current and previous day calculation
-current_day=`${DATE} -d "${YYYYMMDD}"`
-current_hh=`${DATE} -d ${HH} +"%H"`
-
-prev_hh=`${DATE} -d "$current_hh -24 hour" +"%H"`
-previous_day=`${DATE} '+%C%y%m%d' -d "$current_day-1 days"`
-previous_day="${previous_day} ${prev_hh}"
+CDATE=${PDY}${cyc}
+CDATEm1="${PDYm1}${cyc}"
 
 # Number of files to process
 nfiles=24
-smokeFile=SMOKE_RRFS_data_${YYYYMMDDHH}00.nc
+smokeFile="SMOKE_RRFS_data_${CDATE}00.nc"
 
 for i in $(seq 0 $(($nfiles - 1)) )
 do 
-   if [ "$ebb_dc" -eq 2 ]; then	
+  if [ "${EBB_DCYCLE}" -eq 2 ]; then	
+    # For ebb_dc == 2	   
+    timestr=$($NDATE +$i ${CDATEm1})
+    intp_fname=${PREDEF_GRID_NAME}_intp_${timestr}00_${timestr}59.nc
+  else
+    # For ebb_dc == 1	   
+    timestr=$($NDATE +$i ${CDATE})
+    intp_fname=${PREDEF_GRID_NAME}_intp_${timestr}00_${timestr}59.nc
+  fi
 
-      # For ebb_dc == 2	   
-      timestr=`date +%Y%m%d%H -d "$previous_day + $i hours"`
-      intp_fname=${PREDEF_GRID_NAME}_intp_${timestr}00_${timestr}59.nc
-   else
-       # For ebb_dc == 1	   
-      timestr=`date +%Y%m%d%H -d "$current_day $current_hh + $i hours"`
-      intp_fname=${PREDEF_GRID_NAME}_intp_${timestr}00_${timestr}59.nc
-   fi
-
-   if  [ -f ${rave_nwges_dir}/${intp_fname} ]; then
-      ${LN} -sf ${rave_nwges_dir}/${intp_fname} ${workdir}/${intp_fname}
-      echo "${rave_nwges_dir}/${intp_fname} interoplated file available to reuse"
-   else
-      echo "${rave_nwges_dir}/${intp_fname} interoplated file non available to reuse"  
-   fi
+  if [ -f ${rave_nwges_dir}/${intp_fname} ]; then
+    ln -nsf ${rave_nwges_dir}/${intp_fname} ${workdir}/${intp_fname}
+    echo "${rave_nwges_dir}/${intp_fname} interoplated file available to reuse"
+  else
+    echo "${rave_nwges_dir}/${intp_fname} interoplated file non available to reuse"  
+  fi
 done
-
+#
 #-----------------------------------------------------------------------
 #
 #  link RAVE data to work directory  $workdir
 #
 #-----------------------------------------------------------------------
-
-previous_2day=`${DATE} '+%C%y%m%d' -d "$current_day-2 days"`
-YYYYMMDDm1=${previous_day:0:8}
-YYYYMMDDm2=${previous_2day:0:8}
-if [ -d ${FIRE_RAVE_DIR}/${YYYYMMDDm1}/rave ]; then
-   fire_rave_dir_work=${workdir}
-   ln -snf ${FIRE_RAVE_DIR}/${YYYYMMDD}/rave/RAVE-HrlyEmiss-3km_* ${fire_rave_dir_work}/.
-   ln -snf ${FIRE_RAVE_DIR}/${YYYYMMDDm1}/rave/RAVE-HrlyEmiss-3km_* ${fire_rave_dir_work}/.
-   ln -snf ${FIRE_RAVE_DIR}/${YYYYMMDDm2}/rave/RAVE-HrlyEmiss-3km_* ${fire_rave_dir_work}/.
+#
+if [ -d ${FIRE_RAVE_DIR}/${PDYm1}/rave ]; then
+  fire_rave_dir_work=${workdir}
+  ln -snf ${FIRE_RAVE_DIR}/${PDY}/rave/RAVE-HrlyEmiss-3km_* ${fire_rave_dir_work}/.
+  ln -snf ${FIRE_RAVE_DIR}/${PDYm1}/rave/RAVE-HrlyEmiss-3km_* ${fire_rave_dir_work}/.
+  ln -snf ${FIRE_RAVE_DIR}/${PDYm2}/rave/RAVE-HrlyEmiss-3km_* ${fire_rave_dir_work}/.
 else
-   fire_rave_dir_work=${FIRE_RAVE_DIR}
+  fire_rave_dir_work=${FIRE_RAVE_DIR}
 fi
 
 # Check whether the RAVE files need to be split into hourly files
 # Format the current day and hour properly for UTC
-if [ "$ebb_dc" -eq 1 ]; then
-    ddhh_to_use="${current_day}${current_hh}"
-    dd_to_use="${current_day}"
+if [ "${EBB_DCYCLE}" -eq 1 ]; then
+  ddhh_to_use="${PDY}${cyc}"
+  dd_to_use="${PDY}"
 else
-    ddhh_to_use="${previous_day}${prev_hh}"
-    dd_to_use="${previous_day}"
+  ddhh_to_use="${PDYm1}${cyc}"
+  dd_to_use="${PDYm1}"
 fi
 
 # Construct file names and check their existence
@@ -149,24 +131,23 @@ echo "Checking for files in directory: $fire_rave_dir_work"
 files_found=$(find "$fire_rave_dir_work" -type f \( -name "${intp_fname##*/}" -o -name "${intp_fname_beta##*/}" \))
 
 if [ -z "$files_found" ]; then
-    echo "No files found matching patterns."
+  echo "No files found matching patterns."
 else
-    echo "Files found, proceeding with processing..."
-    for file_to_use in $files_found; do
-        echo "Using file: $file_to_use"
-        for hour in {00..23}; do
-            output_file="${fire_rave_dir_work}/Hourly_Emissions_3km_${dd_to_use}${hour}00_${dd_to_use}${hour}00.nc"
-            if [ -f "$output_file" ]; then
-                echo "Output file for hour $hour already exists: $output_file. Skipping..."
-                continue
-            fi
-            echo "Splitting data for hour $hour..."
-            ncks -d time,$hour,$hour "$file_to_use" "$output_file"
-        done
-        echo "Hourly files processing completed for: $file_to_use"
+  echo "Files found, proceeding with processing..."
+  for file_to_use in $files_found; do
+    echo "Using file: $file_to_use"
+    for hour in {00..23}; do
+      output_file="${fire_rave_dir_work}/Hourly_Emissions_3km_${dd_to_use}${hour}00_${dd_to_use}${hour}00.nc"
+      if [ -f "$output_file" ]; then
+        echo "Output file for hour $hour already exists: $output_file. Skipping..."
+        continue
+      fi
+      echo "Splitting data for hour $hour..."
+      ncks -d time,$hour,$hour "$file_to_use" "$output_file"
     done
+    echo "Hourly files processing completed for: $file_to_use"
+  done
 fi
-
 #
 #-----------------------------------------------------------------------
 #
@@ -174,7 +155,7 @@ fi
 #
 #-----------------------------------------------------------------------
 #
-python -u  ${USHdir}/generate_fire_emissions.py \
+python -u ${USHdir}/generate_fire_emissions.py \
   "${FIX_SMOKE_DUST}/${PREDEF_GRID_NAME}" \
   "${fire_rave_dir_work}" \
   "${workdir}" \
@@ -189,8 +170,8 @@ export err
 
 # Check the return code before calling err_chk
 if [ $err -ne 0 ]; then
-    echo "Error: generate_fire_emissions.py failed with return code $err"
-    err_chk
+  echo "Error: generate_fire_emissions.py failed with return code $err"
+  err_chk
 fi
 
 #Copy the the hourly, interpolated RAVE data to $rave_nwges_dir so it
@@ -204,56 +185,49 @@ are_all_files_older_than_15_days() {
 
 # Check if all files in the rave_nwges_dir are older than 5 days
 if are_all_files_older_than_15_days "${rave_nwges_dir}"; then
-    echo "All files are older than 5 days. Replacing all files."
-
-    # Loop through all files in the work directory and replace them in rave_nwges_dir
-    for file in ${workdir}/*; do
-        filename=$(basename "$file")
-        target_file="${rave_nwges_dir}/${filename}"
+  echo "All files are older than 5 days. Replacing all files."
+  # Loop through all files in the work directory and replace them in rave_nwges_dir
+  for file in ${workdir}/*; do
+    filename=$(basename "$file")
+    target_file="${rave_nwges_dir}/${filename}"
         
-        cp "${file}" "${target_file}"
-        echo "Copied file: $filename"
-    done
+    cp "${file}" "${target_file}"
+    echo "Copied file: $filename"
+  done
 else
-    echo "Not all files are older than 5 days. Checking individual files."
+  echo "Not all files are older than 5 days. Checking individual files."
+  # Loop through all files in the work directory
+  for file in ${workdir}/*; do
+    filename=$(basename "$file")
+    target_file="${rave_nwges_dir}/${filename}"
 
-    # Loop through all files in the work directory
-    for file in ${workdir}/*; do
-        filename=$(basename "$file")
-        target_file="${rave_nwges_dir}/${filename}"
-
-        # Check if the file matches the pattern or is missing in the target directory
-        if [[ "$filename" =~ SMOKE_RRFS_data_.*\.nc ]]; then
-            cp "${file}" "${target_file}"
-            echo "Copied file: $filename"
-        elif [ ! -f "${target_file}" ]; then
-            cp "${file}" "${target_file}"
-            echo "Copied missing file: $filename"
-        fi
-    done
+    # Check if the file matches the pattern or is missing in the target directory
+    if [[ "$filename" =~ SMOKE_RRFS_data_.*\.nc ]]; then
+      cp "${file}" "${target_file}"
+      echo "Copied file: $filename"
+    elif [ ! -f "${target_file}" ]; then
+      cp "${file}" "${target_file}"
+      echo "Copied missing file: $filename"
+    fi
+  done
 fi
 
 echo "Copy RAVE interpolated files completed"
-
 #
 #-----------------------------------------------------------------------
 #
-# Print exit message.
+# Print message indicating successful completion of script.
 #
 #-----------------------------------------------------------------------
 #
 print_info_msg "
 ========================================================================
+Smoke and Dust has successfully generated output files !!!!
+
 Exiting script:  \"${scrfunc_fn}\"
 In directory:    \"${scrfunc_dir}\"
 ========================================================================"
 #
 #-----------------------------------------------------------------------
 #
-# Restore the shell options saved at the beginning of this script/function.
-#
-#-----------------------------------------------------------------------
-#
 { restore_shell_opts; } > /dev/null 2>&1
-
-
