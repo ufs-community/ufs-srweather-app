37ba84340801073d73046d74285215b779e4ba3a
JianpingHuang-NOAA <54634403+JianpingHuang-NOAA@users.noreply.github.com>
2024-08-22 14:42:02 -0400

    [production/AQM.v7] Update the workflow to support aqm.v7.0.8 implementation  (#1119)
    This PR is used to update the settings of using 14 computational nodes for the forecast job in support of the AQM v7.0.8 implementation.

--- sorc/auto_A3_cp_fix.sh
+++ sorc/auto_A3_cp_fix.sh
@@ -2,7 +2,7 @@
 #
 set -ax
 #
-export dev_fix=/lfs/h2/emc/physics/noscrub/UFS_SRW_App/aqm.v7/fix
+export dev_fix=/lfs/h2/emc/physics/noscrub/UFS_SRW_App/aqm.v7.0.8/fix
 cd ../
 
 export HOMEaqm=$(pwd)
