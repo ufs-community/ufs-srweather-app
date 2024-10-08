9435f7f29accbee047ac5a8c0c679996e03ac9d7
gerard ketefian <gerard.ketefian@noaa.gov>
2024-10-08 14:14:10 -0600

    Clean up comments, remove commented-out code.

--- ush/run_eval_METplus_timestr_tmpl.sh
+++ ush/run_eval_METplus_timestr_tmpl.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
-
 #
 #-----------------------------------------------------------------------
 #
-# Source the variable definitions file and the bash utility functions.
+# This script is simply a wrapper to the eval_METplus_timestr_tmpl bash
+# function.  It is needed in order to enable the function to be called
+# from a python script.
 #
 #-----------------------------------------------------------------------
 #
-#OBS_DIR="/scratch2/BMC/fv3lam/Gerard.Ketefian/AGILE/expt_dirs/get_obs_hpss.do_vx_det.singlecyc.init_00z_fcstlen_36hr.winter_wx.SRW/obs_data/ccpa"
-#OBS_CCPA_APCP_FN_TEMPLATE="{valid?fmt=%Y%m%d}/ccpa.t{valid?fmt=%H}z.01h.hrap.conus.gb2"
-
-#USHdir="/scratch2/BMC/fv3lam/Gerard.Ketefian/AGILE/ufs-srweather-app/ush"
-#yyyymmdd_task="20230217"
-#lhr="22"
-#METplus_timestr_tmpl="/scratch2/BMC/fv3lam/Gerard.Ketefian/AGILE/expt_dirs/get_obs_hpss.do_vx_det.singlecyc.init_00z_fcstlen_36hr.winter_wx.SRW/obs_data/ccpa/{valid?fmt=%Y%m%d}/ccpa.t{valid?fmt=%H}z.01h.hrap.conus.gb2"
-
-#USHdir="/scratch2/BMC/fv3lam/Gerard.Ketefian/AGILE/ufs-srweather-app/ush"; yyyymmdd_task="20230217"; lhr="22"; METplus_timestr_tmpl="/scratch2/BMC/fv3lam/Gerard.Ketefian/AGILE/expt_dirs/get_obs_hpss.do_vx_det.singlecyc.init_00z_fcstlen_36hr.winter_wx.SRW/obs_data/ccpa/{valid?fmt=%Y%m%d}/ccpa.t{valid?fmt=%H}z.01h.hrap.conus.gb2"
 set -u
 . $USHdir/source_util_funcs.sh
 eval_METplus_timestr_tmpl \
@@ -24,5 +16,3 @@ eval_METplus_timestr_tmpl \
   METplus_timestr_tmpl="${METplus_timestr_tmpl}" \
   outvarname_evaluated_timestr="fp_proc"
 echo "${fp_proc}"
-
-#  METplus_timestr_tmpl="${OBS_DIR}/${OBS_CCPA_APCP_FN_TEMPLATE}" \
