6dd8e20723f39d5e2cf628d18729614bea824b99
gerard ketefian <gerard.ketefian@noaa.gov>
2024-10-10 09:01:14 -0600

    Modify old test for set_cycle_dates to fit new version of this function.  This includes adding a new test for the case in which the output should be a list of datetime objects (the default is for the output to be a list of strings).

--- tests/test_python/test_set_cycle_dates.py
+++ tests/test_python/test_set_cycle_dates.py
@@ -1,20 +1,22 @@
 """ Test set_cycle_dates.py """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import unittest
 
-from set_cycle_dates import set_cycle_dates
+from set_cycle_and_obs_timeinfo import set_cycle_dates
 
 class Testing(unittest.TestCase):
     """ Define the tests"""
-    def test_set_cycle_dates(self):
+
+    def test_set_cycle_dates_string(self):
 
         """ Test that the proper list of dates are produced given the
-        intput data"""
+        input data and return_type left to its default value (so the 
+        output should be a list of strings)"""
         cdates = set_cycle_dates(
-            date_start=datetime(2022, 1, 1, 6),
-            date_end=datetime(2022, 1, 2, 12),
-            incr_cycl_freq=6,
+            start_time_first_cycl=datetime(2022, 1, 1, 6),
+            start_time_last_cycl=datetime(2022, 1, 2, 12),
+            cycl_intvl=timedelta(hours=6),
         )
         self.assertEqual(
             cdates,
@@ -25,5 +27,28 @@ class Testing(unittest.TestCase):
                 "2022010200",
                 "2022010206",
                 "2022010212",
+            ],
+        )
+
+    def test_set_cycle_dates_datetime(self):
+
+        """ Test that the proper list of dates are produced given the
+        input data and return_type left set to "datetime" (so the output
+        should be a list of datetime objects)"""
+        cdates = set_cycle_dates(
+            start_time_first_cycl=datetime(2022, 1, 1, 6),
+            start_time_last_cycl=datetime(2022, 1, 2, 12),
+            cycl_intvl=timedelta(hours=6),
+            return_type="datetime",
+        )
+        self.assertEqual(
+            cdates,
+            [
+                datetime(2022, 1, 1, 6),
+                datetime(2022, 1, 1, 12),
+                datetime(2022, 1, 1, 18),
+                datetime(2022, 1, 2, 0),
+                datetime(2022, 1, 2, 6),
+                datetime(2022, 1, 2, 12),
             ],
         )
