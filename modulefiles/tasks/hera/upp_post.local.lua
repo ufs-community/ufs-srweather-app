a0be6bdbd667932605f4847d68d6923a7be4d088
Chan-Hoo Jeon <chan-hoo.jeon@noaa.gov>
2024-10-18 15:56:54 -0500

    fix module file for upp on orion

--- modulefiles/tasks/hera/upp_post.local.lua
+++ modulefiles/tasks/hera/upp_post.local.lua
@@ -3,9 +3,11 @@ prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
 
+load(pathJoin("jasper", jasper_ver))
 load(pathJoin("hdf5", hdf5_ver))
 load(pathJoin("netcdf-c", netcdf_c_ver))
 load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
+load(pathJoin("g2", g2_ver))
 load(pathJoin("wgrib2", wgrib2_ver))
 load(pathJoin("prod_util", prod_util_ver))
 
