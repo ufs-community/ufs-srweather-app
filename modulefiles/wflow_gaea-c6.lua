f18a40c308e04a4543af0e00454a0215a1802f01
Chan-hoo Jeon <Chan-hoo.Jeon@gaea62.ncrc.gov>
2025-01-10 10:44:00 -0500

    fix miniconda3 path

--- modulefiles/wflow_gaea-c6.lua
+++ modulefiles/wflow_gaea-c6.lua
@@ -3,14 +3,15 @@ This module loads python environement for running the UFS SRW-AQM/SD/FB on
 the NOAA RDHPC machine Gaea C6
 ]])
 
-whatis([===[Loads libraries needed for running the UFS SRW-AQM/SD/FB on Gaea c6 ]===])
+whatis([===[Loads libraries needed for running the UFS SRW-AQM/SD/FB on Gaea C6 ]===])
 
 unload("python")
-prepend_path("MODULEPATH","/ncrc/proj/epic/rocoto/modulefiles/")
+prepend_path("MODULEPATH","/ncrc/proj/epic/rocoto/modulefiles")
 load("rocoto")
-load("conda")
+prepend_path("MODULEPATH","/ncrc/proj/epic/miniconda3/modulefiles")
+load("miniconda3")
 
-pushenv("MKLROOT", "/opt/intel/oneapi/mkl/2023.2.0/")
+pushenv("MKLROOT", "/opt/intel/oneapi/mkl/2023.2.0")
 
 if mode() == "load" then
    LmodMsgRaw([===[Please do the following to activate conda:
