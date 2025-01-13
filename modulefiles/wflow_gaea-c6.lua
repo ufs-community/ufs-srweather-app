52e883368c088200b9eacbcdf965a1d969a80aa3
Chan-hoo Jeon <Chan-hoo.Jeon@gaea62.ncrc.gov>
2025-01-13 06:41:30 -0500

    update wflow module file for gaea-c6

--- modulefiles/wflow_gaea-c6.lua
+++ modulefiles/wflow_gaea-c6.lua
@@ -8,13 +8,16 @@ whatis([===[Loads libraries needed for running the UFS SRW-AQM/SD/FB on Gaea C6 
 unload("python")
 prepend_path("MODULEPATH","/ncrc/proj/epic/rocoto/modulefiles")
 load("rocoto")
-prepend_path("MODULEPATH","/ncrc/proj/epic/miniconda3/modulefiles")
-load("miniconda3")
 
 pushenv("MKLROOT", "/opt/intel/oneapi/mkl/2023.2.0")
 
-if mode() == "load" then
-   LmodMsgRaw([===[Please do the following to activate conda:
-       > conda activate srw_app
-]===])
-end
+prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
+
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
+
+load(pathJoin("py-f90nml", py_f90nml_ver))
+load(pathJoin("py-jinja2", py_jinja2_ver))
+load(pathJoin("py-numpy", py_numpy_ver))
+load(pathJoin("py-pyyaml", py_pyyaml_ver))
+
