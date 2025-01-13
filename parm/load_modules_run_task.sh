63d19247a8e7ee4c0b77dab24638604fa4f32bcb
Chan-hoo Jeon <Chan-hoo.Jeon@gaea64.ncrc.gov>
2025-01-13 11:43:37 -0500

    update module files

--- parm/load_modules_run_task.sh
+++ parm/load_modules_run_task.sh
@@ -118,6 +118,11 @@ if [ "${task_name}" = "smoke_dust" ]; then
     # Esmpy/ESMF segfaults when running through conda. Use a manually built esmpy v8.3.1 and local Python modules.
     module load py-xarray py-pyyaml py-netcdf4
     export PYTHONPATH=/work/noaa/epic/bwkoziol/main_aqm/esmf/src/addon/ESMPy/src:$PYTHONPATH
+  elif [ "${machine}" = "gaea-c6" ]; then
+    module unload python
+    module use /ncrc/proj/epic/miniconda3/modulefiles
+    module load miniconda3/4.12.0
+    conda activate /ncrc/home1/Chan-hoo.Jeon/main_aqm_pyenv
   fi
   set -u
 fi
