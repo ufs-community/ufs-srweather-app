22a156418cd26a07686f2a504633e0ee9e280fc7
Michael Kavulich, Jr. <kavulich@ucar.edu>
2025-02-21 21:26:58 +0000

    More test fixes: correct AWS retrieval command, correctly escape backslashes in SED string

--- tests/test_python/test_generate_FV3LAM_wflow.py
+++ tests/test_python/test_generate_FV3LAM_wflow.py
@@ -56,7 +56,7 @@ class Testing(unittest.TestCase):
         # If running CI, point config.yaml to correct location for fix files
         if fix_files:=get_env_var("CI_FIX_FILES"):
             run_command(
-               f"{sed} -i '-s/\/home\/username\/DATA\/UFS\//{fix_files}/g' {USHdir}/config.yaml")
+               f"{sed} -i '-s/\\/home\\/username\\/DATA\\/UFS\\//{fix_files}/g' {USHdir}/config.yaml")
         run_workflow(USHdir, logfile)
 
     def setUp(self):
