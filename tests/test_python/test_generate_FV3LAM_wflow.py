e8705dcdd21a269ce1f6072a5244acfafe38d5af
Michael Kavulich, Jr. <kavulich@ucar.edu>
2025-02-22 00:33:44 +0000

    More test debugging

--- tests/test_python/test_generate_FV3LAM_wflow.py
+++ tests/test_python/test_generate_FV3LAM_wflow.py
@@ -55,8 +55,21 @@ class Testing(unittest.TestCase):
         )
         # If running CI, point config.yaml to correct location for fix files
         if fix_files:=get_env_var("CI_FIX_FILES"):
-            run_command(f"{sed} -i 's|/home/username/DATA/UFS|{fix_files}|g' "\
-                        f"{USHdir}/config.yaml")
+            print(f"{fix_files=}")
+            config_file=f"{USHdir}/config.yaml"
+            sed_command=f"{sed} -i 's|/home/username/DATA/UFS|{fix_files}|g' "\
+                        f"{USHdir}/config.yaml"
+            print(f"{fix_files=}")
+            print(f"{config_file} contents before sed:")
+            with open(config_file, "r", encoding="utf-8") as file:
+                contents = file.read()
+                print(contents)
+            run_command(sed_command)
+            print(f"{config_file} contents after sed:")
+            with open(config_file, "r", encoding="utf-8") as file:
+                contents = file.read()
+                print(contents)
+
         run_workflow(USHdir, logfile)
 
     def setUp(self):
