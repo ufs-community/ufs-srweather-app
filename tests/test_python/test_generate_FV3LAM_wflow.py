8335e958ce463f34e3b6c7cec73010d34882bba9
Michael Kavulich, Jr. <kavulich@ucar.edu>
2025-03-07 18:18:15 +0000

    Make pylint happy

--- tests/test_python/test_generate_FV3LAM_wflow.py
+++ tests/test_python/test_generate_FV3LAM_wflow.py
@@ -32,7 +32,7 @@ class Testing(unittest.TestCase):
             p.join()
             exit_code = p.exitcode
             if exit_code != 0:
-                with open(logfile, 'r') as fin:
+                with open(logfile, 'r', encoding='utf-8') as fin:
                     print(fin.read())
                 sys.exit(exit_code)
 
