227974af769ea4592be319b3d9bc296bda294937
michael.lueken <Michael.Lueken@noaa.gov>
2025-07-09 15:23:54 -0400

    [feature/hash_update_july_2025] Updated: * Externals.cfg - update ufs-weather-model hash to 1f0ab43 (July 1, 2025) and UPP hash to e5f9fcb (May 2, 2025) * scripts/exregional_run_post.sh - update path to postxconfig-NT-rrfs.txt following the update to UPP hash

--- scripts/exregional_run_post.sh
+++ scripts/exregional_run_post.sh
@@ -165,7 +165,7 @@ else
   if [ $(boolify "${CPL_AQM}") = "TRUE" ]; then
     post_config_fp="${PARMdir}/upp/postxconfig-NT-AQM.txt"
   else
-    post_config_fp="${PARMdir}/upp/postxconfig-NT-rrfs.txt"
+    post_config_fp="${PARMdir}/upp/rrfs/postxconfig-NT-rrfs.txt"
   fi
   print_info_msg "
 ====================================================================
