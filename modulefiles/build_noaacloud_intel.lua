f8eacce6c2ef8b8158c25207173e8cf1850b70d0
michael.lueken <Michael.Lueken@noaa.gov>
2025-07-17 13:21:17 +0000

    [feature/hash_update_july_2025] Update modulefiles/build_noaacloud_intel.lua so that the weather model will build on NOAA Cloud platforms

--- modulefiles/build_noaacloud_intel.lua
+++ modulefiles/build_noaacloud_intel.lua
@@ -7,10 +7,19 @@ whatis([===[Loads libraries needed for building the UFS SRW App on NOAA cloud ]=
 
 prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/apps/modules/modulefiles")
-load("gnu")
-load("stack-intel")
-load("stack-intel-oneapi-mpi")
-unload("gnu")
+
+gnu_ver=os.getenv("gnu_ver") or ""
+load(pathJoin("gnu", gnu_ver))
+
+stack_intel_ver=os.getenv("stack_intel_ver") or ""
+load(pathJoin("stack-intel", stack_intel_ver))
+
+stack_intel_oneapi_mpi_ver=os.getenv("stack_intel_oneapi_mpi_ver") or ""
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
+
+gnu_ver=os.getenv("gnu_ver") or ""
+unload(pathJoin("gnu", gnu_ver))
+
 load("cmake/3.23.1") 
 
 load("srw_common")
@@ -18,4 +27,6 @@ load("srw_common")
 load(pathJoin("nco", os.getenv("nco_ver") or "5.0.6"))
 load(pathJoin("prod_util", os.getenv("prod_util_ver") or "2.1.1"))
 
+setenv("CC", "mpiicc")
+setenv("CXX", "mpiicpc")
 setenv("FC", "mpiifort")
