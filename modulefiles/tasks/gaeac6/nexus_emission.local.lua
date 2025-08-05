3d883a5880ef55b450ccb65c42d4767f58ddadd4
michael.lueken <Michael.Lueken@noaa.gov>
2025-08-05 13:24:52 -0400

    [feature/spack-stack-1.9.2] Final AQM corrections on Gaea C6

--- modulefiles/tasks/gaeac6/nexus_emission.local.lua
+++ modulefiles/tasks/gaeac6/nexus_emission.local.lua
@@ -1,11 +1,3 @@
-unload("esmf/8.8.0")
-unload("python/3.11.7")
-unload("python-venv/1.0")
-unload("py-mpi4py/4.0.1")
-unload("py-numpy/1.26.4")
-unload("py-pip/23.1.2")
-unload("py-pyyaml/6.0.2")
-
 unload("python_srw")
 load("python_srw_aqm")
 
