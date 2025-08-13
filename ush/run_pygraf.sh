97c61d033c3066ec76f4cabced7043c17dd76e5d
Michael J. Kavulich, Jr. <kavulich@ucar.edu>
2025-08-13 17:56:13 +0000

    pygraf settings for RRFS smoke

--- ush/run_pygraf.sh
+++ ush/run_pygraf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
   
 cd ${PYGRAF_DIR}
-python3 -u create_graphics.py maps -d ${VX_FCST_INPUT_BASEDIR}/${PDY}${cyc} -f 0 ${FCST_LEN_HRS} --file_type prs --file_tmpl "hrrr.t${cyc}z.wrfprsf{FCST_TIME:02d}.grib2" --images image_lists/hrrr_smoke.yml hourly -m "${CAPTION}" -n $nprocs -o $OUTDIR -s ${PDY}${cyc} -r ${RESOLUTION}
+python3 -u create_graphics.py maps -d ${VX_FCST_INPUT_BASEDIR}/${PDY}${cyc} -f 0 ${FCST_LEN_HRS} --file_type prs --file_tmpl "rrfs.t${cyc}z.prslev.f{FCST_TIME:03d}.grib2" --images image_lists/rrfs_smoke.yml hourly -m "${CAPTION}" -n $nprocs -o $OUTDIR -s ${PDY}${cyc} -r ${RESOLUTION}
 
