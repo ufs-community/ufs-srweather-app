48400851521e117268e98494426d6539419306a1
michael.lueken <Michael.Lueken@noaa.gov>
2025-08-14 12:02:40 -0400

    [feature/spack-stack-1.9.2] Update Gaea C6 run_vx.local.lua modulefile to use MET 12.0.1 and METplus 6.0.0

--- modulefiles/tasks/gaeac6/run_vx.local.lua
+++ modulefiles/tasks/gaeac6/run_vx.local.lua
@@ -1,8 +1,8 @@
 --[[
 Compiler-specific modules are used for met and metplus libraries
 --]]
-local met_ver = (os.getenv("met_ver") or "11.1.1")
-local metplus_ver = (os.getenv("metplus_ver") or "5.1.0")
+local met_ver = (os.getenv("met_ver") or "12.0.1")
+local metplus_ver = (os.getenv("metplus_ver") or "6.0.0")
 if (mode() == "load") then
   load(pathJoin("met", met_ver))
   load(pathJoin("metplus",metplus_ver))
@@ -24,3 +24,4 @@ if (mode() == "unload") then
 end
 load("conda")
 setenv("SRW_ENV", "srw_app")
+setenv("LD_PRELOAD", "/usr/lib64/libstdc++.so.6")
