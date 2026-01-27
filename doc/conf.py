d5d279887f0ae3bdbcc630e40a8c146ee9bd36ab
Joshua Kublnick <joshua.kublnick@snhu.edu>
2026-01-27 12:52:30 -0500

    Adding link to linkcheck ignore due to excessive load time

--- doc/conf.py
+++ doc/conf.py
@@ -121,6 +121,7 @@ linkcheck_ignore = [r'https://www\.intel\.com/content/www/us/en/docs/cpp\-compil
                     r'https://www.axios.com/local/new-orleans/2025/01/21/in-photos-historic-snowstorm-new-orleans-louisiana',
                     r'https://repository.library.noaa.gov/view/noaa/30725',
                     r'https://sourceforge.net/projects/xming',
+                    r'https://doi.org/10.5281/zenodo.14834682',
                    ]
 
 # Ignore anchor tags for SRW App data bucket. Shows Not Found even when they exist.
