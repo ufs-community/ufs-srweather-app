a049f6386ac78d770c37d5753e5bacaa498df53b
Joshua Kublnick <joshua.kublnick@snhu.edu>
2026-02-19 13:35:54 -0500

    fixing link

--- doc/conf.py
+++ doc/conf.py
@@ -123,6 +123,7 @@ linkcheck_ignore = [r'https://www\.intel\.com/content/www/us/en/docs/cpp\-compil
                     r'https://sourceforge.net/projects/xming',
                     r'https://doi.org/10.5281/zenodo.14834682',
                     r'https://www.naturalearthdata.com/downloads/',
+                    r'https://www.xquartz.org/',
                    ]
 
 # Ignore anchor tags for SRW App data bucket. Shows Not Found even when they exist.
