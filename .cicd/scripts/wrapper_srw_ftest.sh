d7f0eb24765fc95aa754c09a92f6f3855261ae5a
michael.lueken <Michael.Lueken@noaa.gov>
2026-08-18 13:45:42 +0000

    [bf/GaeaC6_CICD_fix]: * .cicd/scripts/wrapper_srw_ftest.sh - Updated job_id to include | awk -F';' '{print $1}' to allow the Functional WorkflowTaskTests stage to run on Gaea C6 following the SLURM update

--- .cicd/scripts/wrapper_srw_ftest.sh
+++ .cicd/scripts/wrapper_srw_ftest.sh
@@ -47,7 +47,7 @@ fi
 
 # Call job card and return job_id
 echo "Running: ${workflow_cmd} -A ${SRW_PROJECT} ${arg_1} ${WORKSPACE}/${SRW_PLATFORM}/.cicd/scripts/${workflow_cmd}_srw_ftest.sh"
-job_id=$(${workflow_cmd} -A ${SRW_PROJECT} ${arg_1} ${WORKSPACE}/${SRW_PLATFORM}/.cicd/scripts/${workflow_cmd}_srw_ftest.sh)
+job_id=$(${workflow_cmd} -A ${SRW_PROJECT} ${arg_1} ${WORKSPACE}/${SRW_PLATFORM}/.cicd/scripts/${workflow_cmd}_srw_ftest.sh) | awk -F';' '{print $1}'
 
 echo "Waiting ten seconds for node to initialize"
 sleep 10
